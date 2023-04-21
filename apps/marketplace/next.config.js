012d278931ec86397b0850d8be3d757041323a59
Matthew Ng <matthew.ngjb@gmail.com>
2023-04-21 22:08:35 +0800

    build: :wrench: - updated next.config.js file to accept domains

--- apps/marketplace/next.config.js
+++ apps/marketplace/next.config.js
@@ -7,6 +7,17 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: process.env.NEXT_BUILD_OPTION === 'ignoreType',
   },
+  images: {
+    domains: [
+      'images.unsplash.com',
+      'via.placeholder.com',
+      'source.unsplash.com',
+      'rvndpcxlgtqfvrxhahnm.supabase.co',
+      'www.google.com',
+      'www.asiamediajournal.com',
+      'via.placeholder.com',
+    ],
+  },
 };
 
 module.exports = nextConfig;
