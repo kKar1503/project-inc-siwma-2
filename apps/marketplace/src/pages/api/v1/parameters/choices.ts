282a79104b5d36745ceef458e1c1639d11289958
Xavier Teo <xavierteo26@gmail.com>
2023-04-22 17:27:47 +0800

    feat: :sparkles: add GET endpoint for /parameters/choices

---
+++ apps/marketplace/src/pages/api/v1/parameters/choices.ts
@@ -0,0 +1,13 @@
+import { apiHandler } from "@/utils/api";
+import { formatAPIResponse } from "@/utils/stringUtils";
+import PrismaClient from "@/utils/prisma";
+
+export default apiHandler({
+  allowNonAuthenticated: true,
+}).get(async (req, res) => {
+  // Retrieve all parameter choices from the database
+  const parameterChoices = await PrismaClient.parameter_choices.findMany();
+
+  // Return the result
+  res.status(200).json(formatAPIResponse(parameterChoices));
+});
