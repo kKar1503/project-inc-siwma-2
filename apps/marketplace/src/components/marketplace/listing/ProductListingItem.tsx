6ead4d8a0ca2beed7bf1a2fd565834ff3f24f93d
Matthew Ng <matthew.ngjb@gmail.com>
2023-04-24 17:08:28 +0800

    refactor: :fire: - Removed unnecessary img state

--- apps/marketplace/src/components/marketplace/listing/ProductListingItem.tsx
+++ apps/marketplace/src/components/marketplace/listing/ProductListingItem.tsx
@@ -40,63 +40,59 @@ const ProductListingItem = ({
   createdAt,
   companyName,
   unit_price: isUnitPrice,
-}: ProductListingItemProps) => {
-  const [image, setImage] = React.useState('');
-
-  return (
-    <Card sx={{ maxWidth: 288, maxHeight: 600 }}>
-      <CardHeader
-        avatar={
-          <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-            {ownerFullName[0]}
-          </Avatar>
-        }
-        title={ownerFullName}
-        titleTypographyProps={{
-          fontSize: 16,
-          fontWeight: 'bold',
-        }}
-        subheader={companyName}
-      />
-      <CardMedia component="img" height="288" image={img} alt="Paella dish" />
-      <CardContent style={{ paddingLeft: 16 }}>
-        <div style={{ paddingBottom: 16 }}>
-          {type === 'Buy' && <BuyBadge />}
-          {type === 'Sell' && <SellBadge />}
-          {negotiable && <NegotiableBadge />}
-        </div>
-        <div style={{ paddingBottom: 16 }}>
-          <Typography variant="body2" color="text.primary" fontWeight={400} fontSize={20}>
-            {name}
-          </Typography>
-        </div>
-        <div style={{ paddingBottom: 16 }}>
-          <Typography variant="h4" color="text.primary" fontWeight="bold" fontSize={24}>
-            {new Intl.NumberFormat('en-SG', {
-              style: 'currency',
-              currency: 'SGD',
-            }).format(price)}
-            {isUnitPrice && <span className="text-sm font-normal">/unit</span>}
-          </Typography>
-        </div>
-        <div style={{ paddingBottom: 16 }}>
-          <Rating
-            defaultValue={rating}
-            readOnly
-            size="medium"
-            precision={0.5}
-            style={{ color: '#00C853' }}
-            emptyIcon={<StarIcon fontSize="inherit" />}
-          />
-        </div>
-        <div style={{ paddingBottom: 16 }}>
-          <Typography variant="subtitle1" color="text.secondary" fontSize={16}>
-            {DateTime.fromISO(createdAt).toRelative({ locale: 'en-SG' })}
-          </Typography>
-        </div>
-      </CardContent>
-    </Card>
-  );
-};
+}: ProductListingItemProps) => (
+  <Card sx={{ maxWidth: 288, maxHeight: 600 }}>
+    <CardHeader
+      avatar={
+        <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+          {ownerFullName[0]}
+        </Avatar>
+      }
+      title={ownerFullName}
+      titleTypographyProps={{
+        fontSize: 16,
+        fontWeight: 'bold',
+      }}
+      subheader={companyName}
+    />
+    <CardMedia component="img" height="288" image={img} alt="Paella dish" />
+    <CardContent style={{ paddingLeft: 16 }}>
+      <div style={{ paddingBottom: 16 }}>
+        {type === 'Buy' && <BuyBadge />}
+        {type === 'Sell' && <SellBadge />}
+        {negotiable && <NegotiableBadge />}
+      </div>
+      <div style={{ paddingBottom: 16 }}>
+        <Typography variant="body2" color="text.primary" fontWeight={400} fontSize={20}>
+          {name}
+        </Typography>
+      </div>
+      <div style={{ paddingBottom: 16 }}>
+        <Typography variant="h4" color="text.primary" fontWeight="bold" fontSize={24}>
+          {new Intl.NumberFormat('en-SG', {
+            style: 'currency',
+            currency: 'SGD',
+          }).format(price)}
+          {isUnitPrice && <span className="text-sm font-normal">/unit</span>}
+        </Typography>
+      </div>
+      <div style={{ paddingBottom: 16 }}>
+        <Rating
+          defaultValue={rating}
+          readOnly
+          size="medium"
+          precision={0.5}
+          style={{ color: '#00C853' }}
+          emptyIcon={<StarIcon fontSize="inherit" />}
+        />
+      </div>
+      <div style={{ paddingBottom: 16 }}>
+        <Typography variant="subtitle1" color="text.secondary" fontSize={16}>
+          {DateTime.fromISO(createdAt).toRelative({ locale: 'en-SG' })}
+        </Typography>
+      </div>
+    </CardContent>
+  </Card>
+);
 
 export default ProductListingItem;
