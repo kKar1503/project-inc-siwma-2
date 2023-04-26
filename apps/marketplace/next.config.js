7e950e81303aa978ffe5c8e18da59302dcff36ea
Javier <JAVIERFOKJF11.21@ichat.sp.edu.sg>
2023-04-26 17:07:33 +0800

    refactor: editted package json dependencies and made requested changes

--- apps/marketplace/next.config.js
+++ apps/marketplace/next.config.js
@@ -1,5 +1,7 @@
 const { PrismaPlugin } = require('@prisma/nextjs-monorepo-workaround-plugin');
 
+/** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
   compiler: {
