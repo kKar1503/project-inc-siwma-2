c341ed735f5637082ed832f5b26339ff4d623b72
Cat <45095604+catcd1w3r5@users.noreply.github.com>
2023-04-27 12:07:58 +0800

    feat: implemented endpoint: DELETE /api/v1/advertisement/:id

--- apps/marketplace/src/pages/api/v1/advertisments/[id].tsx
+++ apps/marketplace/src/pages/api/v1/advertisments/[id].tsx
@@ -2,6 +2,7 @@ import {apiHandler, formatAPIResponse} from '@/utils/api';
 import {NextApiRequest, NextApiResponse} from "next";
 import PrismaClient from '@inc/db';
 import {NotFoundError} from '@/errors';
+import apiGuardMiddleware from "@/utils/api/server/middlewares/apiGuardMiddleware";
 
 const GET = async (req: NextApiRequest, res: NextApiResponse) => {
     const reqId = req.query.id as string;
@@ -29,10 +30,29 @@ const PUT = async (req: NextApiRequest, res: NextApiResponse) => {
 }
 
 const DELETE = async (req: NextApiRequest, res: NextApiResponse) => {
+    const reqId = req.query.id as string;
+    const id = parseInt(reqId, 10);
 
+    if (Number.isNaN(id)) {
+        throw new NotFoundError(`advertisement not found`);
+    }
+
+
+    await PrismaClient.advertisements.delete({
+        where: {
+            id,
+        },
+    });
+
+    res.status(204).end();
 }
 
-export default apiHandler()
-    .get(GET)
-    .put(PUT)
-    .delete(DELETE);
+export default apiHandler({
+    allowAdminsOnly: true,
+})
+    .get(apiGuardMiddleware({
+        allowNonAuthenticated: true,
+    }), GET)                   // no need admin    no need auth
+    .put(PUT)           // needs admin      needs auth
+    .delete(DELETE);    // needs admin      needs auth
+
