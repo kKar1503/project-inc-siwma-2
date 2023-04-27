1d5d0ad4c8a3dbc5e88f646317a9146b7267a656
Javier <JAVIERFOKJF11.21@ichat.sp.edu.sg>
2023-04-27 22:57:13 +0800

    refactor: edited import statements and rebased with dev

--- apps/marketplace/src/pages/api/v1/listings/index.ts
+++ apps/marketplace/src/pages/api/v1/listings/index.ts
@@ -1,5 +1,5 @@
 import { apiHandler, formatAPIResponse } from '@/utils/api';
-import PrismaClient from '@/utils/prisma';
+import PrismaClient from '@inc/db';
 import { z } from 'zod';
 import { apiGuardMiddleware } from '@/utils/api/server/middlewares/apiGuardMiddleware';
 
