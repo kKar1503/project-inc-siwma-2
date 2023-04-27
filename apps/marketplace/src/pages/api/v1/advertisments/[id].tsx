e2830b8da11a88b164d0736be48d320ae104d64a
Cat <45095604+catcd1w3r5@users.noreply.github.com>
2023-04-27 11:47:56 +0800

    feat: implemented endpoint: GET /api/v1/advertisement/:id

--- apps/marketplace/src/pages/api/v1/advertisments/[id].tsx
+++ apps/marketplace/src/pages/api/v1/advertisments/[id].tsx
@@ -1,8 +1,27 @@
-import {apiHandler} from '@/utils/api';
+import {apiHandler, formatAPIResponse} from '@/utils/api';
 import {NextApiRequest, NextApiResponse} from "next";
+import PrismaClient from '@inc/db';
+import {NotFoundError} from '@/errors';
 
 const GET = async (req: NextApiRequest, res: NextApiResponse) => {
+    const reqId = req.query.id as string;
+    const id = parseInt(reqId, 10);
 
+    if (Number.isNaN(id)) {
+        throw new NotFoundError(`advertisement not found`);
+    }
+
+    const advertisement = await PrismaClient.advertisements.findUnique({
+        where: {
+            id,
+        }
+    });
+
+    if (!advertisement) {
+        throw new NotFoundError(`advertisement not found`);
+    }
+
+    res.status(200).json(formatAPIResponse(advertisement));
 }
 
 const PUT = async (req: NextApiRequest, res: NextApiResponse) => {
