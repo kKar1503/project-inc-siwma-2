07142a443a637515ba93906b919911305659043e
Cat <45095604+catcd1w3r5@users.noreply.github.com>
2023-04-27 11:35:10 +0800

    feat: implemented endpoint: GET /api/v1/advertisement

--- apps/marketplace/src/pages/api/v1/advertisments/index.tsx
+++ apps/marketplace/src/pages/api/v1/advertisments/index.tsx
@@ -1,12 +1,13 @@
-import {apiHandler} from '@/utils/api';
+import {apiHandler, formatAPIResponse} from '@/utils/api';
 import {NextApiRequest, NextApiResponse} from "next";
-
+import PrismaClient from '@inc/db';
 const POST = async (req: NextApiRequest, res: NextApiResponse) => {
 
 }
 
 const GET = async (req: NextApiRequest, res: NextApiResponse) => {
-
+    const advertisements = await PrismaClient.advertisements.findMany();
+    res.status(200).json(formatAPIResponse(advertisements));
 }
 
 export default apiHandler()
