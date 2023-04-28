aa1b45aef6c70f4b2faa114a5ea8483144665fe0
Charmaine Choo <charmainechoo288@gmail.com>
2023-04-28 21:51:02 +0800

    refactor: :lipstick: minor ui update

--- apps/marketplace/src/components/marketplace/profile/ProfileDetailCard.tsx
+++ apps/marketplace/src/components/marketplace/profile/ProfileDetailCard.tsx
@@ -15,6 +15,7 @@ import WhatsAppIcon from '@mui/icons-material/WhatsApp';
 import StarIcon from '@mui/icons-material/Star';
 
 export type ProfileDetailCardProps = {
+  username: string;
   name: string;
   email: string;
   company: string;
@@ -24,9 +25,11 @@ export type ProfileDetailCardProps = {
   bio: string;
   rating: number;
   href: string;
+  reviews: string;
 };
 
 const ProfileDetailCard = ({
+  username,
   name,
   email,
   company,
@@ -36,100 +39,98 @@ const ProfileDetailCard = ({
   bio,
   rating,
   href,
-}: ProfileDetailCardProps) => {
-  return (
-    <Card sx={{ width: 300, display: 'inline-grid' }}>
-      <CardHeader
-        titleTypographyProps={{
-          fontSize: 18,
-        }}
-        subheaderTypographyProps={{
-          fontSize: 13,
-        }}
-        title="Your Profile"
-        subheader="View your profile details here"
-      />
-      <Divider variant="middle" />
-      <CardContent>
-        <Avatar sx={{ mb: 1.5 }}>{profilePic}</Avatar>
-        <Typography variant="body1" sx={{ fontWeight: 'bold' }}>
-          {name}
-        </Typography>
-        {/* admin only */}
-        <Typography variant="body2" color="text.secondary" sx={{ mt: 1 }}>
-          {company}
-        </Typography>
-        <Typography variant="body2" sx={{ mt: 1 }}>
-          @account_username
-        </Typography>
-        <Typography variant="body2" sx={{ mt: 1 }}>
-          {email}
-        </Typography>
+  reviews,
+}: ProfileDetailCardProps) => (
+  <Card sx={{ width: 300, display: 'inline-grid' }}>
+    <CardHeader
+      titleTypographyProps={{
+        fontSize: 18,
+      }}
+      subheaderTypographyProps={{
+        fontSize: 13,
+      }}
+      title="Your Profile"
+      subheader="View your profile details here"
+    />
+    <Divider variant="middle" />
+    <CardContent>
+      <Avatar sx={{ mb: 1.5 }}>{profilePic}</Avatar>
+      <Typography variant="body1" sx={{ fontWeight: 'bold' }}>
+        {name}
+      </Typography>
+      {/* admin only */}
+      <Typography variant="body2" color="text.secondary">
+        {company}
+      </Typography>
+      <Typography variant="body2">@{username}</Typography>
+      <Typography variant="body2">{email}</Typography>
 
-        <Box
-          sx={{
-            mt: 1,
-            display: 'flex',
-            alignItems: 'center',
-          }}
-        >
-          <Typography variant="body1" sx={{ mr: 1, paddingTop: 0.5 }}>
-            {rating}
-          </Typography>
-          <Rating
-            readOnly
-            defaultValue={rating}
-            size="medium"
-            sx={{ color: '#00C853' }}
-            emptyIcon={<StarIcon fontSize="inherit" />}
-          />
-          <Typography variant="body1" sx={{ ml: 1, paddingTop: 0.5 }}>
-            180 Reviews
-          </Typography>
-        </Box>
-      </CardContent>
+      <Box
+        sx={{
+          mt: 1,
+          display: 'flex',
+          alignItems: 'center',
+        }}
+      >
+        <Typography variant="body1" sx={{ mr: 1, paddingTop: 0.5 }}>
+          {rating}
+        </Typography>
+        <Rating
+          readOnly
+          defaultValue={rating}
+          size="medium"
+          sx={{ color: '#00C853' }}
+          emptyIcon={<StarIcon fontSize="inherit" />}
+          precision={0.5}
+        />
+        <Typography variant="body1" sx={{ ml: 1, paddingTop: 0.5 }}>
+          {reviews} Reviews
+        </Typography>
+      </Box>
+    </CardContent>
 
-      <Divider variant="middle" />
-      <CardContent>
-        <Typography sx={{ fontWeight: 'bold' }}>Bio:</Typography>
-        <Typography>{bio}</Typography>
-      </CardContent>
-      <Divider variant="middle" />
-      <CardContent>
-        <Typography sx={{ fontWeight: 'bold' }}>Linked accounts:</Typography>
-        <Box
-          sx={{
-            mt: 1,
-            display: 'flex',
-            alignItems: 'center',
-          }}
-        >
-          <TelegramIcon
-            sx={{ backgroundColor: '#0088cc', color: 'white', borderRadius: 5, p: 0.5 }}
-          />
-          <Typography sx={{ ml: 1 }}>{contactMethod}</Typography>
-        </Box>
-        <Box
-          sx={{
-            mt: 1,
-            display: 'flex',
-            alignItems: 'center',
-          }}
-        >
-          <WhatsAppIcon
-            sx={{ backgroundColor: '#25D366', color: 'white', borderRadius: 5, p: 0.5 }}
-          />
-          <Typography sx={{ ml: 1 }}> +65 {mobileNumber} </Typography>
-        </Box>
-      </CardContent>
-      <CardActions sx={{ flexDirection: 'column' }}>
+    <Divider variant="middle" />
+    <CardContent>
+      <Typography sx={{ fontWeight: 'bold' }}>Bio:</Typography>
+      <Typography>{bio}</Typography>
+    </CardContent>
+    <Divider variant="middle" />
+    <CardContent>
+      <Typography sx={{ fontWeight: 'bold' }}>Linked accounts:</Typography>
+      <Box
+        sx={{
+          mt: 1,
+          display: 'flex',
+          alignItems: 'center',
+        }}
+      >
+        <TelegramIcon
+          sx={{ backgroundColor: '#0088cc', color: 'white', borderRadius: 5, p: 0.5 }}
+        />
+        <Typography sx={{ ml: 1 }}>{contactMethod}</Typography>
+      </Box>
+      <Box
+        sx={{
+          mt: 1,
+          display: 'flex',
+          alignItems: 'center',
+        }}
+      >
+        <WhatsAppIcon
+          sx={{ backgroundColor: '#25D366', color: 'white', borderRadius: 5, p: 0.5 }}
+        />
+        <Typography sx={{ ml: 1 }}> +65 {mobileNumber} </Typography>
+      </Box>
+    </CardContent>
+    <CardActions sx={{ flexDirection: 'column' }}>
+      <Box sx={{ width: '98%' }}>
         <Button
           component={Link}
           href="/editprofile"
           variant="contained"
           type="submit"
           color="primary"
-          sx={{ width: '98%', mb: 2, mt: 10, fontWeight: 'bold' }}
+          sx={{ width: '100%', mb: 2, mt: 10, fontWeight: 'bold' }}
         >
           Edit profile
         </Button>
@@ -139,13 +140,12 @@ const ProfileDetailCard = ({
           variant="contained"
           type="submit"
           color="error"
-          sx={{ width: '98%', mb: 2, fontWeight: 'bold' }}
+          sx={{ width: '100%', mb: 2, fontWeight: 'bold' }}
         >
           Logout
         </Button>
-      </CardActions>
-    </Card>
-  );
-};
-
+      </Box>
+    </CardActions>
+  </Card>
+);
 export default ProfileDetailCard;
