62c2f7f97ec2dfc6a44f6a0779a7acb29e1168a0
Hein Htet Aung <hhaung5142@gmail.com>
2023-04-30 02:14:55 +0800

    FIlter form component

---
+++ apps/marketplace/src/components/marketplace/FilterForm.tsx
@@ -0,0 +1,36 @@
+import * as Mui from '@mui/material';
+
+const FilterForm = () => {
+  return (
+    <Mui.FormControl sx={{ p: 1, mt: 2 }}>
+      <Mui.Divider sx={{ my: 2 }} />
+      <Mui.FormLabel sx={{ fontWeight: 600 }}>Negotiability</Mui.FormLabel>
+      <Mui.RadioGroup>
+        <Mui.FormControlLabel value="negotiable" control={<Mui.Radio />} label="Negotiable" />
+        <Mui.FormControlLabel
+          value="non-negotiable"
+          control={<Mui.Radio />}
+          label="Non-Negotiable"
+        />
+      </Mui.RadioGroup>
+      <Mui.Divider sx={{ my: 2 }} />
+
+      <Mui.FormLabel sx={{ fontWeight: 600 }}>Price</Mui.FormLabel>
+      <div style={{ display: 'flex', marginBottom: 2 }}>
+        <Mui.TextField id="standard-basic min" label="Min" variant="standard" sx={{ mr: 2 }} />
+        <Mui.TextField id="standard-basic max" label="Max" variant="standard" />
+      </div>
+
+      <Mui.Divider sx={{ my: 2 }} />
+      <Mui.FormLabel sx={{ fontWeight: 600 }}>Condition</Mui.FormLabel>
+      <Mui.Select sx={{ height: '45px' }}>
+        <Mui.MenuItem value={1}>1</Mui.MenuItem>
+      </Mui.Select>
+
+      <Mui.Divider sx={{ my: 2 }} />
+      <Mui.Button variant="contained">APPLY</Mui.Button>
+    </Mui.FormControl>
+  );
+};
+
+export default FilterForm;
