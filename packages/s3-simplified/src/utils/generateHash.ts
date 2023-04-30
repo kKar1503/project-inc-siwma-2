e4597bb913248a247c9bd06c4421b103ad945e22
Cat <45095604+catcd1w3r5@users.noreply.github.com>
2023-05-01 03:02:44 +0800

    fix: reverted from hash back to uuid as it introduction unnecessary complications.

--- packages/s3-simplified/src/utils/generateHash.ts
+++ packages/s3-simplified/src/utils/generateHash.ts
@@ -2,7 +2,10 @@ import crypto from 'crypto';
 
 
 export const generateHash = (buffer:Buffer): string => {
-    const hash = crypto.createHash('sha512');
-    hash.update(buffer);
-    return hash.digest('hex');
+    // const hash = crypto.createHash('sha512');
+    // hash.update(buffer);
+    // return hash.digest('hex');
+
+    // generate uuid instead
+    return crypto.randomUUID();
 }
