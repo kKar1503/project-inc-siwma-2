d4aee7569d714545e836a9c935d4afba6a32f487
Karandeep Singh <46585033+ConfuseIous@users.noreply.github.com>
2023-04-30 20:20:59 +0800

    feat: delete invite by email

---
+++ apps/marketplace/src/pages/api/v1/invites/email/[email]/index.ts
@@ -0,0 +1,33 @@
+import { apiHandler, formatAPIResponse } from '@/utils/api';
+import { z } from 'zod';
+import client from '@inc/db';
+
+const emailSchema = z.object({
+  email: z.string(),
+});
+
+export default apiHandler({
+  allowNonAuthenticated: true,
+}).delete(async (req, res) => {
+  const parsedBody = emailSchema.safeParse(req.query);
+
+  if (!parsedBody.success) {
+    return res
+      .status(422)
+      .json(formatAPIResponse({ status: '422', detail: 'invalid request body' }));
+  }
+
+  const { email } = parsedBody.data;
+
+  const invite = await client.invite.deleteMany({
+    where: {
+      email,
+    },
+  });
+
+  if (!invite) {
+    return res.status(404).json(formatAPIResponse({ status: '404', detail: 'invite not found' }));
+  }
+
+  return res.status(200).json(formatAPIResponse({ status: '200', data: invite }));
+});
