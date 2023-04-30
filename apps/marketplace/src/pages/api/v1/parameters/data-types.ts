0b159c1aa18fcd52d3ab56aa5b05fbec2fda5d38
Xavier Teo <xavierteo26@gmail.com>
2023-04-30 22:36:57 +0800

    fix: :bug: parameter endpoints were broken
    - it was changed from snake case to camel case because the prisma schema was changed to camel case but after the schema was changed back to snake case the code was left as camel case and hence it is broken this is the fix

--- apps/marketplace/src/pages/api/v1/parameters/data-types.ts
+++ apps/marketplace/src/pages/api/v1/parameters/data-types.ts
@@ -1,9 +1,9 @@
 import { apiHandler, formatAPIResponse } from '@/utils/api';
-import { DataType } from '@inc/db';
+import { datatype } from '@inc/db';
 
 export default apiHandler().get(async (req, res) => {
   // Retrieve all parameter data types from the database
-  const dataTypes = await Object.keys(DataType);
+  const dataTypes = await Object.keys(datatype);
 
   // Return the result
   res.status(200).json(formatAPIResponse(dataTypes));
