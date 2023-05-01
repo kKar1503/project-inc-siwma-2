f9cde644166daf3f9c0e39e65cd9933cdf81030d
Adeeb <adityaadeeb04@gmail.com>
2023-05-01 13:08:00 +0800

    formatting

--- apps/marketplace/src/pages/api/v1/listings/[id]/index.ts
+++ apps/marketplace/src/pages/api/v1/listings/[id]/index.ts
@@ -6,23 +6,23 @@ import { apiGuardMiddleware } from '@/utils/api/server/middlewares/apiGuardMiddl
 
 // -- Functions --//
 function parseListingId($id: string | string[] | undefined): number {
-  if (Array.isArray($id)) {
-    $id = $id[0];
-  }
+    if (Array.isArray($id)) {
+        $id = $id[0];
+    }
 
-  if (!$id) {
-    throw new NotFoundError(`Listing id not provided`);
-  }
+    if (!$id) {
+        throw new NotFoundError(`Listing id not provided`);
+    }
 
-  // Parse and validate listing id provided
-  const id = parseInt($id, 10);
+    // Parse and validate listing id provided
+    const id = parseInt($id, 10);
 
-  // Check if the listing id is valid
-  if (Number.isNaN(id)) {
-    throw new NotFoundError(`Listing with id '${id}'`);
-  }
+    // Check if the listing id is valid
+    if (Number.isNaN(id)) {
+        throw new NotFoundError(`Listing with id '${id}'`);
+    }
 
-  return id;
+    return id;
 }
 
 /**
@@ -31,72 +31,72 @@ function parseListingId($id: string | string[] | undefined): number {
  * @returns The listing if it exists
  */
 async function checkListingExists($id: string | number) {
-  // Parse and validate listing id provided
-  const id = typeof $id === 'number' ? $id : parseListingId($id);
+    // Parse and validate listing id provided
+    const id = typeof $id === 'number' ? $id : parseListingId($id);
 
-  // Check if the listing exists
-  const listing = await PrismaClient.listing.findUnique({
-    where: { id: id },
-    include: {
-      users: {
-        select: {
-          company_id: true,
+    // Check if the listing exists
+    const listing = await PrismaClient.listing.findUnique({
+        where: { id: id },
+        include: {
+            users: {
+                select: {
+                    company_id: true,
+                },
+            },
+            listings_parameters_value: true, // add this line
         },
-      },
-      listings_parameters_value: true, // add this line
-    },
-  });
+    });
 
-  // Check if the listing exists
-  if (!listing) {
-    throw new NotFoundError(`Listing with id '${id}'`);
-  }
+    // Check if the listing exists
+    if (!listing) {
+        throw new NotFoundError(`Listing with id '${id}'`);
+    }
 
-  return listing;
+    return listing;
 }
 
 export default apiHandler()
-  .get(async (req, res) => {
-    // Retrieve the listing from the database
-    const id = parseListingId(req.query.id);
-    const listing = await checkListingExists(id);
+    .get(async (req, res) => {
+        // Retrieve the listing from the database
+        const id = parseListingId(req.query.id);
+        const listing = await checkListingExists(id);
 
-    // Return the result
-    res.status(200).json(formatAPIResponse(formatSingleListingResponse(listing)));
-  })
-  .delete(apiGuardMiddleware(), async (req, res) => {
-    const id = parseListingId(req.query.id);
-    const userId = req.token?.user?.id;
-    const userRole = req.token?.user?.role;
+        // Return the result
+        res.status(200).json(formatAPIResponse(formatSingleListingResponse(listing)));
+    })
+    .delete(apiGuardMiddleware(), async (req, res) => {
+        const id = parseListingId(req.query.id);
+        const userId = req.token?.user?.id;
+        const userRole = req.token?.user?.role;
 
-    try {
-      const listing = await checkListingExists(id);
+        try {
+            const listing = await checkListingExists(id);
 
-      const isOwner = listing.owner === userId;
-      const isAdmin = userRole && userRole >= 1;
-      const sameCompany = req.token?.user?.company === listing.users.company_id;
+            const isOwner = listing.owner === userId;
+            const isAdmin = userRole && userRole >= 1;
+            const sameCompany = req.token?.user?.company === listing.users.company_id;
 
-      if (!isOwner && !isAdmin && !sameCompany) {
-        return res.status(403).json({
-          errors: [{ status: 403, detail: 'Forbidden' }],
-        });
-      }
+            if (!isOwner && !isAdmin && !sameCompany) {
+                return res.status(403).json({
+                    errors: [{ status: 403, detail: 'Forbidden' }],
+                });
+            }
 
-      await PrismaClient.listing.delete({
-        where: { id },
-      });
+            await PrismaClient.listing.delete({
+                where: { id },
+            });
 
-      res.status(204).end();
-    } catch (error) {
-      if (error instanceof NotFoundError) {
-        res.status(404).json({
-          errors: [{ status: 404, detail: error.message }],
-        });
-      } else {
-        console.error('Error deleting listing:', error);
-        res.status(500).json({
-          errors: [{ status: 500, detail: 'Internal server error' }],
-        });
-      }
-    }
-  });
+            res.status(204).end();
+        } catch (error) {
+            if (error instanceof NotFoundError) {
+                res.status(404).json({
+                    errors: [{ status: 404, detail: error.message }],
+                });
+            } else {
+                console.error('Error deleting listing:', error);
+                res.status(500).json({
+                    errors: [{ status: 500, detail: 'Internal server error' }],
+                });
+            }
+        }
+    });
