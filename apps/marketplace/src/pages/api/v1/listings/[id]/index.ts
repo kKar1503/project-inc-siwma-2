be6788ba07998d5e045c4c6245706ecc43d79eea
Javier <JAVIERFOKJF11.21@ichat.sp.edu.sg>
2023-05-01 12:59:48 +0800

    adding changes for pr

--- apps/marketplace/src/pages/api/v1/listings/[id]/index.ts
+++ apps/marketplace/src/pages/api/v1/listings/[id]/index.ts
@@ -1,20 +1,28 @@
-import { apiHandler } from '@/utils/api';
+import { apiHandler, formatAPIResponse } from '@/utils/api';
 import PrismaClient from '@inc/db';
 import { NotFoundError } from '@/errors';
-import { formatListingResponse, listingsRequestBody } from '..';
+import { formatListingResponse, listingsRequestBody, formatSingleListingResponse } from '..';
 import { apiGuardMiddleware } from '@/utils/api/server/middlewares/apiGuardMiddleware';
 
 // -- Functions --//
-function parseListingId($id: string) {
-    // Parse and validate listing id provided
-    const id = parseInt($id, 10);
+function parseListingId($id: string | string[] | undefined): number {
+  if (Array.isArray($id)) {
+    $id = $id[0];
+  }
 
-    // Check if the listing id is valid
-    if (Number.isNaN(id)) {
-        throw new NotFoundError(`Listing with id '${id}'`);
-    }
+  if (!$id) {
+    throw new NotFoundError(`Listing id not provided`);
+  }
 
-    return id;
+  // Parse and validate listing id provided
+  const id = parseInt($id, 10);
+
+  // Check if the listing id is valid
+  if (Number.isNaN(id)) {
+    throw new NotFoundError(`Listing with id '${id}'`);
+  }
+
+  return id;
 }
 
 /**
@@ -23,54 +31,72 @@ function parseListingId($id: string) {
  * @returns The listing if it exists
  */
 async function checkListingExists($id: string | number) {
-    // Parse and validate listing id provided
-    const id = typeof $id === 'number' ? $id : parseListingId($id);
+  // Parse and validate listing id provided
+  const id = typeof $id === 'number' ? $id : parseListingId($id);
 
-    // Check if the listing exists
-    const listing = await PrismaClient.listing.findUnique({
-        where: {
-            id,
+  // Check if the listing exists
+  const listing = await PrismaClient.listing.findUnique({
+    where: { id: id },
+    include: {
+      users: {
+        select: {
+          company_id: true,
         },
-    });
+      },
+      listings_parameters_value: true, // add this line
+    },
+  });
 
-    // Check if the listing exists
-    if (!listing) {
-        throw new NotFoundError(`Listing with id '${id}'`);
-    }
+  // Check if the listing exists
+  if (!listing) {
+    throw new NotFoundError(`Listing with id '${id}'`);
+  }
 
-    return listing;
+  return listing;
 }
 
 export default apiHandler()
-    .get(async (req, res) => {
-        // Retrieve the listing from the database
-        const id = parseListingId(req.query.id as string);
-        const listing = await checkListingExists(id);
+  .get(async (req, res) => {
+    // Retrieve the listing from the database
+    const id = parseListingId(req.query.id);
+    const listing = await checkListingExists(id);
 
-        // Return the result
-        res.status(200).json(formatListingResponse(listing));
-    })
+    // Return the result
+    res.status(200).json(formatAPIResponse(formatSingleListingResponse(listing)));
+  })
+  .delete(apiGuardMiddleware(), async (req, res) => {
+    const id = parseListingId(req.query.id);
+    const userId = req.token?.user?.id;
+    const userRole = req.token?.user?.role;
 
-    .delete(async (req, res) => {
-        apiGuardMiddleware({
-            allowNonAuthenticated: true,
-        })
+    try {
+      const listing = await checkListingExists(id);
 
-        // Deletes a listing
-        // Parse and validate listing id provided
-        const id = parseListingId(req.query.id as string);
+      const isOwner = listing.owner === userId;
+      const isAdmin = userRole && userRole >= 1;
+      const sameCompany = req.token?.user?.company === listing.users.company_id;
 
-
-        // // Check if the listing exists
-        // await checkListingExists(id);
-
-        // Delete the listing from the database
-        await PrismaClient.listing.delete({
-            where: {
-                id,
-            },
+      if (!isOwner && !isAdmin && !sameCompany) {
+        return res.status(403).json({
+          errors: [{ status: 403, detail: 'Forbidden' }],
         });
+      }
 
-        // Return 204 no content
-        res.status(204).end();
-    });
+      await PrismaClient.listing.delete({
+        where: { id },
+      });
+
+      res.status(204).end();
+    } catch (error) {
+      if (error instanceof NotFoundError) {
+        res.status(404).json({
+          errors: [{ status: 404, detail: error.message }],
+        });
+      } else {
+        console.error('Error deleting listing:', error);
+        res.status(500).json({
+          errors: [{ status: 500, detail: 'Internal server error' }],
+        });
+      }
+    }
+  });
--- apps/marketplace/src/pages/api/v1/listings/index.ts
+++ apps/marketplace/src/pages/api/v1/listings/index.ts
@@ -6,141 +6,162 @@ import { Decimal } from '@prisma/client/runtime';
 
 // -- Type definitions -- //
 export type ListingResponse = {
-    id: string;
-    name: string;
-    description: string;
-    price: Decimal;
-    unitPrice?: boolean;
-    negotiable?: boolean;
-    categoryId: string;
-    type: listingtype;
-    active: boolean;
-    owner: string;
-    parameters?: Array<{
-        paramId: string;
-        value: string;
-    }>;
+  id: string;
+  name: string;
+  description: string;
+  price: Decimal;
+  unitPrice?: boolean;
+  negotiable?: boolean;
+  categoryId: string;
+  type: listingtype;
+  active: boolean;
+  owner: string;
+  parameters?: Array<{
+    paramId: string;
+    value: string;
+  }>;
 };
 
-type ListingWithParameters = listing & {
-    listings_parameters_value: Array<{
-        parameter_id: number;
-        value: string;
-    }>;
+export type ListingWithParameters = listing & {
+  listings_parameters_value: Array<{
+    parameter_id: number;
+    value: string;
+  }>;
 };
 
 // -- Helper functions -- //
 export function formatListingResponse($listings: ListingWithParameters[]) {
-    return $listings.map((listing) => ({
-        id: listing.id.toString(),
-        name: listing.name,
-        description: listing.description,
-        price: listing.price,
-        unitPrice: listing.unit_price,
-        negotiable: listing.negotiable,
-        categoryId: listing.category_id.toString(),
-        listingType: listing.type,
-        owner: listing.owner,
-        active: listing.active,
-        parameters: listing.listings_parameters_value
-            ? listing.listings_parameters_value.map((parameter) => ({
-                paramId: parameter.parameter_id.toString(),
-                value: parameter.value,
-            }))
-            : undefined,
-    }));
+  return $listings.map((listing) => ({
+    id: listing.id.toString(),
+    name: listing.name,
+    description: listing.description,
+    price: listing.price,
+    unitPrice: listing.unit_price,
+    negotiable: listing.negotiable,
+    categoryId: listing.category_id.toString(),
+    listingType: listing.type,
+    owner: listing.owner,
+    active: listing.active,
+    parameters: listing.listings_parameters_value
+      ? listing.listings_parameters_value.map((parameter) => ({
+          paramId: parameter.parameter_id.toString(),
+          value: parameter.value,
+        }))
+      : undefined,
+  }));
+}
+
+export function formatSingleListingResponse(listing: ListingWithParameters): ListingResponse {
+  return {
+    id: listing.id.toString(),
+    name: listing.name,
+    description: listing.description,
+    price: listing.price,
+    unitPrice: listing.unit_price,
+    negotiable: listing.negotiable,
+    categoryId: listing.category_id.toString(),
+    type: listing.type,
+    owner: listing.owner,
+    active: listing.active,
+    parameters: listing.listings_parameters_value
+      ? listing.listings_parameters_value.map((parameter) => ({
+          paramId: parameter.parameter_id.toString(),
+          value: parameter.value,
+        }))
+      : undefined,
+  };
 }
 
 export const listingsRequestBody = z.object({
-    name: z.string(),
-    description: z.string(),
-    price: z.number(),
-    unitPrice: z.boolean().optional(),
-    negotiable: z.boolean().optional(),
-    categoryId: z.number(),
-    type: z.nativeEnum(listingtype),
-    parameters: z
-        .array(
-            z.object({
-                paramId: z.string(),
-                value: z.string(),
-            })
-        )
-        .optional(),
+  name: z.string(),
+  description: z.string(),
+  price: z.number(),
+  unitPrice: z.boolean().optional(),
+  negotiable: z.boolean().optional(),
+  categoryId: z.number(),
+  type: z.nativeEnum(listingtype),
+  parameters: z
+    .array(
+      z.object({
+        paramId: z.string(),
+        value: z.string(),
+      })
+    )
+    .optional(),
 });
 
 export default apiHandler()
-    .get(apiGuardMiddleware(), async (req, res) => {
-        const listings = await PrismaClient.listing.findMany({
-            include: {
-                listings_parameters_value: true,
-            },
-        });
-
-        res.status(200).json(formatAPIResponse(formatListingResponse(listings)));
-    })
-    .post(apiGuardMiddleware(), async (req, res) => {
-        try {
-            const data = listingsRequestBody.parse(req.body);
-            console.log('Parsed request body:', data);
-
-            // Use the user ID from the request object
-            const userId = req.token?.user?.id;
-
-            // Check if the category exists
-            const categoryExists = await PrismaClient.category.findUnique({
-                where: { id: data.categoryId },
-            });
-
-            if (!categoryExists) {
-                return res.status(422).json({
-                    errors: [{ status: 422, detail: 'Category not found' }],
-                });
-            }
-
-            const listing = await PrismaClient.listing.create({
-                data: {
-                    name: data.name,
-                    description: data.description,
-                    price: data.price,
-                    unit_price: data.unitPrice,
-                    negotiable: data.negotiable,
-                    category_id: data.categoryId,
-                    type: data.type,
-                    active: true,
-                    owner: userId,
-                    listings_parameters_value: data.parameters
-                        ? {
-                            create: data.parameters.map((parameter) => ({
-                                value: parameter.value,
-                                parameter: {
-                                    connect: {
-                                        id: parseInt(parameter.paramId),
-                                    },
-                                },
-                            })),
-                        }
-                        : undefined,
-                },
-                include: {
-                    listings_parameters_value: true,
-                },
-            });
-            console.log('Created listing:', listing);
-
-            res.status(201).json({ listingId: listing.id });
-        } catch (error) {
-            console.error('Error creating listing:', error);
-
-            let detail;
-            if (error instanceof z.ZodError) {
-                detail = error.issues[0].path.join('/');
-            } else {
-                detail = 'Invalid parameter supplied';
-            }
-
-            res.status(422).json({
-                errors: [{ status: 422, detail }],
-            });
-        }
+  .get(apiGuardMiddleware(), async (req, res) => {
+    const listings = await PrismaClient.listing.findMany({
+      include: {
+        listings_parameters_value: true,
+      },
     });
+
+    res.status(200).json(formatAPIResponse(formatListingResponse(listings)));
+  })
+  .post(apiGuardMiddleware(), async (req, res) => {
+    try {
+      const data = listingsRequestBody.parse(req.body);
+      console.log('Parsed request body:', data);
+
+      // Use the user ID from the request object
+      const userId = req.token?.user?.id;
+
+      // Check if the category exists
+      const categoryExists = await PrismaClient.category.findUnique({
+        where: { id: data.categoryId },
+      });
+
+      if (!categoryExists) {
+        return res.status(422).json({
+          errors: [{ status: 422, detail: 'Category not found' }],
+        });
+      }
+
+      const listing = await PrismaClient.listing.create({
+        data: {
+          name: data.name,
+          description: data.description,
+          price: data.price,
+          unit_price: data.unitPrice,
+          negotiable: data.negotiable,
+          category_id: data.categoryId,
+          type: data.type,
+          active: true,
+          owner: userId,
+          listings_parameters_value: data.parameters
+            ? {
+                create: data.parameters.map((parameter) => ({
+                  value: parameter.value,
+                  parameter: {
+                    connect: {
+                      id: parseInt(parameter.paramId),
+                    },
+                  },
+                })),
+              }
+            : undefined,
+        },
+        include: {
+          listings_parameters_value: true,
+        },
+      });
+      console.log('Created listing:', listing);
+
+      res.status(201).json({ listingId: listing.id });
+    } catch (error) {
+      console.error('Error creating listing:', error);
+
+      let detail;
+      if (error instanceof z.ZodError) {
+        detail = error.issues[0].path.join('/');
+      } else {
+        detail = 'Invalid parameter supplied';
+      }
+
+      res.status(422).json({
+        errors: [{ status: 422, detail }],
+      });
+    }
+  });
