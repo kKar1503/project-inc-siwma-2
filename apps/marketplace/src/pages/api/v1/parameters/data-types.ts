181e0c0066b38c9629cac55ee92988ce43f6c895
Karandeep Singh <46585033+ConfuseIous@users.noreply.github.com>
2023-05-01 16:52:44 +0800

    resolve merge conflicts

--- apps/marketplace/src/pages/api/v1/parameters/data-types.ts
+++ apps/marketplace/src/pages/api/v1/parameters/data-types.ts
@@ -1,10 +1,10 @@
 import { apiHandler, formatAPIResponse } from '@/utils/api';
-import { datatype } from '@inc/db';
 import { NextApiRequest, NextApiResponse } from 'next';
+import { DataType } from '@inc/db';
 
 export default apiHandler().get(async (req: NextApiRequest, res: NextApiResponse) => {
   // Retrieve all parameter data types from the database
-  const dataTypes = await Object.keys(datatype);
+  const dataTypes = await Object.keys(DataType);
 
   // Return the result
   res.status(200).json(formatAPIResponse(dataTypes));
