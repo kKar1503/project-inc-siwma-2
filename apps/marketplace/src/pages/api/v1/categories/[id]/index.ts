c84fd2ee308eac59f742293711b9a941d2d94e23
Kaung Khant Zaw <mmmike2002@gmail.com>
2023-05-03 11:16:51 +0800

    feat: :sparkles: GET,PUT, DELETE /api/v1/categories/[id]

--- apps/marketplace/src/pages/api/v1/categories/[id]/index.ts
+++ apps/marketplace/src/pages/api/v1/categories/[id]/index.ts
@@ -1,54 +1,139 @@
 import { apiHandler, formatAPIResponse, parseToNumber } from '@/utils/api';
-import { NextApiRequest, NextApiResponse } from 'next';
 import { z } from 'zod';
 import PrismaClient from '@inc/db';
-import { NotFoundError } from '@/errors';
+import { NotFoundError, ParamError } from '@/errors';
+import { apiGuardMiddleware } from '@/utils/api/server/middlewares/apiGuardMiddleware';
+import { getCategoriesRequestBody, getResponse } from '../index';
 
-const getCategoryRequestBody = z.object({
-  includeParameters: z.boolean().optional(),
+const editCategoryRequestBody = z.object({
+  name: z.string().optional(),
+  description: z.string().optional(),
+  image: z.string().optional(),
+  crossSectionImage: z.string().optional(),
+  parameters: z
+    .object({
+      parameterId: z.number(),
+      required: z.boolean(),
+    })
+    .array()
+    .optional(),
 });
 
-function parseId(id: any): number {
-  if (id && typeof id === 'string') {
+function parseId(id: string | undefined): number {
+  if (id) {
     return parseToNumber(id);
   }
   return 0;
 }
 
 export default apiHandler()
-  .get(async (req: NextApiRequest, res: NextApiResponse) => {
+  .get(async (req, res) => {
     const { id } = req.query;
-    const { includeParameters } = getCategoryRequestBody.parse(req.body);
+    const { includeParameters = false } = getCategoriesRequestBody.parse(req.body);
 
-    const response = await PrismaClient.category.findFirst({
+    const response: getResponse | null = await PrismaClient.category.findFirst({
       where: {
-        id: parseId(id),
+        id: parseId(id as string),
       },
-      include: {
+      select: {
+        name: true,
+        description: true,
+        image: true,
+        crossSectionImage: true,
+        createdAt: false,
+        updatedAt: false,
         categoriesParameters: includeParameters,
       },
     });
 
     if (!response) {
-      throw new NotFoundError('category');
+      throw new NotFoundError('Category');
+    }
+
+    // delete unnecessary properties from paramters array
+    /* eslint-disable no-param-reassign */
+    if (response.categoriesParameters) {
+      response.categoriesParameters.forEach((parameter) => {
+        delete parameter.categoryId;
+        delete parameter.createdAt;
+        delete parameter.updatedAt;
+      });
     }
 
     res.status(200).json(formatAPIResponse(response));
   })
-  .put(async (req: NextApiRequest, res: NextApiResponse) => {
-    const { id } = req.query;
-  })
-  .delete(async (req: NextApiRequest, res: NextApiResponse) => {
+  .put(apiGuardMiddleware({ allowAdminsOnly: true }), async (req, res) => {
     const { id } = req.query;
 
-    const response = await PrismaClient.category.delete({
+    const { name, description, image, crossSectionImage, parameters } =
+      editCategoryRequestBody.parse(req.body);
+
+    if (name === '') {
+      throw new ParamError('name');
+    }
+
+    if (parameters) {
+      parameters.forEach(async (parameter) => {
+        const response2 = await PrismaClient.categoriesParameters.update({
+          where: {
+            categoryId_parameterId: {
+              categoryId: parseId(id as string),
+              parameterId: parameter.parameterId,
+            },
+          },
+          data: {
+            parameterId: parameter.parameterId,
+            required: parameter.required,
+          },
+        });
+      });
+    }
+
+    const response = await PrismaClient.category.update({
       where: {
-        id: parseId(id),
+        id: parseId(id as string),
+      },
+      data: {
+        name,
+        description,
+        image,
+        crossSectionImage,
+      },
+      select: {
+        name: true,
+        description: true,
+        image: true,
+        crossSectionImage: true,
+        createdAt: false,
+        updatedAt: false,
+        categoriesParameters: {
+          select: {
+            parameterId: true,
+            required: true,
+            createdAt: false,
+            updatedAt: false,
+          },
+        },
       },
     });
 
     if (!response) {
-      throw new NotFoundError('category');
+      throw new NotFoundError('Category');
+    }
+
+    res.status(200).json(formatAPIResponse(response));
+  })
+  .delete(apiGuardMiddleware({ allowAdminsOnly: true }), async (req, res) => {
+    const { id } = req.query;
+
+    const response = await PrismaClient.category.delete({
+      where: {
+        id: parseId(id as string),
+      },
+    });
+
+    if (!response) {
+      throw new NotFoundError('Category');
     }
 
     res.status(204).end();
