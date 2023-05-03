87d529e5192c342426770886035b634d5f381068
Kaung Khant Zaw <mmmike2002@gmail.com>
2023-05-03 11:16:13 +0800

    feat: :sparkles: GET, POST /api/v1/categories

--- apps/marketplace/src/pages/api/v1/categories/index.ts
+++ apps/marketplace/src/pages/api/v1/categories/index.ts
@@ -1,40 +1,91 @@
 import { apiHandler, formatAPIResponse } from '@/utils/api';
-import { NextApiRequest, NextApiResponse } from 'next';
 import { z } from 'zod';
 import PrismaClient from '@inc/db';
+import { apiGuardMiddleware } from '@/utils/api/server/middlewares/apiGuardMiddleware';
+import { ParamError } from '@/errors';
+
+export type getResponse = {
+  id?: number;
+  name: string;
+  description: string;
+  image: string;
+  crossSectionImage: string;
+  categoriesParameters?: {
+    categoryId?: number;
+    parameterId: number;
+    required: boolean;
+    createdAt?: Date;
+    updatedAt?: Date;
+  }[];
+};
 
 export const getCategoriesRequestBody = z.object({
   includeParameters: z.boolean().optional(),
 });
 
-const createCategoryRequestBody = z.object({
+export const categoryRequestBody = z.object({
   name: z.string(),
+  description: z.string(),
+  image: z.string(),
+  crossSectionImage: z.string(),
   parameters: z
     .object({
-      parameterId: z.string(),
+      parameterId: z.number(),
       required: z.boolean(),
     })
     .array(),
 });
 
-export default apiHandler({ allowNonAuthenticated: false })
-  .get(async (req: NextApiRequest, res: NextApiResponse) => {
+export default apiHandler()
+  .get(async (req, res) => {
     const { includeParameters = false } = getCategoriesRequestBody.parse(req.body);
 
-    const response = await PrismaClient.categoriesParameters.findMany({
-      include: {
-        category: true,
-        parameter: includeParameters,
+    const response: getResponse[] = await PrismaClient.category.findMany({
+      select: {
+        id: true,
+        name: true,
+        description: true,
+        image: true,
+        crossSectionImage: true,
+        createdAt: false,
+        updatedAt: false,
+        categoriesParameters: includeParameters,
       },
     });
 
+    // delete unnecessary properties from paramters array
+    /* eslint-disable no-param-reassign */
+    response.forEach((r) => {
+      if (r.categoriesParameters) {
+        r.categoriesParameters.forEach((parameter) => {
+          delete parameter.categoryId;
+          delete parameter.createdAt;
+          delete parameter.updatedAt;
+        });
+      }
+    });
     res.status(200).json(formatAPIResponse(response));
   })
-  .post(async (req: NextApiRequest, res: NextApiResponse) => {
-    const { name, parameters } = createCategoryRequestBody.parse(req.body);
+  .post(apiGuardMiddleware({ allowAdminsOnly: true }), async (req, res) => {
+    const { name, description, image, crossSectionImage, parameters } = categoryRequestBody.parse(
+      req.body
+    );
+
+    if (name === '') {
+      throw new ParamError('name');
+    }
+
     const response = await PrismaClient.category.create({
       data: {
         name,
+        description,
+        image,
+        crossSectionImage,
+        categoriesParameters: {
+          create: parameters,
+        },
       },
     });
+
+    res.status(201).json(formatAPIResponse({ categoryId: response.id }));
   });
