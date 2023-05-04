55817a6fa6cf285bd44a73c63ec8573d08bfb53a
Kar <ISoNice4572@gmail.com>
2023-05-05 02:33:12 +0800

    feat: :lipstick: MUI Breakpoints

---
+++ packages/ui/lib/theme/breakpoints.ts
@@ -0,0 +1,14 @@
+// ** Type Import
+import type { BreakpointsOptions } from '@mui/material';
+
+const Breakpoints: BreakpointsOptions = {
+	values: {
+    xs: 0,
+		sm: 600,
+		md: 900,
+		lg: 1200,
+		xl: 1536,
+  }
+}
+
+export default Breakpoints
