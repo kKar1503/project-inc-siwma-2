f5012723f3be896371198de31a1ed202d40c8d4a
Kaung Khant Zaw <mmmike2002@gmail.com>
2023-05-04 13:33:16 +0800

    fix: comments

--- apps/marketplace/src/utils/api/client/zod/companyEndpoints.ts
+++ apps/marketplace/src/utils/api/client/zod/companyEndpoints.ts
@@ -8,15 +8,7 @@ export const getCompanyResponseBody = z.object({
   logo: z.string(),
   visibility: z.boolean(),
   createdAt: z.date().optional(),
-  companiesComments: z
-    .object({
-      id: z.number(),
-      companyId: z.number(),
-      comments: z.string(),
-      createdAt: z.date(),
-    })
-    .array()
-    .optional(),
+  comments: z.string().optional(),
 });
 
 export const getCompaniesResponseBody = getCompanyResponseBody.array();
