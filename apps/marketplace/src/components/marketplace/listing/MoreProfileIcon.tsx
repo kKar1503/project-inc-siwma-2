80667f9b60c2536ab161ea93200e861e5f84000c
Matthew Ng <matthew.ngjb@gmail.com>
2023-05-04 21:33:07 +0800

    refactor: :recycle: - Refactored sx into styled() component for better clarity

--- apps/marketplace/src/components/marketplace/listing/MoreProfileIcon.tsx
+++ apps/marketplace/src/components/marketplace/listing/MoreProfileIcon.tsx
@@ -8,6 +8,31 @@ import IconButton from '@mui/material/IconButton';
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
 import ArchiveIcon from '@mui/icons-material/Archive';
+import { styled } from '@mui/material/styles';
+
+const StyledMenu = styled(Menu)({
+  overflow: 'visible',
+  filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+  mt: 1.5,
+  '& .MuiAvatar-root': {
+    width: 32,
+    height: 32,
+    ml: -0.5,
+    mr: 1,
+  },
+  '&:before': {
+    content: '""',
+    display: 'block',
+    position: 'absolute',
+    top: 0,
+    right: 14,
+    width: 10,
+    height: 10,
+    bgcolor: 'background.paper',
+    transform: 'translateY(-50%) rotate(45deg)',
+    zIndex: 0,
+  },
+});
 
 const MoreProfileIcon = () => {
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
@@ -21,18 +46,11 @@ const MoreProfileIcon = () => {
 
   return (
     <Box>
-      <IconButton
-        onClick={handleClick}
-        size="small"
-        sx={{ ml: 'auto' }}
-        aria-controls={open ? 'menu' : undefined}
-        aria-haspopup="true"
-        aria-expanded={open ? 'true' : undefined}
-      >
+      <IconButton onClick={handleClick} size="small" sx={{ ml: 'auto' }}>
         <MoreVertIcon sx={{ color: 'black' }} />
       </IconButton>
 
-      <Menu
+      <StyledMenu
         anchorEl={anchorEl}
         id="menu"
         open={open}
@@ -40,29 +58,6 @@ const MoreProfileIcon = () => {
         onClick={handleClose}
         PaperProps={{
           elevation: 0,
-          sx: {
-            overflow: 'visible',
-            filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
-            mt: 1.5,
-            '& .MuiAvatar-root': {
-              width: 32,
-              height: 32,
-              ml: -0.5,
-              mr: 1,
-            },
-            '&:before': {
-              content: '""',
-              display: 'block',
-              position: 'absolute',
-              top: 0,
-              right: 14,
-              width: 10,
-              height: 10,
-              bgcolor: 'background.paper',
-              transform: 'translateY(-50%) rotate(45deg)',
-              zIndex: 0,
-            },
-          },
         }}
         transformOrigin={{ horizontal: 'right', vertical: 'top' }}
         anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
@@ -86,7 +81,7 @@ const MoreProfileIcon = () => {
           </ListItemIcon>
           Delete Listing
         </MenuItem>
-      </Menu>
+      </StyledMenu>
     </Box>
   );
 };
