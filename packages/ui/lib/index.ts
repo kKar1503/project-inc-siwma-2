28014be404369ea735d89cf901e05b90deda2f0a
Kar <ISoNice4572@gmail.com>
2023-05-05 02:41:20 +0800

    feat: :package: Update @inc/ui export

--- packages/ui/lib/index.ts
+++ packages/ui/lib/index.ts
@@ -1,1 +1,3 @@
 export { default as Button } from './components/Button';
+export { default as ThemeComponent } from './theme/ThemeComponent'
+export type { MuiThemeProps } from './theme/ThemeComponent'
