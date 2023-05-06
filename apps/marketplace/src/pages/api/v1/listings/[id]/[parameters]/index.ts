03275d1cf8c729fc078f90ebbdfc206b8c51fcfa
Javier <JAVIERFOKJF11.21@ichat.sp.edu.sg>
2023-05-07 01:54:50 +0800

    feat: added working PUT endpoint for listing parameters

--- apps/marketplace/src/pages/api/v1/listings/[id]/[parameters]/index.ts
+++ apps/marketplace/src/pages/api/v1/listings/[id]/[parameters]/index.ts
@@ -46,6 +46,7 @@ const parameterSchema = z.object({
 const requestSchema = z.object({
   parameters: z.array(parameterSchema),
 });
+
 // Custom request type with 'token' property
 interface CustomApiRequest extends NextApiRequest {
   token?: {
@@ -57,90 +58,112 @@ interface CustomApiRequest extends NextApiRequest {
   };
 }
 
+type RequestBodyParameter = {
+  parameterId: string;
+  value: string;
+};
+
 const createListingParameter = async (req: CustomApiRequest, res: NextApiResponse) => {
-    const id = parseListingId(req.query.id as string);
-    const userId = req.token?.user?.id;
-    const userRole = req.token?.user?.role;
+  const id = parseListingId(req.query.id as string);
+  const userId = req.token?.user?.id;
+  const userRole = req.token?.user?.role;
 
-    const listing = await checkListingExists(id);
+  const listing = await checkListingExists(id);
 
-    const isOwner = listing.owner === userId;
-    const isAdmin = userRole && userRole >= 1;
-    const sameCompany = req.token?.user?.company === listing.users.companyId.toString();
+  const isOwner = listing.owner === userId;
+  const isAdmin = userRole && userRole >= 1;
+  const sameCompany = req.token?.user?.company === listing.users.companyId.toString();
 
-    if (!isOwner && !isAdmin && !sameCompany) {
-        throw new ForbiddenError();
-    }
+  if (!isOwner && !isAdmin && !sameCompany) {
+    throw new ForbiddenError();
+  }
 
-    const { paramId, value } = req.body;
+  const { paramId, value } = req.body;
 
-    // Make sure to check if paramId is a valid number
-    const paramIdInt = parseInt(paramId, 10);
-    if (isNaN(paramIdInt)) {
-        return res.status(400).json(formatAPIResponse({ success: false, error: 'Invalid parameter ID' }));
-    }
-    
-    await PrismaClient.listingsParametersValue.create({
-        data: {
-            value: value,
-            listing: {
-                connect: {
-                    id: id,
-                },
-            },
-            parameter: {
-                connect: {
-                    id: paramIdInt,
-                },
-            },
+  // Make sure to check if paramId is a valid number
+  const paramIdInt = parseInt(paramId, 10);
+  if (isNaN(paramIdInt)) {
+    return res
+      .status(400)
+      .json(formatAPIResponse({ success: false, error: 'Invalid parameter ID' }));
+  }
+
+  await PrismaClient.listingsParametersValue.create({
+    data: {
+      value: value,
+      listing: {
+        connect: {
+          id: id,
         },
-    });
-    
-    res.status(201).json(formatAPIResponse({ success: true, data: 'Listing parameter created successfully' }));
+      },
+      parameter: {
+        connect: {
+          id: paramIdInt,
+        },
+      },
+    },
+  });
+
+  res
+    .status(201)
+    .json(formatAPIResponse({ success: true, data: 'Listing parameter created successfully' }));
+};
+
+const updateListingParameters = async (req: CustomApiRequest, res: NextApiResponse) => {
+  const id = parseListingId(req.query.id as string);
+  const userId = req.token?.user?.id;
+  const userRole = req.token?.user?.role;
+
+  const listing = await checkListingExists(id);
+
+  const isOwner = listing.owner === userId;
+  const isAdmin = userRole && userRole >= 1;
+  const sameCompany = req.token?.user?.company === listing.users.companyId.toString();
+
+  if (!isOwner && !isAdmin && !sameCompany) {
+    throw new ForbiddenError();
+  }
+
+  const parameters = z.array(parameterSchema).parse(req.body);
+
+  // Check for invalid paramIds and values
+  for (const param of parameters) {
+    const paramIdInt = parseInt(param.parameterId, 10);
+    if (isNaN(paramIdInt)) {
+      return res.status(422).json(formatAPIResponse({ success: false, error: 'Invalid paramId' }));
+    }
+  }
+
+  // Update the parameters in the database and format the response
+  const updatedParameters = await Promise.all(
+    parameters.map(async (param: RequestBodyParameter) => {
+      const parameterId = parseInt(param.parameterId, 10);
+
+      // Update the parameter value
+      const updatedParameter = await PrismaClient.listingsParametersValue.update({
+        where: {
+          listingId_parameterId: {
+            listingId: id,
+            parameterId: parameterId,
+          },
+        },
+        data: {
+          value: param.value,
+        },
+      });
+
+      // Format the response
+      return {
+        paramId: updatedParameter.parameterId,
+        value: updatedParameter.value,
+      };
+    })
+  );
+
+  res.status(200).json(formatAPIResponse({ success: true, data: updatedParameters }));
 };
 
 export default apiHandler()
   .get(getListingParameters)
   .post(createListingParameter)
-  .put(async (req, res) => {
-    const id = parseListingId(req.query.id as string);
-    const userId = req.token?.user?.id;
-    const userRole = req.token?.user?.role;
-
-    const listing = await checkListingExists(id);
-
-    const isOwner = listing.owner === userId;
-    const isAdmin = userRole && userRole >= 1;
-    const sameCompany = req.token?.user?.company === listing.users.companyId;
-
-    if (!isOwner && !isAdmin && !sameCompany) {
-      throw new ForbiddenError();
-    }
-
-    // Assuming that the request body contains the new parameters
-    const { parameters } = requestSchema.parse(req.body);
-
-    // Delete all the existing parameters for this listing
-    await PrismaClient.listingsParametersValue.deleteMany({
-      where: {
-        listingId: id,
-      },
-    });
-
-    // Add the new parameters to the database
-    await Promise.all(
-      parameters.map(async (param: ListingsParametersValue) => {
-        await PrismaClient.listingsParametersValue.create({
-          data: {
-            listingId: id,
-            parameterId: param.parameterId,
-            value: param.value,
-          },
-        });
-      })
-    );
-
-    res
-      .status(200)
-      .json(formatAPIResponse({ success: true, data: 'Listing parameters updated successfully' }));
-  });
+  .put(updateListingParameters);
