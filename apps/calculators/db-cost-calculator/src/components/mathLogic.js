cc1a05f19fdbf8757642dbae3f5a15ac3a8e70ec
Adeeb <adityaadeeb04@gmail.com>
2023-05-06 17:49:06 +0800

    feat: adding bit to price calculations

--- apps/calculators/db-cost-calculator/src/components/mathLogic.js
+++ apps/calculators/db-cost-calculator/src/components/mathLogic.js
@@ -41,3 +41,42 @@ function companyCommentsToBits(numCompanyComments) {
   return numCompanyComments * 310;
 }
 
+function bitsToGiB(bits) {
+  const bytes = bits / 8;
+  const gibibytes = bytes / 2 ** 30;
+  return gibibytes.toFixed(2);
+}
+
+function totalCost(gib) {
+  let cost = 0;
+  if (gib > 10) {
+    cost = (gib - 10) * 0.2;
+    cost = +cost.toFixed(2); // round to 2 decimal places
+  }
+  return cost;
+}
+
+export const mathLogic = (
+  numCompany,
+  numUsersperCompany,
+  percentUsers,
+  numListings,
+  numRooms,
+  numMessages,
+  numCategories,
+  numParams,
+) => {
+  const totalBits = calculateUserBits(numCompany, numUsersperCompany, percentUsers)
+    + listingBits(numListings)
+    + roomsBits(numRooms)
+    + messagesBits(numMessages)
+    + categoriestoBits(numCategories)
+    + paramsToBits(numParams)
+    + advertisementsToBits(4)
+    + listingBookmarkToBits(4)
+    + companyBookmarkToBits(4)
+    + companyCommentsToBits(2);
+  const totalGB = bitsToGiB(totalBits);
+  const totalCosts = totalCost(totalGB);
+  return totalCosts;
+};
