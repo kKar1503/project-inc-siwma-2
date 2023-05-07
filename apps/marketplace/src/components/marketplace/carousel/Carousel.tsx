dbf8ecd80fa4dd6aaa529b45f289f738f890f615
Mokke-31 <107909574+Mokke-31@users.noreply.github.com>
2023-05-07 15:05:39 +0800

    feat: moved the files from packages to apps
    moved file to apps folder and created new folder for carousel component

--- apps/marketplace/src/components/marketplace/carousel/Carousel.tsx
+++ apps/marketplace/src/components/marketplace/carousel/Carousel.tsx
@@ -35,7 +35,7 @@ const images = [
   },
 ];
 
-function Carousel() {
+function SwipeableTextMobileStepper() {
   const theme = useTheme();
   const [activeStep, setActiveStep] = React.useState(0);
   const maxSteps = images.length;
@@ -97,14 +97,26 @@ function Carousel() {
         position="static"
         activeStep={activeStep}
         nextButton={
-          <Button size="small" onClick={handleNext} disabled={activeStep === maxSteps - 1}>
+          <Button
+            size="small"
+            onClick={handleNext}
+            disabled={activeStep === maxSteps - 1}
+          >
             Next
-            {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
+            {theme.direction === 'rtl' ? (
+              <KeyboardArrowLeft />
+            ) : (
+              <KeyboardArrowRight />
+            )}
           </Button>
         }
         backButton={
           <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
-            {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
+            {theme.direction === 'rtl' ? (
+              <KeyboardArrowRight />
+            ) : (
+              <KeyboardArrowLeft />
+            )}
             Back
           </Button>
         }
@@ -113,4 +125,4 @@ function Carousel() {
   );
 }
 
-export default Carousel;
+export default SwipeableTextMobileStepper;
--- packages/ui/lib/index.ts
+++ packages/ui/lib/index.ts
@@ -1,5 +1,4 @@
 export { default as Button } from './components/Button';
-export { default as Carousel } from './components/Carousel';
 export { default as ThemeComponent } from './theme/ThemeComponent';
 export type { MuiThemeProps } from './theme/ThemeComponent';
 export * as MuiColors from '@mui/material/colors';
