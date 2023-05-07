4e4dd01d047f46d8c9b9162fed074c1352802864
Matthew Ng <matthew.ngjb@gmail.com>
2023-05-08 04:09:24 +0800

    style: :lipstick: - Updated style for BuyBadge component

--- apps/marketplace/src/components/marketplace/listing/BuyBadge.tsx
+++ apps/marketplace/src/components/marketplace/listing/BuyBadge.tsx
@@ -1,15 +1,26 @@
 import { Chip } from '@mui/material';
+import { useTheme } from '@mui/material/styles';
 
-const BuyBadge = () => (
-  <Chip
-    sx={{
-      backgroundColor: '#34D399',
-      color: '#FFFFFF',
-      fontWeight: 'bold',
-      mr: 0.5,
-    }}
-    label="Buying"
-  />
-);
+const BuyBadge = () => {
+  const theme = useTheme();
+
+  return (
+    <Chip
+      // sx={{
+      //   backgroundColor: '#34D399',
+      //   color: '#FFFFFF',
+      //   fontWeight: 'bold',
+      //   mr: 0.5,
+      // }}
+      label="Buying"
+      sx={({ spacing, palette }) => ({
+        mr: spacing(1),
+        bgcolor: palette.secondary[300],
+        fontWeight: 'bold',
+        color: palette.common.white,
+      })}
+    />
+  );
+};
 
 export default BuyBadge;
