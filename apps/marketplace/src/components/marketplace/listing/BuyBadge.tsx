f3b56fb669e375f839b37b82e0552d0bcb73a72b
Matthew Ng <matthew.ngjb@gmail.com>
2023-05-08 04:12:02 +0800

    style: :lipstick: - Updated styles for SellBadge

--- apps/marketplace/src/components/marketplace/listing/BuyBadge.tsx
+++ apps/marketplace/src/components/marketplace/listing/BuyBadge.tsx
@@ -6,19 +6,13 @@ const BuyBadge = () => {
 
   return (
     <Chip
-      // sx={{
-      //   backgroundColor: '#34D399',
-      //   color: '#FFFFFF',
-      //   fontWeight: 'bold',
-      //   mr: 0.5,
-      // }}
-      label="Buying"
       sx={({ spacing, palette }) => ({
         mr: spacing(1),
         bgcolor: palette.secondary[300],
         fontWeight: 'bold',
         color: palette.common.white,
       })}
+      label="Buying"
     />
   );
 };
