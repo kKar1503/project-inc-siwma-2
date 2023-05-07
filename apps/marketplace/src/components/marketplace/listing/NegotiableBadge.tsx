c091e8c6c076e26d39024a97fbddf196fd73da9e
Matthew Ng <matthew.ngjb@gmail.com>
2023-05-08 04:16:36 +0800

    style: :lipstick: - Updated NegotiableBadge styles

--- apps/marketplace/src/components/marketplace/listing/NegotiableBadge.tsx
+++ apps/marketplace/src/components/marketplace/listing/NegotiableBadge.tsx
@@ -1,13 +1,24 @@
 import { Chip } from '@mui/material';
+import { createTheme } from '@mui/material/styles';
+
+// warning color not included in palette, created here for negotiable tag use
+// yellow was an option but too bright for user
+const theme = createTheme({
+  palette: {
+    warning: {
+      main: '#ed6c02',
+    },
+  },
+});
 
 const NegotiableBadge = () => (
   <Chip
-    sx={{
-      color: '#FFFFFF',
+    sx={({ spacing, palette }) => ({
+      mr: spacing(1),
+      bgcolor: theme.palette.warning.main,
       fontWeight: 'bold',
-      mr: 0.5,
-    }}
-    color="warning"
+      color: palette.common.white,
+    })}
     label="Negotiable"
   />
 );
