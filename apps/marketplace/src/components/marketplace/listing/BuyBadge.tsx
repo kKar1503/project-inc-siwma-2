ce6501f090f3f0e3b591f14a8f1775fe9fda3ced
Matthew Ng <matthew.ngjb@gmail.com>
2023-05-08 12:19:03 +0800

    refactor: :recycle: - Refactor Style

--- apps/marketplace/src/components/marketplace/listing/BuyBadge.tsx
+++ apps/marketplace/src/components/marketplace/listing/BuyBadge.tsx
@@ -1,20 +1,15 @@
 import { Chip } from '@mui/material';
-import { useTheme } from '@mui/material/styles';
 
-const BuyBadge = () => {
-  const theme = useTheme();
-
-  return (
-    <Chip
-      sx={({ spacing, palette }) => ({
-        mr: spacing(1),
-        bgcolor: palette.secondary[300],
-        fontWeight: 'bold',
-        color: palette.common.white,
-      })}
-      label="Buying"
-    />
-  );
-};
+const BuyBadge = () => (
+  <Chip
+    sx={({ spacing, palette }) => ({
+      mr: spacing(1),
+      bgcolor: palette.secondary[300],
+      fontWeight: 'bold',
+      color: palette.common.white,
+    })}
+    label="Buying"
+  />
+);
 
 export default BuyBadge;
