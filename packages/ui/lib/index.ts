551753b21373933c149ab858ae7f867cb6b0c817
Matthew Ng <matthew.ngjb@gmail.com>
2023-05-10 02:23:26 +0800

    feat: :sparkles: - Added new SelectComponent in @inc/ui
    select component made for available use everywhere, accepts props to change its values and returns values to parent

--- packages/ui/lib/index.ts
+++ packages/ui/lib/index.ts
@@ -1,5 +1,6 @@
 export { default as Button } from './components/Button';
 export { default as StarsRating } from './components/StarsRating';
+export { default as SelectComponent } from './components/SelectComponent';
 export { default as ThemeComponent } from './theme/ThemeComponent';
 export type { MuiThemeProps } from './theme/ThemeComponent';
 export * as MuiColors from '@mui/material/colors';
