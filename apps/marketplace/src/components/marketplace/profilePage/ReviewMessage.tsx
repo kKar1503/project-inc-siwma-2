87deb069422e418b21ab7dbe5e28b342f3eec59f
Matthew Ng <matthew.ngjb@gmail.com>
2023-05-10 21:55:33 +0800

    feat: :sparkles: - Updated ReviewMessage component to include more features
    added noOfReviews, profilePic and review from buyer/seller check

--- apps/marketplace/src/components/marketplace/profilePage/ReviewMessage.tsx
+++ apps/marketplace/src/components/marketplace/profilePage/ReviewMessage.tsx
@@ -7,46 +7,66 @@ import Typography from '@mui/material/Typography';
 import { DateTime } from 'luxon';
 import { useMemo } from 'react';
 import Link from 'next/link';
+import { StarsRating } from '@inc/ui';
 import StarRating from './StarRatings';
 
 export type ReviewProps = {
-  id: number;
+  noOfReviews: number;
+  profilePic: string;
   createdAt: string;
   rating: number;
   username: string;
   companyName: string;
   body: string;
+  buyer: boolean;
 };
 
-const ReviewMessage = ({ id, createdAt, rating, username, companyName, body }: ReviewProps) => {
+const ReviewMessage = ({
+  noOfReviews,
+  profilePic,
+  createdAt,
+  rating,
+  username,
+  companyName,
+  body,
+  buyer,
+}: ReviewProps) => {
   const datetime = useMemo(
     () => DateTime.fromISO(createdAt).toRelative({ locale: 'en-SG' }),
     [createdAt]
   );
 
   return (
-    <List sx={{ m: 2, maxWidth: 'md' }}>
+    <List sx={{ m: 2 }}>
       <Box sx={{ m: 1 }}>
         <Stack direction="row" spacing={2} alignItems="center">
-          <Avatar src="/broken-image.jpg" sx={{ width: 45, height: 45 }} />
+          <Avatar src={profilePic} sx={{ width: 45, height: 45 }} />
           <Stack>
             <Stack direction="row" spacing={1} justifyContent="center" alignItems="center">
-              <Link href="/#" style={{ textDecoration: 'none', color: 'black' }}>
+              <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                <Link href="/#" style={{ textDecoration: 'none', color: 'black' }}>
+                  <Typography
+                    variant="h6"
+                    component="div"
+                    sx={{
+                      flexGrow: 1,
+                      fontWeight: 'bold',
+                      '&:hover': { textDecoration: 'underline' },
+                    }}
+                  >
+                    {username}
+                  </Typography>
+                </Link>
                 <Typography
-                  variant="h6"
-                  component="div"
-                  sx={{
+                  sx={({ typography }) => ({
+                    fontSize: typography.subtitle1,
                     flexGrow: 1,
-                    fontWeight: 'bold',
-                    '&:hover': { textDecoration: 'underline' },
-                  }}
+                  })}
                 >
-                  {username}
+                  &nbsp;review from {buyer ? 'buyer' : 'seller'}
                 </Typography>
-              </Link>
-              <Typography sx={{ fontWeight: 'bold' }}>
-                &#183;
-              </Typography>
+              </Box>
+              <Typography sx={{ fontWeight: 'bold' }}>&#183;</Typography>
               <Typography variant="body1" sx={{ flexGrow: 1 }}>
                 {datetime}
               </Typography>
@@ -63,8 +83,11 @@ const ReviewMessage = ({ id, createdAt, rating, username, companyName, body }: R
           </Stack>
         </Stack>
       </Box>
-      <Box>
-        <StarRating rating={rating} />
+      <Box sx={{ display: 'flex' }}>
+        <Stack direction="row" spacing={-1}>
+          <Typography sx={{ mx: 2, fontWeight: 'bold' }}>{rating.toFixed(1)}</Typography>
+          <StarsRating rating={rating} />
+        </Stack>
       </Box>
       <Box>
         <Typography
@@ -72,7 +95,9 @@ const ReviewMessage = ({ id, createdAt, rating, username, companyName, body }: R
           component="div"
           sx={{ flexGrow: 1, color: 'grey', mx: 2, my: 1 }}
         >
-          {id}
+          {/* check if display review of reviews based on number */}
+          {noOfReviews}
+          {noOfReviews === 1 ? ' Review' : ' Reviews'}
         </Typography>
       </Box>
       <Box>
