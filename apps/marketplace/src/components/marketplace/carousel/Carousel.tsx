dc12af84378fad622ea0e8d38179584874da2ced
Mokke-31 <107909574+Mokke-31@users.noreply.github.com>
2023-05-10 16:55:12 +0800

    feat: improved carousel feature
    improved carousel feature

--- apps/marketplace/src/components/marketplace/carousel/Carousel.tsx
+++ apps/marketplace/src/components/marketplace/carousel/Carousel.tsx
@@ -3,7 +3,6 @@ import { useTheme } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import MobileStepper from '@mui/material/MobileStepper';
 import Paper from '@mui/material/Paper';
-import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
 import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
@@ -14,22 +13,18 @@ const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
 
 const images = [
   {
-    label: 'San Francisco – Oakland Bay Bridge, United States',
     imgPath:
       'https://images.unsplash.com/photo-1537944434965-cf4679d1a598?auto=format&fit=crop&w=400&h=250&q=60',
   },
   {
-    label: 'Bird',
     imgPath:
       'https://images.unsplash.com/photo-1538032746644-0212e812a9e7?auto=format&fit=crop&w=400&h=250&q=60',
   },
   {
-    label: 'Bali, Indonesia',
     imgPath:
       'https://images.unsplash.com/photo-1537996194471-e657df975ab4?auto=format&fit=crop&w=400&h=250',
   },
   {
-    label: 'Goč, Serbia',
     imgPath:
       'https://images.unsplash.com/photo-1512341689857-198e7e2f3ca8?auto=format&fit=crop&w=400&h=250&q=60',
   },
@@ -53,7 +48,7 @@ const Carousel = () => {
   };
 
   return (
-    <Box sx={{ maxWidth: 400, flexGrow: 1 }}>
+    <Box sx={{ maxWidth: 'max', flexGrow: 1 }}>
       <Paper
         square
         elevation={0}
@@ -61,12 +56,9 @@ const Carousel = () => {
           display: 'flex',
           alignItems: 'center',
           height: 50,
-          pl: 2,
           bgcolor: 'background.default',
         }}
-      >
-        <Typography>{images[activeStep].label}</Typography>
-      </Paper>
+      />
       <AutoPlaySwipeableViews
         axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
         index={activeStep}
@@ -74,20 +66,38 @@ const Carousel = () => {
         enableMouseEvents
       >
         {images.map((step, index) => (
-          <div key={step.label}>
+          <div>
             {Math.abs(activeStep - index) <= 2 ? (
               <Box
-                component="img"
                 sx={{
-                  height: 255,
-                  display: 'block',
-                  maxWidth: 400,
-                  overflow: 'hidden',
-                  width: '100%',
+                  position: 'relative',
                 }}
-                src={step.imgPath}
-                alt={step.label}
-              />
+              >
+                <Box
+                  component="img"
+                  sx={{
+                    height: 255,
+                    display: 'block',
+                    maxWidth: 'max',
+                    overflow: 'hidden',
+                    width: '100%',
+                    opacity: '30%',
+                  }}
+                  src={step.imgPath}
+                />
+                <Box
+                  component="img"
+                  sx={{
+                    height: 255,
+                    display: 'block',
+                    maxWidth: 'max',
+                    overflow: 'hidden',
+                    width: 'min',
+                    zIndex: 'tooltip',
+                  }}
+                  src={step.imgPath}
+                />
+              </Box>
             ) : null}
           </div>
         ))}
@@ -97,32 +107,20 @@ const Carousel = () => {
         position="static"
         activeStep={activeStep}
         nextButton={
-          <Button
-            size="small"
-            onClick={handleNext}
-            disabled={activeStep === maxSteps - 1}
-          >
+          <Button size="small" onClick={handleNext} disabled={activeStep === maxSteps - 1}>
             Next
-            {theme.direction === 'rtl' ? (
-              <KeyboardArrowLeft />
-            ) : (
-              <KeyboardArrowRight />
-            )}
+            {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
           </Button>
         }
         backButton={
           <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
-            {theme.direction === 'rtl' ? (
-              <KeyboardArrowRight />
-            ) : (
-              <KeyboardArrowLeft />
-            )}
+            {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
             Back
           </Button>
         }
       />
     </Box>
   );
-}
+};
 
 export default Carousel;
