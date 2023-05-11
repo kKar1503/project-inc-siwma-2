7360693a86aa76f21583632a4005680a4d3172b7
yiningsp <yyiningsp@gmail.com>
2023-05-11 17:31:22 +0800

    feat: login page with nextauth
    login page with nextauth and using mui theme

--- apps/marketplace/src/pages/Login.tsx
+++ apps/marketplace/src/pages/Login.tsx
@@ -1,95 +1,167 @@
-import {
-  Typography,
-  Button,
-  Divider,
-  TextField,
-  Box,
-  Checkbox,
-  Container,
-  FormControlLabel,
-  Grid,
-} from '@mui/material';
-// rmb to edit the imports
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import Divider from '@mui/material/Divider';
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+import Checkbox from '@mui/material/Checkbox';
+import Container from '@mui/material/Container';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Grid from '@mui/material/Grid';
 import Link from 'next/link';
 import Image from 'next/image';
+import { useState } from 'react';
+import { signIn } from 'next-auth/react';
 
-const LoginForm = () => (
-  // const handleSubmit = (event: { preventDefault: () => void; currentTarget: HTMLFormElement | undefined; }) => {
-  //   event.preventDefault();
-  //   const data = new FormData(event.currentTarget);
-  //   console.log({
-  //     email: data.get('email'),
-  //     password: data.get('password'),
-  //   });
-  // };
-  <Box>
-    <Box sx={{ zIndex: -1, opacity: 0.7 }}>
-      <Image src="/../public/images/favicons/siwma-bg.jpg" alt="logo" fill />
-    </Box>
-          <Container sx={{ zIndex: 1500 }} component="main" maxWidth="sm">  
+const LoginForm = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState(false);
+  const handleSubmit = async (event: {
+    preventDefault: () => void;
+    currentTarget: HTMLFormElement | undefined;
+  }) => {
+    event.preventDefault();
+    const authResult = await signIn('credentials', {
+      redirect: false,
+      email,
+      password,
+    });
+    if (!authResult!.ok) {
+      setErrorMessage(true);
+    } else {
+      return {
+        redirect: {
+          destination: '/index',
+        },
+      };
+    }
+    console.log(authResult);
+
+    return authResult;
+  };
+
+  return (
+    <Box>
       <Box
-        // onSubmit={handleSubmit}
-        sx={({ shape, shadows, spacing, palette }) => ({
-          // backgroundImage= `url('/../public/images/favicons/siwma-bg.jpg')`,
-          boxShadow: shadows[5],
-          mt: spacing(3),
-          px: spacing(4),
-          py: spacing(4),
-          bgcolor: palette.common.white,
-          ...shape,
-        })}
+        sx={{
+          width: '100vw',
+          backgroundSize: 'cover',
+        }}
       >
-        <Image src="/../public/images/favicons/Siwma-logo.png" alt="logo" width={450} height={80} />
-        <Divider flexItem />
-        <Typography variant="h5" sx={{ fontWeight: 'Bold', mt: 2 }}>
-          Sign In
-        </Typography>
-        <Typography variant="body1" sx={{ fontSize: 16 }}>
-          Please sign in to your account
-        </Typography>
-        <Box component="form">
-          <TextField
-            fullWidth
-            name="email"
-            id="email"
-            label="E-mail"
-            placeholder="Your company e-mail"
-            // autoComplete="email"
-            type="email"
-            variant="standard"
-            autoFocus
-            margin="normal"
-          />
-          <TextField
-            fullWidth
-            name="password"
-            id="password"
-            label="Password"
-            placeholder="Your password"
-            // autoComplete="current-password"
-            type="password"
-            variant="standard"
-          />
-          <Grid container sx={{ my: 4 }}>
-            <Grid item xs>
-              <FormControlLabel
-                control={<Checkbox value="remember" color="primary" />}
-                label="Remember me"
+        <Image src="/../public/images/siwma-bg.png" alt="logo" fill />
+        <Container
+          component="main"
+          maxWidth="sm"
+          sx={{
+            justifyContent: 'center',
+            display: 'flex',
+            flexDirection: 'column',
+            height: '100vh',
+          }}
+        >
+          <Box
+            sx={({ shape, shadows, spacing, palette }) => ({
+              boxShadow: shadows[5],
+              px: spacing(4),
+              py: spacing(4),
+              position: 'relative',
+              bgcolor: palette.common.white,
+              ...shape,
+            })}
+          >
+            <Image
+              src="/../public/images/favicons/Siwma-logo.jpeg"
+              alt="logo"
+              width={460}
+              height={80}
+            />
+            <Divider flexItem />
+            <Typography
+              sx={({ spacing, typography }) => ({
+                fontSize: typography.h5,
+                mt: spacing(2),
+                fontWeight: 'bold',
+              })}
+            >
+              Sign In
+            </Typography>
+            <Typography
+              sx={({ typography }) => ({
+                fontSize: typography.body1,
+              })}
+            >
+              Please sign in to your account
+            </Typography>
+            <Box component="form" onSubmit={handleSubmit}>
+              <TextField
+                fullWidth
+                name="email"
+                id="email"
+                label="E-mail"
+                placeholder="Your company e-mail"
+                value={email}
+                // autoComplete="email"
+                type="email"
+                variant="standard"
+                autoFocus
+                margin="normal"
+                onChange={(e) => setEmail(e.target.value)}
               />
-            </Grid>
-            <Grid item>
-              <Link href="/forgot-your-password">
-                <Typography sx={{ mt: 1 }}>Forgot your password?</Typography>
-              </Link>
-            </Grid>
-          </Grid>
-          <Button type="submit" fullWidth variant="contained">
-            SIGN IN
-          </Button>
-        </Box>
+
+              <TextField
+                fullWidth
+                name="password"
+                id="password"
+                label="Password"
+                placeholder="Your password"
+                value={password}
+                // autoComplete="current-password"
+                type="password"
+                variant="standard"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              {errorMessage && (
+                <Typography
+                  sx={({ palette }) => ({
+                    color: palette.error.main,
+                  })}
+                >
+                  Invalid email or password
+                </Typography>
+              )}
+              <Grid
+                container
+                sx={({ spacing }) => ({
+                  my: spacing(4),
+                })}
+              >
+                <Grid item xs>
+                  <FormControlLabel
+                    control={<Checkbox value="remember" color="primary" />}
+                    label="Remember me"
+                  />
+                </Grid>
+                <Grid item>
+                  <Link href="/forgot-your-password">
+                    <Typography
+                      sx={({ spacing }) => ({
+                        mt: spacing(1),
+                      })}
+                    >
+                      Forgot your password?
+                    </Typography>
+                  </Link>
+                </Grid>
+              </Grid>
+              <Button type="submit" fullWidth variant="contained">
+                SIGN IN
+              </Button>
+            </Box>
+          </Box>
+        </Container>
       </Box>
-    </Container>
-  </Box>
-);
+    </Box>
+  );
+};
 
 export default LoginForm;
