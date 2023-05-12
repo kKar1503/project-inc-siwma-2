6b7a704e1a58c77a31488cd9bf261a6c8d8b6b9a
Matthew Ng <matthew.ngjb@gmail.com>
2023-05-12 20:20:46 +0800

    feat: :sparkles: - Added linking for appropriate events

--- apps/marketplace/src/components/marketplace/listing/MoreProfileIcon.tsx
+++ apps/marketplace/src/components/marketplace/listing/MoreProfileIcon.tsx
@@ -8,15 +8,41 @@ import IconButton from '@mui/material/IconButton';
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
 import ArchiveIcon from '@mui/icons-material/Archive';
+import { useRouter } from 'next/router';
 
-const MoreProfileIcon = () => {
+export type MoreProfileIconProps = {
+  productId: number;
+};
+
+const MoreProfileIcon = ({ productId }: MoreProfileIconProps) => {
+  const router = useRouter();
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
   const open = Boolean(anchorEl);
   const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    // stops click from propogating through to Link tag
+    event.preventDefault();
     // anchors menu to where "More" VertIcon was clicked
     setAnchorEl(event.currentTarget);
   };
-  const handleClose = () => {
+
+  const handleClose = (event: React.MouseEvent<HTMLElement>) => {
+    event.stopPropagation();
+    setAnchorEl(null);
+  };
+
+  const handleEditClick = () => {
+    // send user to page to edit listing
+    router.push(`${productId}/edit-listing`);
+    setAnchorEl(null);
+  };
+
+  const handleArchiveClick = () => {
+    // set listing to archived state endpoint
+    setAnchorEl(null);
+  };
+
+  const handleDeleteClick = () => {
+    // delete listing endpoint
     setAnchorEl(null);
   };
 
@@ -36,19 +62,19 @@ const MoreProfileIcon = () => {
         anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
       >
         {/* change handle close functions to handleEdit, handleArchive, handleDelete with relevant APIs */}
-        <MenuItem onClick={handleClose}>
+        <MenuItem onClick={handleEditClick}>
           <ListItemIcon>
             <EditIcon fontSize="small" />
           </ListItemIcon>
           Edit Listing
         </MenuItem>
-        <MenuItem onClick={handleClose}>
+        <MenuItem onClick={handleArchiveClick}>
           <ListItemIcon>
             <ArchiveIcon fontSize="small" />
           </ListItemIcon>
           Archive Listing
         </MenuItem>
-        <MenuItem onClick={handleClose}>
+        <MenuItem onClick={handleDeleteClick}>
           <ListItemIcon>
             <DeleteIcon fontSize="small" />
           </ListItemIcon>
--- apps/marketplace/src/components/marketplace/listing/ProductListingItem.tsx
+++ apps/marketplace/src/components/marketplace/listing/ProductListingItem.tsx
@@ -5,6 +5,7 @@ import CardMedia from '@mui/material/CardMedia';
 import CardContent from '@mui/material/CardContent';
 import Avatar from '@mui/material/Avatar';
 import Typography from '@mui/material/Typography';
+import Link from 'next/link';
 import { red } from '@mui/material/colors';
 import { StarsRating } from '@inc/ui';
 import Card from '@mui/material/Card';
@@ -15,12 +16,12 @@ import SellBadge from './SellBadge';
 import NegotiableBadge from './NegotiableBadge';
 
 export type ProductListingItemProps = {
+  productId: number;
   img: string;
   profileImg: string;
   type: string;
   name: string;
   rating: number;
-  href: string;
   price: number;
   negotiable: boolean;
   ownerId: string;
@@ -32,12 +33,12 @@ export type ProductListingItemProps = {
 };
 
 const ProductListingItem = ({
+  productId,
   img,
   profileImg,
   type,
   name,
   rating,
-  href,
   price,
   negotiable,
   ownerId,
@@ -55,72 +56,80 @@ const ProductListingItem = ({
 
   return (
     <Card sx={{ maxWidth: 288, maxHeight: 600, border: '1px solid #C4C4C4' }}>
-      <CardHeader
-        avatar={
-          <Avatar sx={{ bgcolor: red[500] }} src={profileImg}>
-            {ownerFullName.charAt(0)}
-          </Avatar>
-        }
-        title={ownerFullName}
-        titleTypographyProps={{
-          fontSize: 16,
-          fontWeight: 'bold',
-        }}
-        subheader={companyName}
-      />
-      <CardMedia component="img" height="288" image={img} />
+      <Link style={{ textDecoration: 'none' }} href={`/profile/${ownerId}`}>
+        <CardHeader
+          avatar={
+            <Avatar sx={{ bgcolor: red[500] }} src={profileImg}>
+              {ownerFullName.charAt(0)}
+            </Avatar>
+          }
+          title={ownerFullName}
+          titleTypographyProps={{
+            fontSize: 16,
+            fontWeight: 'bold',
+          }}
+          subheader={companyName}
+        />
+      </Link>
+      <Link style={{ textDecoration: 'none' }} href={`/product/${productId}`}>
+        <CardMedia component="img" height="288" image={img} />
+      </Link>
       <CardContent
         sx={({ spacing }) => ({
           pl: spacing(2),
         })}
       >
-        <Box
-          sx={({ spacing }) => ({
-            display: 'flex',
-            pb: spacing(2),
-          })}
-        >
-          {type === 'Buy' && <BuyBadge />}
-          {type === 'Sell' && <SellBadge />}
-          {negotiable && <NegotiableBadge />}
-
-          <Box sx={{ ml: 'auto' }}>{isOwnProfile && <MoreProfileIcon />}</Box>
-        </Box>
-        <Box
-          sx={({ spacing }) => ({
-            pb: spacing(2),
-          })}
-        >
-          <Typography
-            sx={{ overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap' }}
-            variant="body2"
-            color="text.primary"
-            fontWeight={400}
-            fontSize={20}
+        <Link style={{ textDecoration: 'none' }} href={`/product/${productId}`}>
+          <Box
+            sx={({ spacing }) => ({
+              display: 'flex',
+              pb: spacing(2),
+            })}
           >
-            {name}
-          </Typography>
-        </Box>
-        <Box
-          sx={({ spacing }) => ({
-            pb: spacing(2),
-          })}
-        >
-          <Typography variant="h4" color="text.primary" fontWeight="bold" fontSize={24}>
-            {new Intl.NumberFormat('en-SG', {
-              style: 'currency',
-              currency: 'SGD',
-            }).format(price)}
-            {isUnitPrice && '/unit'}
-          </Typography>
-        </Box>
-        <Box
-          sx={({ spacing }) => ({
-            pb: spacing(2),
-          })}
-        >
-          <StarsRating rating={rating} />
-        </Box>
+            {type === 'Buy' && <BuyBadge />}
+            {type === 'Sell' && <SellBadge />}
+            {negotiable && <NegotiableBadge />}
+
+            <Box sx={{ ml: 'auto' }}>
+              {isOwnProfile && <MoreProfileIcon productId={productId} />}
+            </Box>
+          </Box>
+          <Box
+            sx={({ spacing }) => ({
+              pb: spacing(2),
+            })}
+          >
+            <Typography
+              sx={{ overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap' }}
+              variant="body2"
+              color="text.primary"
+              fontWeight={400}
+              fontSize={20}
+            >
+              {name}
+            </Typography>
+          </Box>
+          <Box
+            sx={({ spacing }) => ({
+              pb: spacing(2),
+            })}
+          >
+            <Typography variant="h4" color="text.primary" fontWeight="bold" fontSize={24}>
+              {new Intl.NumberFormat('en-SG', {
+                style: 'currency',
+                currency: 'SGD',
+              }).format(price)}
+              {isUnitPrice && '/unit'}
+            </Typography>
+          </Box>
+          <Box
+            sx={({ spacing }) => ({
+              pb: spacing(2),
+            })}
+          >
+            <StarsRating rating={rating} />
+          </Box>
+        </Link>
         <Box
           sx={({ spacing }) => ({
             pb: spacing(2),
