12e7b4a85629b4b156450d2cba6874095a2e0e28
Hein Htet Aung <hhaung5142@gmail.com>
2023-05-13 21:48:22 +0800

    feat: :sparkles: Implemented Category selection after discussion with frontend leaders

--- apps/marketplace/src/components/marketplace/filter/FilterForm.tsx
+++ apps/marketplace/src/components/marketplace/filter/FilterForm.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import Divider from '@mui/material/Divider';
@@ -13,17 +13,21 @@ import Button from '@mui/material/Button';
 
 const FilterForm = ({
   setSort,
+  setCategory,
   setNegotiation,
   setMinPrice,
   setMaxPrice,
 }: {
   setSort: (sort: string) => void;
+  setCategory: (category: string) => void;
   setNegotiation: (negotiation: string) => void;
   setMinPrice: (minPrice: string) => void;
   setMaxPrice: (maxPrice: string) => void;
 }) => {
   const sortOptions = ['Recent', 'Price - High to Low', 'Price - Low to High'];
+  const [categoryOptions, setCategoryOptions] = useState<string[]>([]);
   const [sortOption, setSortOption] = useState<string>('');
+  const [categoryOption, setCategoryOption] = useState<string>('');
   const [negotiationOption, setNegotiationOption] = useState<string>('');
   const [minPriceOption, setMinPriceOption] = useState<string>('');
   const [maxPriceOption, setMaxPriceOption] = useState<string>('');
@@ -31,15 +35,22 @@ const FilterForm = ({
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     setSort(sortOption);
+    setCategory(categoryOption);
     setNegotiation(negotiationOption);
     setMinPrice(minPriceOption);
     setMaxPrice(maxPriceOption);
 
     console.log(`Sort: ${sortOption}`);
+    console.log(`Category: ${categoryOption}`);
     console.log(`Negotiation: ${negotiationOption}`);
     console.log(`Min Price: ${minPriceOption}`);
     console.log(`Max Price: ${maxPriceOption}`);
   };
+
+  useEffect(() => {
+    // Get categories from backend
+    setCategoryOptions([]);
+  }, []);
 
   return (
     <form style={{ padding: 1, marginTop: 2, width: '100%' }} onSubmit={handleSubmit}>
@@ -54,6 +65,20 @@ const FilterForm = ({
         value={sortOption as string}
       >
         {sortOptions.map((option) => (
+          <MenuItem key={option} value={option}>
+            {option}
+          </MenuItem>
+        ))}
+      </Select>
+
+      <Divider sx={{ my: 2 }} />
+      <FormLabel sx={{ fontWeight: 600 }}>Category</FormLabel>
+      <Select
+        sx={{ height: '45px', width: '100%' }}
+        onChange={(e: SelectChangeEvent<string>) => setCategoryOption(e.target.value as string)}
+        value={categoryOption as string}
+      >
+        {categoryOptions.map((option) => (
           <MenuItem key={option} value={option}>
             {option}
           </MenuItem>
