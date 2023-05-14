510c67f766400b18a16038ad0faea481a4915200
Hein Htet Aung <hhaung5142@gmail.com>
2023-05-14 17:41:11 +0800

    fix: :art: eslint fix

--- apps/marketplace/src/components/marketplace/createListing/ListingForm.tsx
+++ apps/marketplace/src/components/marketplace/createListing/ListingForm.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
@@ -20,76 +21,74 @@ const ListingForm = ({
   setNegotiable,
   setUnitPrice,
   setDescription,
-}: SetListingProps) => {
-  return (
-    <>
-      <Grid item xs={12} md={12} sx={{ width: '100%' }}>
-        <Typography variant="h6" sx={{ fontWeight: 'bold' }}>
-          General Information
-        </Typography>
-        <Typography variant="body1">Enter the details of the listing below</Typography>
-      </Grid>
-      <Grid item xs={12} md={12} sx={{ width: '100%' }}>
-        <TextField
-          size="medium"
-          variant="outlined"
-          label="Listing Title"
-          fullWidth
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-            setTitle(e.target.value);
-          }}
-        />
-      </Grid>
+}: SetListingProps) => (
+  <>
+    <Grid item xs={12} md={12} sx={{ width: '100%' }}>
+      <Typography variant="h6" sx={{ fontWeight: 'bold' }}>
+        General Information
+      </Typography>
+      <Typography variant="body1">Enter the details of the listing below</Typography>
+    </Grid>
+    <Grid item xs={12} md={12} sx={{ width: '100%' }}>
+      <TextField
+        size="medium"
+        variant="outlined"
+        label="Listing Title"
+        fullWidth
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+          setTitle(e.target.value);
+        }}
+      />
+    </Grid>
 
-      <Grid item xs={12} md={12} sx={{ width: '100%' }}>
-        <OutlinedInput
-          className="outlined-adornment-amount"
-          startAdornment={<InputAdornment position="start">S$</InputAdornment>}
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-            setPrice(parseFloat(e.target.value));
-          }}
-        />
-      </Grid>
+    <Grid item xs={12} md={12} sx={{ width: '100%' }}>
+      <OutlinedInput
+        className="outlined-adornment-amount"
+        startAdornment={<InputAdornment position="start">S$</InputAdornment>}
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+          setPrice(parseFloat(e.target.value));
+        }}
+      />
+    </Grid>
 
-      <Grid item xs={12} md={12} sx={{ width: '100%' }}>
-        <FormControlLabel
-          value="negotiable"
-          control={
-            <Checkbox
-              onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                setNegotiable(e.target.checked);
-              }}
-            />
-          }
-          label="Negotiable?"
-        />
-        <FormControlLabel
-          value="unitPrice"
-          control={
-            <Checkbox
-              onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                setUnitPrice(e.target.checked);
-              }}
-            />
-          }
-          label="Price per unit?"
-        />
-      </Grid>
+    <Grid item xs={12} md={12} sx={{ width: '100%' }}>
+      <FormControlLabel
+        value="negotiable"
+        control={
+          <Checkbox
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+              setNegotiable(e.target.checked);
+            }}
+          />
+        }
+        label="Negotiable?"
+      />
+      <FormControlLabel
+        value="unitPrice"
+        control={
+          <Checkbox
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+              setUnitPrice(e.target.checked);
+            }}
+          />
+        }
+        label="Price per unit?"
+      />
+    </Grid>
 
-      <Grid item xs={12} md={12} sx={{ width: '100%' }}>
-        <TextField
-          rows={6}
-          variant="outlined"
-          label="Listing Description"
-          fullWidth
-          multiline
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-            setDescription(e.target.value);
-          }}
-        />
-      </Grid>
-    </>
-  );
-};
+    <Grid item xs={12} md={12} sx={{ width: '100%' }}>
+      <TextField
+        rows={6}
+        variant="outlined"
+        label="Listing Description"
+        fullWidth
+        multiline
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+          setDescription(e.target.value);
+        }}
+      />
+    </Grid>
+  </>
+);
 
 export default ListingForm;
