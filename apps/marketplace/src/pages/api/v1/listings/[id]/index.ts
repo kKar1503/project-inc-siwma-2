05df3e9b49c6a57127049ac43afcf1763333fd09
Javier <JAVIERFOKJF11.21@ichat.sp.edu.sg>
2023-05-14 23:15:52 +0800

    fix: fixing lint errors

--- apps/marketplace/src/pages/api/v1/listings/[id]/index.ts
+++ apps/marketplace/src/pages/api/v1/listings/[id]/index.ts
@@ -1,7 +1,9 @@
-import { apiHandler, formatAPIResponse } from '@/utils/api';
+import { apiHandler, formatAPIResponse, parseToNumber } from '@/utils/api';
 import PrismaClient from '@inc/db';
 import { NotFoundError, ForbiddenError } from '@inc/errors';
 import { formatSingleListingResponse, getQueryParameters, parseListingId } from '..';
+import z from 'zod';
+import { ListingType } from '@prisma/client';
 
 // -- Functions --//
 
@@ -40,6 +42,25 @@ interface Parameter {
   value: string;
 }
 
+// Define the schema for the request body
+const putListingRequestBody = z.object({
+  name: z.string(),
+  description: z.string(),
+  price: z.number(),
+  unitPrice: z.boolean().optional(),
+  negotiable: z.boolean().optional(),
+  categoryId: z.number(),
+  type: z.nativeEnum(ListingType),
+  listingsParametersValues: z
+    .array(
+      z.object({
+        paramId: z.string(),
+        value: z.string(),
+      })
+    )
+    .optional(),
+});
+
 export default apiHandler()
   .get(async (req, res) => {
     const queryParams = getQueryParameters.parse(req.query);
@@ -69,9 +90,8 @@ export default apiHandler()
       throw new ForbiddenError();
     }
 
-    // Update the listing with the request data
-    const data = req.body;
-
+    // Validate the request body
+    const data = putListingRequestBody.parse(req.body);
 
     // Do not remove this, it is necessary to update the listing
     const updatedListing = await PrismaClient.listing.update({
@@ -100,15 +120,14 @@ export default apiHandler()
         return PrismaClient.listingsParametersValue.upsert({
           where: {
             listingId_parameterId: {
-              // parameterId: parseInt(parameter.paramId),
-              parameterId: parseInt(parameter.paramId),
+              parameterId: parseToNumber(parameter.paramId, 'paramId'),
               listingId: id,
             },
           },
           update: { value: parameter.value },
           create: {
             value: parameter.value,
-            parameterId: parseInt(parameter.paramId),
+            parameterId: parseToNumber(parameter.paramId, 'paramId'),
             listingId: id,
           },
         });
