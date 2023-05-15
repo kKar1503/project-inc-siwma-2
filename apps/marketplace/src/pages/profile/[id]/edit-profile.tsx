64ac9e46e6decf46d43b9d037dc1710f84d2b618
Charmaine Choo <charmainechoo288@gmail.com>
2023-05-15 16:56:42 +0800

    refactor: set props

--- apps/marketplace/src/pages/profile/[id]/edit-profile.tsx
+++ apps/marketplace/src/pages/profile/[id]/edit-profile.tsx
@@ -19,6 +19,118 @@ import ProfileDetailCard, {
   ProfileDetailCardProps,
 } from '@/components/marketplace/profile/ProfileDetailCard';
 
+const profileDetailData = [
+  {
+    ownerId: 1,
+    username: 'diggers',
+    name: 'John Tan',
+    email: 'digs@gmail.com',
+    company: 'Prof. Digging Ltd.',
+    profilePic: 'J',
+    mobileNumber: '2314 5324',
+    telegramUsername: '@digpeople',
+    bio: 'Introducing Professional Digging Limited, a leading mining company with a proven track record of excellence in the industry. With decades of experience in the mining business, we have established ourselves as a trusted and reliable provider of high-quality minerals and metals.',
+    rating: 3.3,
+    reviews: 336,
+  },
+  {
+    ownerId: 2,
+    username: 'rock_hound',
+    name: 'Emily Stone',
+    email: 'emily.stone@gmail.com',
+    company: 'Stone Exploration Co.',
+    profilePic: 'E',
+    mobileNumber: '4590 2379',
+    telegramUsername: '@stone_explorer',
+    bio: 'At Stone Exploration Co., we specialize in the exploration and development of new mineral resources. Our team of experts uses cutting-edge technology to identify promising mineral deposits and assess their potential for commercial mining. We are dedicated to responsible mining practices that prioritize the safety of our workers and the protection of the environment.',
+    rating: 4.6,
+    reviews: 97,
+  },
+  {
+    ownerId: 3,
+    username: 'ore_king',
+    name: 'David Hill',
+    email: 'david.hill@orekingdom.com',
+    company: 'Ore Kingdom Inc.',
+    profilePic: 'D',
+    mobileNumber: '9031 2150',
+    telegramUsername: '@ore_kingdom',
+    bio: 'Ore Kingdom Inc. is a leading provider of iron ore and other minerals to markets around the world. We have extensive experience in mining and processing high-quality ores and are committed to sustainable and responsible practices. Our team is dedicated to delivering the best possible products and services to our customers.',
+    rating: 4.8,
+    reviews: 215,
+  },
+  {
+    ownerId: 4,
+    username: 'gold_digger',
+    name: 'Sarah Jones',
+    email: 'sjones@golddigger.com',
+    company: 'Gold Digger Mining Corp.',
+    profilePic: 'S',
+    mobileNumber: '5678 1234',
+    telegramUsername: '@gold_digger',
+    bio: 'Gold Digger Mining Corp. is a trusted provider of gold and other precious metals. Our team of experts has decades of experience in mining and processing high-quality ores, and we are committed to responsible practices that prioritize safety, sustainability, and environmental protection. We strive to exceed the expectations of our customers and provide them with the best possible products and services.',
+    rating: 4.3,
+    reviews: 184,
+  },
+  {
+    ownerId: 5,
+    username: 'mining_pro',
+    name: 'Jackie Lee',
+    email: 'jackie.lee@miningpro.com',
+    company: 'Mining Pro Solutions',
+    profilePic: 'J',
+    mobileNumber: '7777 9999',
+    telegramUsername: '@mining_pro',
+    bio: 'Mining Pro Solutions provides cutting-edge solutions for the mining industry. Our team of experienced professionals specializes in the design, engineering, and implementation of advanced mining equipment and technologies that improve efficiency, safety, and productivity. We are committed to delivering the best possible solutions to our clients and helping them achieve their goals.',
+    rating: 4.9,
+    reviews: 102,
+  },
+  {
+    ownerId: 6,
+    username: 'ore_genius',
+    name: 'Karen Chen',
+    email: 'karen.chen@oregenius.com',
+    company: 'Ore Genius Inc.',
+    profilePic: 'K',
+    mobileNumber: '1234 5678',
+    telegramUsername: '@ore_genius',
+    bio: 'Ore Genius Inc. is a cutting-edge provider of mineral exploration and analysis services. Our team of experts uses the latest technology to identify and assess mineral deposits, helping our clients make informed decisions about their mining operations. We are committed to delivering high-quality services that exceed our clients expectations and contribute to sustainable and responsible mining practices.',
+    rating: 4.5,
+    reviews: 76,
+  },
+];
+
+export const getServerSideProps = async ({ query }: { query: any }) => {
+  const { id } = query;
+  if (!Number.isInteger(parseFloat(id))) {
+    return {
+      redirect: {
+        destination: '/',
+      },
+    };
+  }
+
+  if (id > profileDetailData.length) {
+    return {
+      redirect: {
+        destination: '/',
+      },
+    };
+  }
+
+  const data = profileDetailData[id - 1];
+
+  return {
+    props: {
+      data,
+    },
+  };
+};
+
+export type ProfilePageProps = {
+  data: ProfileDetailCardProps;
+};
+
 const EditProfile = ({ data }: { data: ProfileDetailCardProps }) => {
   const [profilePic, setProfilepic] = useState<File | null>(null);
   const [name, setName] = useState('');
@@ -311,17 +423,17 @@ const EditProfile = ({ data }: { data: ProfileDetailCardProps }) => {
               </Card>
 
               <ProfileDetailCard
-                ownerId={1}
-                username="gold_digger"
-                name="Sarah Jones"
-                email="sjones@golddigger.com"
-                company="Gold Digger Mining Corp"
-                profilePic="G"
-                mobileNumber={56781234}
-                telegramUsername="gold_digger"
-                bio="Gold Digger Mining Corp. is a trusted provider of gold and other precious metals. Our team of experts has decades of experience in mining and processing high-quality ores, and we are committed to responsible practices that prioritize safety, sustainability, and environmental protection. We strive to exceed the expectations of our customers and provide them with the best possible products and services."
-                rating={3.3}
-                reviews={180}
+                ownerId={data.ownerId}
+                username={data.username}
+                name={data.name}
+                company={data.company}
+                email={data.email}
+                profilePic={data.profilePic}
+                telegramUsername={data.telegramUsername}
+                bio={data.bio}
+                mobileNumber={data.mobileNumber}
+                rating={data.rating}
+                reviews={data.reviews}
               />
             </Box>
           </form>
