cc20e2497504ba31dbeb367e0c0357e2568c03ef
Javier <JAVIERFOKJF11.21@ichat.sp.edu.sg>
2023-05-17 00:12:33 +0800

    refactor: reverting commits

--- apps/marketplace/src/pages/api/v1/categories/[id]/index.ts
+++ apps/marketplace/src/pages/api/v1/categories/[id]/index.ts
@@ -23,7 +23,7 @@ function parseId(id: string | undefined): number {
   if (!id) {
     throw new ParamError('id');
   }
-  return parseToNumber(id, 'id');
+  return parseToNumber(id);
 }
 
 function formatResponse(response: queryResult): getResponse {
