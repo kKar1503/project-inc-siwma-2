17c5733e51267b2e3ee11a1cbb1a4b1da299bb9a
Ong Hin Journ <onghinjourn@gmail.com>
2023-05-17 03:22:53 +0800

    fix: :bug: adds requested fixes
    changes no of threads in buildscript to 4, modifies string utils to comments

--- packages/utils/stringUtils.ts
+++ packages/utils/stringUtils.ts
@@ -1,11 +1,9 @@
-const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-
 /**
  * Capitalize the first letter of a string
  * @param text The text to capitalize the first letter of
  * @returns The text with the first letter capitalized
  */
-const capitalizeFirstLetter = (text: string) =>
+export const capitalizeFirstLetter = (text: string) =>
   // Capitalize the first letter of a string
   text.charAt(0).toUpperCase() + text.slice(1);
 
@@ -17,7 +15,7 @@ const capitalizeFirstLetter = (text: string) =>
  * @param {string} joinWord The word to be used while joining the last word
  * @returns A readable string
  */
-const arrayToString = (arr: (string | number)[], joinWord: string) => {
+export const arrayToString = (arr: (string | number)[], joinWord: string) => {
   // Remove the last element from the array
   const arrWithoutLast = [...arr];
   const poppedElement = arrWithoutLast.pop();
@@ -33,14 +31,13 @@ const arrayToString = (arr: (string | number)[], joinWord: string) => {
 
 /**
  * Generates a random URL safe string
- * @param {number} length The length of the string to generate
- * @returns A random string
  * @example
  * generateString(10); // Returns 'aBcDeFgHiJ'
  * generateString(5); // Returns 'aBcDe'
  **/
-function generateString(length: number): string {
-  let result = ' ';
+export function generateString(length: number): string {
+  let result = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
   const charactersLength = characters.length;
 
   for (let i = 0; i < length; i++) {
@@ -49,5 +46,3 @@ function generateString(length: number): string {
 
   return result;
 }
-
-export { capitalizeFirstLetter, arrayToString, generateString };
