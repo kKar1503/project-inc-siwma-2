f52674d0b5b934a840bc22ec2ca64a347de4d551
Shelby <yeoxhshelby20.21@ichat.sp.edu.sg>
2023-05-19 01:10:55 +0800

    feat: added code to match reponse in docs

--- apps/chat/src/events/newMessage.ts
+++ apps/chat/src/events/newMessage.ts
@@ -5,11 +5,15 @@ import { EVENTS } from '@inc/events';
 const newMsgEvent: EventFile = (io) => ({
   eventName: EVENTS.CLIENT.SEND_MESSAGE,
   type: 'on',
-  callback: ({roomId, message, username, time}) => {
-    logger.info(`New message: ${message}`)
+  callback: ({ roomId, message, username, contentType, time }) => {
+    logger.info(`New message: ${message}`);
     io.to(roomId).emit(EVENTS.SERVER.ROOM_MESSAGE, {
-      message, username, time
-    })
+      sender: username,
+      message: message,
+      messageType: contentType,
+      room: roomId,
+      timestamp: time,
+    });
   },
 });
 
