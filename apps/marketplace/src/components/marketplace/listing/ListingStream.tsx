99c30c5f8c4fd32e65bae5bd021538be087f132d
George Yin <georgeyinxu@gmail.com>
2023-05-18 21:39:02 +0800

    fixed the infinite horizontal scroll

--- apps/marketplace/src/components/marketplace/listing/ListingStream.tsx
+++ apps/marketplace/src/components/marketplace/listing/ListingStream.tsx
@@ -1,25 +1,44 @@
 /* eslint-disable arrow-body-style */
 import React, { useState } from 'react';
+import { Box, styled } from '@mui/material';
 import ProductListingItem, { ProductListingItemProps } from './ProductListingItem';
 
 type Props = {
   listingItemsData: Array<ProductListingItemProps>;
 };
-const ListingStream: React.FC<Props> = ({ listingItemsData }) => {
-  const [hello, setHello] = useState('');
 
+const Scroll = styled('div')({
+  '@keyframes scroll': {
+    '0%': {
+      transform: 'translateX(0)',
+    },
+    '100%': {
+      transform: 'translateX(calc(-288px * 7 + (-48px * 7)))',
+    },
+  },
+});
+
+const ListingStream: React.FC<Props> = ({ listingItemsData }) => {
   return (
-    <div className="slider">
-      <div className="slide-track">
-        { listingItemsData.map((item) => {
-          return (
-            <div className='slide'>
-              <ProductListingItem {...item} />
-            </div>
-          )
-        })}
-      </div>
-    </div>
+    <Scroll sx={{paddingTop: '2rem'}}>
+      <Box sx={{ margin: 'auto', overflow: 'hidden', position: 'relative', width: 'auto' }}>
+        <Box
+          sx={{
+            animation: 'scroll 32s linear infinite',
+            display: 'flex',
+            width: 'calc(288px * 14 + (48px * 14))'
+          }}
+        >
+          {listingItemsData.map((item) => {
+            return (
+              <Box sx={{ width: '288px', margin: '0 24px' }}>
+                <ProductListingItem {...item} />
+              </Box>
+            );
+          })}
+        </Box>
+      </Box>
+    </Scroll>
   );
 };
 
