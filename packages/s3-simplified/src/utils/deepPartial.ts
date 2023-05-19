cc58a7bda88bb97fb98f0f05eed5f68f1f988b5f
Cat <45095604+catcd1w3r5@users.noreply.github.com>
2023-05-19 12:36:23 +0800

    refactor: rewrote pruneAndMerge function to have use proper types

--- packages/s3-simplified/src/utils/deepPartial.ts
+++ packages/s3-simplified/src/utils/deepPartial.ts
@@ -1,5 +1,7 @@
+import { OptionalConfig } from '../interfaces/config';
+
 export type DeepPartial<T> = T extends object ? {
-    [P in keyof T]?: DeepPartial<T[P]>;
+  [P in keyof T]?: DeepPartial<T[P]>;
 } : T;
 
 /**
@@ -8,15 +10,27 @@ export type DeepPartial<T> = T extends object ? {
  * @param defaultValue
  * @param customValue
  */
-export function pruneAndMerge(defaultValue: any, customValue: any): any {
-    if (defaultValue === undefined || defaultValue === null) return undefined;
-    if (customValue === undefined ||customValue === null) return defaultValue;
-    if (typeof defaultValue !== "object") return customValue;
-    if (Array.isArray(defaultValue)) return defaultValue.map(pruneAndMerge);
-    const result: Record<string, any> = {};
-    for (const key of Object.keys(defaultValue)) {
-        const value = pruneAndMerge(defaultValue[key], customValue[key]);
-        if (value !== undefined) result[key] = value;
-    }
-    return result;
+export function pruneAndMerge(defaultValue: OptionalConfig, customValue: DeepPartial<OptionalConfig>): any {
+  return {
+    signedUrl: customValue.signedUrl ? {
+      expiration: customValue.signedUrl.expiration || defaultValue.signedUrl.expiration,
+    } : defaultValue.signedUrl,
+
+    objectCreation: customValue.objectCreation ? {
+
+      multiPartUpload: customValue.objectCreation.multiPartUpload ? {
+        maxPartSize: customValue.objectCreation.multiPartUpload.maxPartSize || defaultValue.objectCreation.multiPartUpload.maxPartSize,
+        enabledThreshold: customValue.objectCreation.multiPartUpload.enabledThreshold || defaultValue.objectCreation.multiPartUpload.enabledThreshold,
+      } : defaultValue.objectCreation.multiPartUpload,
+
+      appendFileTypeToKey: customValue.objectCreation.appendFileTypeToKey || defaultValue.objectCreation.appendFileTypeToKey,
+
+      hash: customValue.objectCreation.hash ? {
+        function: customValue.objectCreation.hash.function || defaultValue.objectCreation.hash.function,
+        requireBuffer: customValue.objectCreation.hash.requireBuffer || defaultValue.objectCreation.hash.requireBuffer,
+        requireMetadata: customValue.objectCreation.hash.requireMetadata || defaultValue.objectCreation.hash.requireMetadata,
+      } : defaultValue.objectCreation.hash,
+
+    } : defaultValue.objectCreation,
+  };
 }
