cf93db3df276175e1509648add8f3b35299efb8e
Adeeb <adityaadeeb04@gmail.com>
2023-05-20 01:52:08 +0800

    refactor : fixing index.js files

--- apps/chat/src/events/index.ts
+++ apps/chat/src/events/index.ts
@@ -1,6 +1,3 @@
 export { default as roomMessage } from './roomMessage';
-<<<<<<< HEAD
 export { default as deleteMessage } from './deleteMessage';
-=======
 export { default as newMsgEvent } from './newMessage';
->>>>>>> dev
