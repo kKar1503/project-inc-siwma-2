e5eebc7211d473d80c6d9c2e63195e85446afca3
Xavier Teo <xavierteo26@gmail.com>
2023-05-21 03:18:02 +0800

    refactor: :truck: rename default exports of client zod schemas

--- apps/marketplace/src/utils/api/client/zod/index.ts
+++ apps/marketplace/src/utils/api/client/zod/index.ts
@@ -1,14 +1,14 @@
 /**
  * Export all zod schemas
  */
-export { default as advertisements } from './advertisements';
-export { default as categories } from './categories';
-export { default as chat } from './chat';
-export { default as companies } from './companies';
-export { default as invites } from './invites';
-export { default as listings } from './listings';
-export { default as parameters } from './parameters';
-export { default as users } from './users';
+export { default as advertSchemas } from './advertisements';
+export { default as categorySchemas } from './categories';
+export { default as chatSchemas } from './chat';
+export { default as companySchemas } from './companies';
+export { default as inviteSchemas } from './invites';
+export { default as listingSchemas } from './listings';
+export { default as paramSchemas } from './parameters';
+export { default as userSchemas } from './users';
 
 export * from './categories';
 export * from './companies';
