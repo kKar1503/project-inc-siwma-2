111bed8388ed225fdb4eb673503edf190a934864
Xavier Teo <xavierteo26@gmail.com>
2023-05-20 22:28:07 +0800

    refactor: :truck: move server-side zod schemas for invites endpoints

--- apps/marketplace/src/utils/api/server/zod/index.ts
+++ apps/marketplace/src/utils/api/server/zod/index.ts
@@ -1,3 +1,4 @@
 export { default as categoriesSchema } from './categories';
 export { default as chatSchema } from './chat';
 export { default as companiesSchema } from './companies';
+export { default as inviteSchema } from './invites';
