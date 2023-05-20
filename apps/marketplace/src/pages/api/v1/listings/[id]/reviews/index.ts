c2b152bf49bfa561f5e1075a0c1cd90431b9e2aa
Javier <JAVIERFOKJF11.21@ichat.sp.edu.sg>
2023-05-20 23:18:39 +0800

    fix: fixing sortBy

--- apps/marketplace/src/pages/api/v1/listings/[id]/reviews/index.ts
+++ apps/marketplace/src/pages/api/v1/listings/[id]/reviews/index.ts
@@ -1,7 +1,7 @@
 import { NextApiResponse } from 'next';
 import { apiHandler, formatAPIResponse } from '@/utils/api';
 import { NotFoundError, ForbiddenError } from '@inc/errors';
-import PrismaClient from '@inc/db';
+import PrismaClient, { Prisma } from '@inc/db';
 import { APIRequestType } from '@/types/api-types';
 import { parseListingId } from '../../index';
 import { checkListingExists } from '../index';
@@ -15,46 +15,45 @@ import { z } from 'zod';
  */
 // Define the Zod validation schema
 const reviewRequestBody = z.object({
-    review: z.string(),
-    rating: z.number().min(0).max(5),
+  review: z.string(),
+  rating: z.number().min(0).max(5),
 });
 
 const getListingReviews = async (req: APIRequestType, res: NextApiResponse) => {
-    const id = parseListingId(req.query.id as string);
-    const listing = await checkListingExists(id);
+  const id = parseListingId(req.query.id as string);
+  const listing = await checkListingExists(id);
 
-    if (!listing) {
-        throw new NotFoundError(`Listing with id '${id}' not found.`);
-    }
+  if (!listing) {
+    throw new NotFoundError(`Listing with id '${id}' not found.`);
+  }
 
-    let orderBy: Prisma.ReviewsOrderByWithRelationInput = {};
+  let orderBy: Prisma.ReviewsOrderByWithRelationInput = {};
 
-    switch (req.query.sortBy) {
-        case 'recent_newest':
-            orderBy = { createdAt: 'desc' };
-            break;
-        case 'recent_oldest':
-            orderBy = { createdAt: 'asc' };
-            break;
-        case 'highest_rating':
-            orderBy = { rating: 'desc' };
-            break;
-        case 'lowest_rating':
-            orderBy = { rating: 'asc' };
-            break;
-        default:
-            break;
-    }
+  switch (req.query.sortBy) {
+    case 'recent_newest':
+      orderBy = { createdAt: 'desc' };
+      break;
+    case 'recent_oldest':
+      orderBy = { createdAt: 'asc' };
+      break;
+    case 'highest_rating':
+      orderBy = { rating: 'desc' };
+      break;
+    case 'lowest_rating':
+      orderBy = { rating: 'asc' };
+      break;
+    default:
+      break;
+  }
 
+  const reviews = await PrismaClient.reviews.findMany({
+    where: {
+      listing: id,
+    },
+    orderBy: orderBy,
+  });
 
-    const reviews = await PrismaClient.reviews.findMany({
-        where: {
-            listing: id,
-        },
-        orderBy: orderBy,
-    });
-
-    res.status(200).json(reviews);
+  res.status(200).json(reviews);
 };
 
 /**
@@ -65,37 +64,33 @@ const getListingReviews = async (req: APIRequestType, res: NextApiResponse) => {
  * @returns The newly created review
  */
 const createListingReview = async (req: APIRequestType, res: NextApiResponse) => {
-    const id = parseListingId(req.query.id as string);
-    const userId = req.token?.user?.id;
-    const userRole = req.token?.user?.role;
+  const id = parseListingId(req.query.id as string);
+  const userId = req.token?.user?.id;
+  const userRole = req.token?.user?.role;
 
-    const listing = await checkListingExists(id);
-    const { review, rating } = reviewRequestBody.parse(req.body);
+  const listing = await checkListingExists(id);
+  const { review, rating } = reviewRequestBody.parse(req.body);
 
-    const isOwner = listing.owner === userId;
-    const isAdmin = userRole && userRole >= 1;
-    const sameCompany = req.token?.user?.company === listing.users.companyId.toString();
+  const isOwner = listing.owner === userId;
+  const isAdmin = userRole && userRole >= 1;
+  const sameCompany = req.token?.user?.company === listing.users.companyId.toString();
 
-    // Check that the request is coming from the owner, an admin, or someone from the same company
-    if (!isOwner && !isAdmin && !sameCompany) {
-        throw new ForbiddenError();
-    }
+  // Check that the request is coming from the owner, an admin, or someone from the same company
+  if (!isOwner && !isAdmin && !sameCompany) {
+    throw new ForbiddenError();
+  }
 
-    // Add the new review
-    const createdReview = await PrismaClient.reviews.create({
-        data: {
-            id: 10,
-            review: review,
-            rating: rating,
-            user: userId,
-            listing: id,
-        },
-    });
-    res.status(201).json(formatAPIResponse({ createdReview }));
+  // Add the new review
+  const createdReview = await PrismaClient.reviews.create({
+    data: {
+      id: 10,
+      review: review,
+      rating: rating,
+      user: userId,
+      listing: id,
+    },
+  });
+  res.status(201).json(formatAPIResponse({ createdReview }));
 };
 
-export default apiHandler()
-    .get(getListingReviews)
-    .post(createListingReview);
-
-
+export default apiHandler().get(getListingReviews).post(createListingReview);
