f5266e3f7cca2a7c5d9c4fc3e06cd70e60077f53
Xavier Teo <xavierteo26@gmail.com>
2023-05-20 17:34:58 +0800

    refactor: :truck: move all response zod schemas to /utils/api/client/zod

---
+++ apps/marketplace/src/utils/api/client/zod/index.ts
@@ -0,0 +1,11 @@
+/**
+ * Export all zod schemas
+ */
+export * from './advertisements';
+export * from './categories';
+export * from './chat';
+export * from './company';
+export * from './invites';
+export * from './listings';
+export * from './parameters';
+export * from './users';
