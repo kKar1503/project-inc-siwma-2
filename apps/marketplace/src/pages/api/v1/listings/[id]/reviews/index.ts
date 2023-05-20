208d7d45450e9a4ca1ae93b331c4997a7af4113b
Adeeb <adityaadeeb04@gmail.com>
2023-05-20 22:43:23 +0800

    feat : wip reviews code

--- apps/marketplace/src/pages/api/v1/listings/[id]/reviews/index.ts
+++ apps/marketplace/src/pages/api/v1/listings/[id]/reviews/index.ts
@@ -1,6 +1,6 @@
 import { NextApiResponse } from 'next';
-import { apiHandler, } from '@/utils/api';
-import { NotFoundError } from '@inc/errors';
+import { apiHandler } from '@/utils/api';
+import { NotFoundError, ForbiddenError } from '@inc/errors';
 import PrismaClient from '@inc/db';
 import { APIRequestType } from '@/types/api-types';
 import { parseListingId } from '../../index';
@@ -43,11 +43,51 @@ const getListingReviews = async (req: APIRequestType, res: NextApiResponse) => {
         where: {
             listing: id,
         },
-        orderBy,
     });
 
     res.status(200).json(reviews);
 };
 
+/**
+ * Adds a review for a listing
+ * @param id The listing id
+ * @param review The review text
+ * @param rating The rating score
+ * @returns The newly created review
+ */
+const createListingReview = async (req: APIRequestType, res: NextApiResponse) => {
+    const id = parseListingId(req.query.id as string);
+    const userId = req.token?.user?.id;
+    const userRole = req.token?.user?.role;
+
+    const listing = await checkListingExists(id);
+    const { review, rating } = req.body;
+
+    const isOwner = listing.owner === userId;
+    const isAdmin = userRole && userRole >= 1;
+    const sameCompany = req.token?.user?.company === listing.users.companyId.toString();
+
+    // Check that the request is coming from the owner, an admin, or someone from the same company
+    if (!isOwner && !isAdmin && !sameCompany) {
+        throw new ForbiddenError();
+    }
+
+    // Add the new review
+    const createdReview = await PrismaClient.reviews.create({
+        data: {
+            id: 7,
+            review: review,
+            rating: rating,
+            user: userId,
+            listing: id,
+        },
+    });
+
+    res.status(201).json(createdReview);
+};
+
 export default apiHandler()
-    .get(getListingReviews);
+    .get(getListingReviews)
+    .post(createListingReview);
+
+
