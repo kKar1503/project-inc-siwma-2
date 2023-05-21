fcf105685d2d1f5923403319ce2c81195e977053
yiningsp <yyiningsp@gmail.com>
2023-05-21 20:49:34 +0800

    refactor: removing unuse screen size
    removing unuse screen size xs and xl

--- packages/ui/lib/hook/useResponsiveness.tsx
+++ packages/ui/lib/hook/useResponsiveness.tsx
@@ -3,20 +3,16 @@ import type { Breakpoint } from '@mui/material';
 
 
 export default function useResponsiveness(breakpoints: Breakpoint[]) {
-  const { xs, sm, md, lg, xl } = useTheme().breakpoints.values;
+  const { sm, md, lg } = useTheme().breakpoints.values;
 
-  const isXs: boolean = useMediaQuery(`(min-width: ${xs}px)`);
   const isSm: boolean = useMediaQuery(`(min-width:${sm}px)`);
   const isMd: boolean = useMediaQuery(`(min-width: ${md}px)`);
   const isLg: boolean = useMediaQuery(`(min-width: ${lg}px)`);
-  const isXl: boolean = useMediaQuery(`(min-width: ${xl}px)`);
 
   const bpMap = {
-    xs: isXs,
     sm: isSm,
     md: isMd,
     lg: isLg,
-    xl: isXl,
   };
 
   const isBp: boolean[] = breakpoints.map((bp: Breakpoint) => bpMap[bp]);
