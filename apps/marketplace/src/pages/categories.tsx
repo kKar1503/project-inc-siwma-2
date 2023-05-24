6780bace1de1b52b34608d4c256faa83e329299a
Mokke-31 <107909574+Mokke-31@users.noreply.github.com>
2023-05-24 22:03:22 +0800

    style: Responsive font
    Made the font for category page responsive for various screen sizes

--- apps/marketplace/src/pages/categories.tsx
+++ apps/marketplace/src/pages/categories.tsx
@@ -1,3 +1,4 @@
+import { Dispatch, SetStateAction, useState } from 'react';
 import Grid from '@mui/material/Grid';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
@@ -5,9 +6,33 @@ import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import { CardActionArea } from '@mui/material';
-import { useTheme } from '@mui/material/styles';
+import { useQuery } from 'react-query';
+import categories, { CategoryResponseBody } from '@/utils/api/client/zod/categories';
 
-const categoryPageData = [
+// export interface Image {
+//   id: string;
+//   name: string;
+//   description: string;
+//   image: string;
+//   crossSectionImage: string;
+//   active: boolean;
+// }
+
+export type CategoryPageTypes = {
+  // data: Image[];
+  data: CategoryResponseBody[]
+};
+
+const useCategoryPageQuery = (setCategoryData: Dispatch<SetStateAction<CategoryPageTypes>>) => {
+  const { data } = useQuery('catData', async () => {
+    const parsedCategoryList = categories.getAll.parse(data);
+
+    setCategoryData({ data: parsedCategoryList });
+    return parsedCategoryList;
+  });
+};
+
+const categoryExamples: CategoryResponseBody[] = [
   {
     id: '1',
     name: 'Beams',
@@ -100,109 +125,82 @@ const categoryPageData = [
   },
 ];
 
-categoryPageData.sort((a, b) => {
-  if (a.name < b.name) {
-    return -1;
-  }
-  if (a.name > b.name) {
-    return 1;
-  }
-  return 0;
-});
+const CategoriesPage = () => {
+  const [categoryData, setCategoryData] = useState<CategoryPageTypes>({ data: [] });
 
-export interface Image {
-  id: string;
-  name: string;
-  description: string;
-  image: string;
-  crossSectionIMage: string;
-  active: boolean;
-}
+  useCategoryPageQuery(setCategoryData);
 
-export type CategoryPageProps = {
-  data: Image[];
-};
-
-export const getServerSideProps = async () => {
-  // api call to get category names and images
-
-  const data = categoryPageData;
-
-  return {
-    props: {
-      data,
-    },
-  };
-};
-
-const CategoriesPage = ({ data }: CategoryPageProps) => (
-  <Box
-    sx={() => ({
-      width: '80%',
-      height: 'full',
-      mr: 'auto',
-      ml: 'auto',
-      maxHeight: 'xl',
-    })}
-  >
-    <Typography
-      sx={({ spacing }) => ({
-        pl: spacing(4),
-        pt: spacing(3),
-      })}
-      variant="h3"
+  return (
+    <Box
+      sx={{
+        width: '80%',
+        height: 'full',
+        mr: 'auto',
+        ml: 'auto',
+        maxHeight: 'xl',
+      }}
     >
-      More Metal Types
-    </Typography>
-    <Typography
-      sx={({ spacing }) => ({
-        pl: spacing(4),
-        pt: spacing(1),
-        fontWeight: 500,
-      })}
-      variant="h5"
-    >
-      Choose from over 8,000 types of shapes and grades of metals!
-    </Typography>
-
-    <Grid
-      sx={({ spacing }) => ({
-        py: spacing(4),
-      })}
-    >
-      <Grid
-        container
-        columnSpacing={3}
-        rowSpacing={3}
-        sx={{
-          direction: 'row',
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}
+      <Typography
+        sx={({ spacing }) => ({
+          pl: spacing(4),
+          pt: spacing(3),
+          fontSize: {xs: 20, md:40, lg: 50, },
+          fontWeight: 700,
+        })}
       >
-        {data.map(({ id, name, image }) => (
-          <Grid key={id} item>
-            <Card>
-              <CardActionArea>
-                <CardMedia component="img" height="140" image={image} />
-                <CardContent>
-                  <Typography
-                    gutterBottom
-                    variant="body1"
-                    sx={{
-                      fontWeight: 500,
-                    }}
-                  >
-                    {name}
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-            </Card>
-          </Grid>
-        ))}
+        More Metal Types
+      </Typography>
+      <Typography
+        sx={({ spacing }) => ({
+          pl: spacing(4),
+          pt: spacing(1),
+          fontSize: {xs: 12, md:24, lg:36, },
+          fontWeight: 500,
+        })}
+        variant="h5"
+      >
+        Choose from over 8,000 types of shapes and grades of metals!
+      </Typography>
+
+      <Grid
+        sx={({ spacing }) => ({
+          py: spacing(4),
+        })}
+      >
+        <Grid
+          container
+          columnSpacing={3}
+          rowSpacing={3}
+          sx={{
+            direction: 'row',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+        >
+          {categoryExamples.map(({ id, name, image }) => (
+            <Grid key={id} item>
+              <Card>
+                <CardActionArea>
+                  <CardMedia component="img" height="140" image={image} />
+                  <CardContent>
+                    <Typography
+                      gutterBottom
+                      variant="body1"
+                      sx={{
+                        fontWeight: 500,
+                      }}
+                    >
+                      {name}
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
       </Grid>
-    </Grid>
-  </Box>
-);
+    </Box>
+  );
+};
 
 export default CategoriesPage;
