9dbd667faccab034899cb073e898c3f11e1d7858
Yam Kar Lok <isonice4572@gmail.com>
2023-05-24 12:22:06 +0800

    feat: Added a default export for the package

--- packages/node-store/index.ts
+++ packages/node-store/index.ts
@@ -1,4 +1,8 @@
 export { default as Pointer, PointerMutator } from './pointer';
 export { default as Store, CreatableStore, MutableStore, createStoreTemplate } from './store';
-export { default as MamaStore } from './mamaStore';
-export { default as PapaStore } from './papaStore';
+export { default as MamaStore, InferStoreType } from './mamaStore';
+
+// Since entry point is the PapaStore, we will add a default export for PapaStore too.
+import PapaStore from './papaStore';
+export { PapaStore };
+export default PapaStore;
