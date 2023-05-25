51197e31739163c999d7abe41fa38c7340f1123e
Karandeep Singh <46585033+ConfuseIous@users.noreply.github.com>
2023-05-26 02:45:31 +0800

    bookmark listings

--- apps/marketplace/src/pages/api/v1/listings/[id]/index.ts
+++ apps/marketplace/src/pages/api/v1/listings/[id]/index.ts
@@ -239,4 +239,49 @@ export default apiHandler()
     });
 
     res.status(204).end();
+  })
+  .patch(async (req, res) => {
+    // Create bookmark
+    const id = parseListingId(req.query.id as string);
+    const userId = req.token?.user?.id;
+
+    const listing = await checkListingExists(id);
+
+    const isOwner = listing.owner === userId;
+
+    if (isOwner) {
+      // Users are not allowed to bookmark their own listings
+      throw new ForbiddenError();
+    }
+
+    // Check if bookmark already exists
+    const bookmark = await PrismaClient.listingBookmarks.findFirst({
+      where: {
+        listingId: id,
+        userId,
+      },
+    });
+
+    /* If the bookmark exists, we're going to remove it.
+     * If the bookmark doesn't exist, we're going to create it.
+     * Hence, bookmarked will be true if the bookmark doesn't exist yet.
+     */
+    const bookmarked = bookmark === null;
+
+    if (bookmark) {
+      await PrismaClient.listingBookmarks.delete({
+        where: {
+          id: bookmark.id,
+        },
+      });
+    } else {
+      await PrismaClient.listingBookmarks.create({
+        data: {
+          listingId: id,
+          userId,
+        },
+      });
+    }
+
+    res.status(200).json(formatAPIResponse({ bookmarked }));
   });
