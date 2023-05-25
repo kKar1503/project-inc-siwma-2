4e63ac833589e742f9299dcf729f041437253704
Cat <45095604+catcd1w3r5@users.noreply.github.com>
2023-05-25 19:03:12 +0800

    feat: integrated delete message with db

--- apps/chat/src/events/deleteMessage.ts
+++ apps/chat/src/events/deleteMessage.ts
@@ -1,14 +1,21 @@
 import { EventFile } from '@inc/types';
 import logger from '../utils/logger';
 import { EVENTS } from '@inc/events';
+import prisma from '@inc/db';
 
 const deleteMessageEvent: EventFile = (io) => ({
-    eventName: EVENTS.CLIENT.DELETE_MESSAGE,
-    type: 'on',
-    callback: async ({ room, message }) => {
-        logger.info(`${message}  ${room}`);
-        io.to(room).emit(EVENTS.SERVER.DELETE_MESSAGE, message);
-    },
+  eventName: EVENTS.CLIENT.DELETE_MESSAGE,
+  type: 'on',
+  callback: async ({ room, messageId }) => {
+    logger.info(`${messageId}  ${room}`);
+    io.to(room).emit(EVENTS.SERVER.DELETE_MESSAGE, messageId);
+
+    prisma.messages.delete({
+      where: {
+        id: messageId,
+      },
+    });
+  },
 });
 
 export default deleteMessageEvent;
