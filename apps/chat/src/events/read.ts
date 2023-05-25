9962af8e1337b35a5f9c87d821562a57e6657455
Cat <45095604+catcd1w3r5@users.noreply.github.com>
2023-05-25 19:03:30 +0800

    feat: integrated read message event with db

--- apps/chat/src/events/read.ts
+++ apps/chat/src/events/read.ts
@@ -1,13 +1,23 @@
 import { EventFile } from '@inc/types';
 import logger from '../utils/logger';
 import { EVENTS } from '@inc/events';
+import prisma from '@inc/db';
+
 
 const readEvent: EventFile = (io) => ({
   eventName: EVENTS.CLIENT.READ,
   type: 'on',
-  callback: ({ room, message }) => {
-    logger.info(`Message ${message} read`);
-    io.to(room).emit(EVENTS.SERVER.READ, { room: room, message: message });
+  callback: ({ room, messageId }) => {
+    logger.info(`Message ${messageId} read`);
+    io.to(room).emit(EVENTS.SERVER.READ, { room: room, message: messageId });
+    prisma.messages.update({
+      where: {
+        id: messageId,
+      },
+      data: {
+        read: true,
+      },
+    });
   },
 });
 
