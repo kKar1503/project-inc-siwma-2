ce3c610801e9343d01d99dceaff5c22a8f5179ec
Cat <45095604+catcd1w3r5@users.noreply.github.com>
2023-05-25 19:03:01 +0800

    feat: integrated new messages with db

--- apps/chat/src/events/newMessage.ts
+++ apps/chat/src/events/newMessage.ts
@@ -1,6 +1,7 @@
 import { EventFile } from '@inc/types';
 import logger from '../utils/logger';
 import { EVENTS } from '@inc/events';
+import prisma, { ContentType } from '@inc/db';
 
 const newMsgEvent: EventFile = (io) => ({
   eventName: EVENTS.CLIENT.SEND_MESSAGE,
@@ -13,6 +14,14 @@ const newMsgEvent: EventFile = (io) => ({
       messageType: contentType,
       room: roomId,
       timestamp: time,
+    });
+    prisma.messages.create({
+      data: {
+        content: message,
+        contentType: contentType as ContentType,
+        room: roomId,
+        author: username,
+      },
     });
   },
 });
