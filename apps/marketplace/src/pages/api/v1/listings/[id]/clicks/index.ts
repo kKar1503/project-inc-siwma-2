126b2f95aaf99f8811aa10c0c112c1e848f2bb93
Cat <45095604+catcd1w3r5@users.noreply.github.com>
2023-05-28 07:03:38 +0800

    fix: swapped listing and user

--- apps/marketplace/src/pages/api/v1/listings/[id]/clicks/index.ts
+++ apps/marketplace/src/pages/api/v1/listings/[id]/clicks/index.ts
@@ -1,21 +1,21 @@
-import { apiHandler, formatAPIResponse } from '@/utils/api';
+import { apiHandler, formatAPIResponse, parseToNumber } from '@/utils/api';
 import { NextApiRequest, NextApiResponse } from 'next';
 import PrismaClient from '@inc/db';
 import { APIRequestType } from '@/types/api-types';
 import { z } from 'zod';
 
 const ParamSchema = z.object({
-  id: z.string(),
+  id: z.string().transform((val) => parseToNumber(val, 'id')),
 });
 
 const BodySchema = z.object({
-  listing: z.number(),
+  user: z.string(),
 });
 
 const POST = async (req: NextApiRequest & APIRequestType, res: NextApiResponse) => {
   // Validate query params
-  const user = ParamSchema.parse(req.query).id;
-  const { listing } = BodySchema.parse(req.body);
+  const listing = ParamSchema.parse(req.query).id;
+  const { user } = BodySchema.parse(req.body);
 
   PrismaClient.listingClicks.create({
     data: {
@@ -24,7 +24,6 @@ const POST = async (req: NextApiRequest & APIRequestType, res: NextApiResponse) 
     },
   });
 
-  // Return advertisement;
   res
     .status(200)
     .json(formatAPIResponse({}));
