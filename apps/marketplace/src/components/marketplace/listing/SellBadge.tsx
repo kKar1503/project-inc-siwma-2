e1aaf617bf2947d4ac1ea37250348b25aa256037
yiningsp <yyiningsp@gmail.com>
2023-05-29 15:04:51 +0800

    style: fix responsiveness for badges
    fix responsiveness for badges for lising page

--- apps/marketplace/src/components/marketplace/listing/SellBadge.tsx
+++ apps/marketplace/src/components/marketplace/listing/SellBadge.tsx
@@ -2,13 +2,13 @@ import { Chip } from '@mui/material';
 
 const SellBadge = () => (
   <Chip
-    sx={({ spacing, palette }) => ({
-      mr: spacing(1),
+    sx={({ palette }) => ({
+      // mr: spacing(1),
       bgcolor: palette.error[500],
       fontWeight: 'bold',
       color: palette.common.white,
     })}
-    label="Selling"
+    label="Sell"
   />
 );
 
