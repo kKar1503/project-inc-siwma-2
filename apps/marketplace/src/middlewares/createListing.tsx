ae93efb97b5f1281844b235cd51b8bf233062e12
Cat <45095604+catcd1w3r5@users.noreply.github.com>
2023-06-01 18:25:38 +0800

    feat: updated middleware to upload images via endpoint created in feat/listing-images-endpoints/xavier

--- apps/marketplace/src/middlewares/createListing.tsx
+++ apps/marketplace/src/middlewares/createListing.tsx
@@ -2,15 +2,18 @@ import apiClient from '@/utils/api/client/apiClient';
 import { PostListingsRequestBody } from '@/utils/api/server/zod/listings';
 
 // backend not confirmed yet, backend stuff still in progress...
-const createListing = async (data: { listingBody: PostListingsRequestBody, images: Blob[] } |undefined) => {
+const createListing = async (data: { listingBody: PostListingsRequestBody, images: Blob[] } | undefined) => {
   // const parsedListing = listings.create.parse(listing);
   if (data === undefined) return false;
   const { listingBody, images } = data;
   const formData = new FormData();
-  formData.append('body', JSON.stringify(listingBody));
   images.forEach((image) => formData.append('file', image));
 
-  return apiClient.post('/v1/listings', listingBody);
+  const result = await apiClient.post('/v1/listings', listingBody);
+  const { id } = result.data;
+  await apiClient.post(`/v1/listings/${id}/images`, formData);
+
+  return true;
 };
 
 export default createListing;
