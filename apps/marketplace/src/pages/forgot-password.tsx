b1bd685d03de2310a2901a187b0c21cc45f89439
Louis Pang <gxpang1@gmail.com>
2023-06-02 02:22:44 +0800

    Added responsiveness

--- apps/marketplace/src/pages/forgot-password.tsx
+++ apps/marketplace/src/pages/forgot-password.tsx
@@ -5,21 +5,66 @@ import TextField from '@mui/material/TextField';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import Image from 'next/image';
-import { FormEvent, useState } from 'react';
+import { useTheme } from '@mui/material/styles';
+import { useRouter } from 'next/router';
+import { FormEvent, useMemo, useState } from 'react';
+import useResponsiveness from '@inc/ui/lib/hook/useResponsiveness';
 
-const ForgetPW = () => {
+const ForgetPassword = () => {
   const [email, setEmail] = useState('');
+  const [isSm, isMd, isLg] = useResponsiveness(['sm', 'md', 'lg']);
+
+  const { spacing, shape, shadows, palette } = useTheme();
+  const router = useRouter();
 
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-
-    if (!email) {
-      alert('Please fill your email!');
-    } else {
-      console.log('Email: ', email);
-      setEmail('');
-    }
   };
+
+  const stylesBox = useMemo(() => {
+    if (isSm) {
+      return {
+        boxShadow: shadows[5],
+        px: '5rem',
+        pb: '10rem',
+        pt: spacing(3),
+        position: 'relative',
+        bgcolor: palette.common.white,
+        ...shape,
+      };
+    }
+    if (isMd) {
+      return {
+        boxShadow: shadows[5],
+        px: '10rem',
+        pb: '12rem',
+        pt: spacing(3),
+        position: 'relative',
+        bgcolor: palette.common.white,
+        ...shape,
+      };
+    }
+    if (isLg) {
+      return {
+        boxShadow: shadows[5],
+        px: '10rem',
+        pb: '15rem',
+        pt: spacing(3),
+        position: 'relative',
+        bgcolor: palette.common.white,
+        ...shape,
+      };
+    }
+    return {
+      boxShadow: shadows[5],
+      px: '10rem',
+      pb: '15rem',
+      pt: spacing(3),
+      position: 'relative',
+      bgcolor: palette.common.white,
+      ...shape,
+    };
+  }, [isSm, isMd, isLg]);
 
   return (
     <Box>
@@ -29,7 +74,7 @@ const ForgetPW = () => {
           backgroundSize: 'cover',
         }}
       >
-        <Image src="/images/siwma-bg.png" alt="background_pic" fill />
+        <Image src="/images/siwma-bg.png" alt="logo" fill />
         <Container
           component="main"
           maxWidth="md"
@@ -40,24 +85,14 @@ const ForgetPW = () => {
             height: '100vh',
           }}
         >
-          <Box
-            sx={({ shape, shadows, spacing, palette }) => ({
-              boxShadow: shadows[5],
-              px: '10rem',
-              pb: '8rem',
-              pt: spacing(3),
-              position: 'relative',
-              bgcolor: palette.common.white,
-              ...shape,
-            })}
-          >
+          <Box sx={stylesBox}>
             <Box
               sx={({ spacing }) => ({
                 position: 'relative',
                 margin: 'auto',
                 display: 'flex',
                 justifyContent: 'center',
-                width: '80%',
+                width: '100%',
                 height: '20%',
                 mb: spacing(2),
               })}
@@ -77,34 +112,39 @@ const ForgetPW = () => {
                   fontWeight: 'bold',
                 })}
               >
-                Forgot Password?
+                Forgot Password
               </Typography>
               <Typography
-                sx={({ spacing, typography }) => ({
-                  mt: spacing(1),
-                  fontSize: typography.body2,
+                sx={({ typography }) => ({
+                  fontSize: typography.body1,
                 })}
               >
                 Please enter your email.
               </Typography>
               <Typography
                 sx={({ typography }) => ({
-                  fontSize: typography.body2,
+                  fontSize: typography.body1,
                 })}
               >
                 Instructions will be sent to your email to recover your lost account.
               </Typography>
             </Box>
+
             <Box component="form" onSubmit={handleSubmit}>
               <TextField
                 fullWidth
+                name="email"
+                id="email"
                 label="Email"
-                placeholder="Your Email"
+                placeholder="Your company's email"
                 value={email}
+                type="email"
                 variant="standard"
+                autoFocus
                 margin="normal"
                 onChange={(e) => setEmail(e.target.value)}
               />
+
               <Button
                 type="submit"
                 fullWidth
@@ -123,4 +163,4 @@ const ForgetPW = () => {
   );
 };
 
-export default ForgetPW;
+export default ForgetPassword;
--- apps/marketplace/src/pages/register.tsx
+++ apps/marketplace/src/pages/register.tsx
@@ -5,27 +5,70 @@ import TextField from '@mui/material/TextField';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import Image from 'next/image';
-import { FormEvent, useState } from 'react';
+import { useTheme } from '@mui/material/styles';
+import { FormEvent, useMemo, useState } from 'react';
+import useResponsiveness from '@inc/ui/lib/hook/useResponsiveness';
 
-const RegisterForm = () => {
-  const [companyName, setCompanyName] = useState('');
+const Register = () => {
+  const [company, setCompany] = useState('');
   const [userName, setUserName] = useState('');
   const [email, setEmail] = useState('');
-  const [phoneNumber, setPhoneNumber] = useState('');
+  const [phone, setPhone] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
+  const [isSm, isMd, isLg] = useResponsiveness(['sm', 'md', 'lg']);
+
+  const { spacing, shape, shadows, palette } = useTheme();
+  // const router = useRouter();
 
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-
-    if (!companyName || !userName || !email || !phoneNumber || !password || !confirmPassword) {
-      alert('Please fill in all the fields!');
-    } else if (password !== confirmPassword) {
-      alert('Password does not match! Please try again');
-      setPassword('');
-      setConfirmPassword('');
-    }
   };
+
+  const stylesBox = useMemo(() => {
+    if (isSm) {
+      return {
+        boxShadow: shadows[5],
+        px: '5rem',
+        pb: '10rem',
+        pt: spacing(3),
+        position: 'relative',
+        bgcolor: palette.common.white,
+        ...shape,
+      };
+    }
+    if (isMd) {
+      return {
+        boxShadow: shadows[5],
+        px: '10rem',
+        pb: '12rem',
+        pt: spacing(3),
+        position: 'relative',
+        bgcolor: palette.common.white,
+        ...shape,
+      };
+    }
+    if (isLg) {
+      return {
+        boxShadow: shadows[5],
+        px: '10rem',
+        pb: '15rem',
+        pt: spacing(3),
+        position: 'relative',
+        bgcolor: palette.common.white,
+        ...shape,
+      };
+    }
+    return {
+      boxShadow: shadows[5],
+      px: '10rem',
+      pb: '15rem',
+      pt: spacing(3),
+      position: 'relative',
+      bgcolor: palette.common.white,
+      ...shape,
+    };
+  }, [isSm, isMd, isLg]);
 
   return (
     <Box>
@@ -35,7 +78,7 @@ const RegisterForm = () => {
           backgroundSize: 'cover',
         }}
       >
-        <Image src="/images/siwma-bg.png" alt="background_pic" fill />
+        <Image src="/images/siwma-bg.png" alt="logo" fill />
         <Container
           component="main"
           maxWidth="md"
@@ -46,26 +89,16 @@ const RegisterForm = () => {
             height: '100vh',
           }}
         >
-          <Box
-            sx={({ shape, shadows, spacing, palette }) => ({
-              boxShadow: shadows[5],
-              px: '8rem',
-              pb: '10rem',
-              pt: spacing(2),
-              position: 'relative',
-              bgcolor: palette.common.white,
-              ...shape,
-            })}
-          >
+          <Box sx={stylesBox}>
             <Box
               sx={({ spacing }) => ({
                 position: 'relative',
                 margin: 'auto',
                 display: 'flex',
                 justifyContent: 'center',
-                width: '80%',
+                width: '100%',
                 height: '20%',
-                mb: spacing(1),
+                mb: spacing(2),
               })}
             >
               <Image src="/images/siwma-logo.jpeg" alt="logo" fill />
@@ -73,7 +106,7 @@ const RegisterForm = () => {
             <Divider flexItem />
             <Box
               sx={({ spacing }) => ({
-                mb: spacing(1),
+                mb: spacing(2),
               })}
             >
               <Typography
@@ -87,85 +120,86 @@ const RegisterForm = () => {
               </Typography>
               <Typography
                 sx={({ typography }) => ({
-                  fontSize: typography.body2,
+                  fontSize: typography.body1,
                 })}
               >
                 Register your user details to create an account
               </Typography>
             </Box>
+
             <Box component="form" onSubmit={handleSubmit}>
               <TextField
                 fullWidth
-                label="Company Name"
-                placeholder="Registerd Company Name"
-                value={companyName}
+                label="Company"
+                placeholder="Company Name"
+                value={company}
                 variant="standard"
+                autoFocus
                 margin="normal"
-                onChange={(e) => setCompanyName(e.target.value)}
+                onChange={(e) => setCompany(e.target.value)}
               />
 
               <TextField
                 fullWidth
                 label="Full Name"
-                placeholder="Your Full Name"
+                placeholder="Full Name"
                 value={userName}
+                type="text"
                 variant="standard"
+                autoFocus
                 margin="normal"
                 onChange={(e) => setUserName(e.target.value)}
               />
-
               <TextField
+                fullWidth
                 label="Email"
-                placeholder="Your Email"
+                placeholder="Your Company's Email"
                 value={email}
                 variant="standard"
+                autoFocus
                 margin="normal"
                 onChange={(e) => setEmail(e.target.value)}
-                sx={{ width: '45.3%' }}
+                sx={{ width: '45%' }}
               />
-
               <TextField
+                fullWidth
                 label="Mobile Number"
                 placeholder="Your Mobile Number"
-                value={phoneNumber}
+                value={phone}
                 variant="standard"
+                autoFocus
                 margin="normal"
-                onChange={(e) => setPhoneNumber(e.target.value)}
-                sx={({ spacing }) => ({
-                  ml: spacing(4),
-                  width: '45.3%',
-                })}
+                onChange={(e) => setPhone(e.target.value)}
+                sx={{
+                  float: 'right',
+                  width: '45%',
+                }}
               />
-
               <TextField
                 fullWidth
                 label="Password"
-                placeholder="Your password"
+                placeholder="Your Password"
                 value={password}
-                type="password"
                 variant="standard"
+                autoFocus
+                margin="normal"
                 onChange={(e) => setPassword(e.target.value)}
-                sx={({ spacing }) => ({
-                  mt: spacing(1),
-                  width: '45.3%',
-                })}
+                sx={{ width: '45%' }}
               />
-
               <TextField
                 fullWidth
                 label="Confirm Password"
-                placeholder="Confirm Your password"
+                placeholder="Confirm Your Password"
                 value={confirmPassword}
-                type="password"
                 variant="standard"
+                autoFocus
+                margin="normal"
                 onChange={(e) => setConfirmPassword(e.target.value)}
-                sx={({ spacing }) => ({
-                  mt: spacing(1),
-                  ml: spacing(4),
-                  width: '45.3%',
-                })}
+                sx={{
+                  float: 'right',
+                  width: '45%',
+                }}
               />
-
               <Button
                 type="submit"
                 fullWidth
@@ -184,4 +218,192 @@ const RegisterForm = () => {
   );
 };
 
-export default RegisterForm;
+export default Register;
+
+// import Typography from '@mui/material/Typography';
+// import Button from '@mui/material/Button';
+// import Divider from '@mui/material/Divider';
+// import TextField from '@mui/material/TextField';
+// import Box from '@mui/material/Box';
+// import Container from '@mui/material/Container';
+// import Image from 'next/image';
+// import { FormEvent, useState } from 'react';
+
+// const RegisterForm = () => {
+//   const [companyName, setCompanyName] = useState('');
+//   const [userName, setUserName] = useState('');
+//   const [email, setEmail] = useState('');
+//   const [phoneNumber, setPhoneNumber] = useState('');
+//   const [password, setPassword] = useState('');
+//   const [confirmPassword, setConfirmPassword] = useState('');
+
+//   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+//     e.preventDefault();
+
+//     if (!companyName || !userName || !email || !phoneNumber || !password || !confirmPassword) {
+//       alert('Please fill in all the fields!');
+//     } else if (password !== confirmPassword) {
+//       alert('Password does not match! Please try again');
+//       setPassword('');
+//       setConfirmPassword('');
+//     }
+//   };
+
+//   return (
+//     <Box>
+//       <Box
+//         sx={{
+//           width: '100vw',
+//           backgroundSize: 'cover',
+//         }}
+//       >
+//         <Image src="/images/siwma-bg.png" alt="background_pic" fill />
+//         <Container
+//           component="main"
+//           maxWidth="md"
+//           sx={{
+//             justifyContent: 'center',
+//             display: 'flex',
+//             flexDirection: 'column',
+//             height: '100vh',
+//           }}
+//         >
+//           <Box
+//             sx={({ shape, shadows, spacing, palette }) => ({
+//               boxShadow: shadows[5],
+//               px: '8rem',
+//               pb: '10rem',
+//               pt: spacing(2),
+//               position: 'relative',
+//               bgcolor: palette.common.white,
+//               ...shape,
+//             })}
+//           >
+//             <Box
+//               sx={({ spacing }) => ({
+//                 position: 'relative',
+//                 margin: 'auto',
+//                 display: 'flex',
+//                 justifyContent: 'center',
+//                 width: '80%',
+//                 height: '20%',
+//                 mb: spacing(1),
+//               })}
+//             >
+//               <Image src="/images/siwma-logo.jpeg" alt="logo" fill />
+//             </Box>
+//             <Divider flexItem />
+//             <Box
+//               sx={({ spacing }) => ({
+//                 mb: spacing(1),
+//               })}
+//             >
+//               <Typography
+//                 sx={({ spacing, typography }) => ({
+//                   fontSize: typography.h5,
+//                   mt: spacing(3),
+//                   fontWeight: 'bold',
+//                 })}
+//               >
+//                 Register Here
+//               </Typography>
+//               <Typography
+//                 sx={({ typography }) => ({
+//                   fontSize: typography.body2,
+//                 })}
+//               >
+//                 Register your user details to create an account
+//               </Typography>
+//             </Box>
+//             <Box component="form" onSubmit={handleSubmit}>
+//               <TextField
+//                 fullWidth
+//                 label="Company Name"
+//                 placeholder="Registerzzd Company Name"
+//                 value={companyName}
+//                 variant="standard"
+//                 margin="normal"
+//                 onChange={(e) => setCompanyName(e.target.value)}
+//               />
+
+//               <TextField
+//                 fullWidth
+//                 label="Full Name"
+//                 placeholder="Your Full Name"
+//                 value={userName}
+//                 variant="standard"
+//                 margin="normal"
+//                 onChange={(e) => setUserName(e.target.value)}
+//               />
+
+//               <TextField
+//                 label="Email"
+//                 placeholder="Your Email"
+//                 value={email}
+//                 variant="standard"
+//                 margin="normal"
+//                 onChange={(e) => setEmail(e.target.value)}
+//                 sx={{ width: '45.3%' }}
+//               />
+
+//               <TextField
+//                 label="Mobile Number"
+//                 placeholder="Your Mobile Number"
+//                 value={phoneNumber}
+//                 variant="standard"
+//                 margin="normal"
+//                 onChange={(e) => setPhoneNumber(e.target.value)}
+//                 sx={({ spacing }) => ({
+//                   ml: spacing(4),
+//                   width: '45.3%',
+//                 })}
+//               />
+
+//               <TextField
+//                 fullWidth
+//                 label="Password"
+//                 placeholder="Your password"
+//                 value={password}
+//                 type="password"
+//                 variant="standard"
+//                 onChange={(e) => setPassword(e.target.value)}
+//                 sx={({ spacing }) => ({
+//                   mt: spacing(1),
+//                   width: '45.3%',
+//                 })}
+//               />
+
+//               <TextField
+//                 fullWidth
+//                 label="Confirm Password"
+//                 placeholder="Confirm Your password"
+//                 value={confirmPassword}
+//                 type="password"
+//                 variant="standard"
+//                 onChange={(e) => setConfirmPassword(e.target.value)}
+//                 sx={({ spacing }) => ({
+//                   mt: spacing(1),
+//                   ml: spacing(4),
+//                   width: '45.3%',
+//                 })}
+//               />
+
+//               <Button
+//                 type="submit"
+//                 fullWidth
+//                 variant="contained"
+//                 sx={({ spacing }) => ({
+//                   mt: spacing(4),
+//                 })}
+//               >
+//                 REGISTER
+//               </Button>
+//             </Box>
+//           </Box>
+//         </Container>
+//       </Box>
+//     </Box>
+//   );
+// };
+
+// export default RegisterForm;
