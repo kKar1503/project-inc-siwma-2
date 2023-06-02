aa8a08b84c95ce5e425e928432f7b7ee6aff5a72
Mokke-31 <107909574+Mokke-31@users.noreply.github.com>
2023-06-02 22:41:47 +0800

    feat: middleware update
    middleware update

--- apps/marketplace/src/middlewares/fetchReviews.tsx
+++ apps/marketplace/src/middlewares/fetchReviews.tsx
@@ -4,7 +4,7 @@ import listings from '@/utils/api/client/zod/listings';
 const fetchReviews = async (listingID: string) => {
   const response = await apiClient.get(`v1/listings/${listingID}/reviews`);
   // parse data through zod to ensure data is correct
-  const reviewData = listings.getReviews.parse(response.data.data[0]);
+  const reviewData = listings.getReviews.parse(response.data.data);
   return reviewData;
 };
 
