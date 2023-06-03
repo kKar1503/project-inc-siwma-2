3f19e67a1c37428bbc734f5b184820af807889c2
Cat <45095604+catcd1w3r5@users.noreply.github.com>
2023-06-03 19:53:58 +0800

    fix: missing import

--- apps/marketplace/src/utils/api/server/apiHandler.ts
+++ apps/marketplace/src/utils/api/server/apiHandler.ts
@@ -1,29 +1,30 @@
 import {
-    BaseError,
-    BucketConnectionFailure,
-    ErrorJSON,
-    InvalidBucketName,
-    InvalidDataProvided,
-    MissingUUID,
-    MultipartUploadError,
-    ObjectCollision,
-    ObjectNotFound,
-    ParamError,
-    ParamInvalidError,
-    ParamRequiredError,
-    ParamTooShortError,
-    ParamTypeError,
-    UnknownError,
-    UnknownS3Error
+  BaseError,
+  BucketConnectionFailure,
+  ErrorJSON,
+  InvalidBucketName,
+  InvalidDataProvided,
+  MissingUUID,
+  MultipartUploadError,
+  ObjectCollision,
+  ObjectNotFound,
+  ParamError,
+  ParamInvalidError,
+  ParamRequiredError,
+  ParamTooShortError,
+  ParamTypeError,
+  QueryError,
+  UnknownError,
+  UnknownS3Error,
 } from '@inc/errors';
 
-import {NextApiResponse} from 'next';
+import { NextApiResponse } from 'next';
 import nextConnect from 'next-connect';
-import {ZodError} from 'zod';
-import {Prisma} from '@prisma/client';
-import {APIHandlerOptions, APIRequestType} from '@/types/api-types';
-import {S3Error} from '@inc/s3-simplified';
-import {apiGuardMiddleware} from './middlewares/apiGuardMiddleware';
+import { ZodError } from 'zod';
+import { Prisma } from '@prisma/client';
+import { APIHandlerOptions, APIRequestType } from '@/types/api-types';
+import { S3Error } from '@inc/s3-simplified';
+import { apiGuardMiddleware } from './middlewares/apiGuardMiddleware';
 import jwtMiddleware from './middlewares/jwtMiddleware';
 
 /**
@@ -105,7 +106,7 @@ function handleZodError(error: ZodError) {
       // Return a param error
       return new ParamTypeError(
         zodPathToString(err.path),
-        acceptedTypes.length > 0 ? acceptedTypes : 'unknown'
+        acceptedTypes.length > 0 ? acceptedTypes : 'unknown',
       ).toJSON();
     }
 
@@ -120,37 +121,37 @@ function handleZodError(error: ZodError) {
  * Prisma error handler
  */
 function handlePrismaError(error: Prisma.PrismaClientKnownRequestError) {
-    // Check if it was a foreign key constraint error
-    if (error.code === 'P2003') {
-        // Yes it was, return a param error
-    }
+  // Check if it was a foreign key constraint error
+  if (error.code === 'P2003') {
+    // Yes it was, return a param error
+  }
 
-    // Unrecognised prisma error
-    return new ParamError();
+  // Unrecognised prisma error
+  return new ParamError();
 }
 
 /**
  * Prisma error handler
  */
 function handleS3Error(error: S3Error) {
-    switch (error.name) {
-        case 'InvalidBucket':
-            return new BucketConnectionFailure();
-        case 'InvalidBucketName':
-            return new InvalidBucketName();
-        case 'InvalidObject':
-            return new ObjectNotFound();
-        case 'ObjectConflict':
-            return new ObjectCollision();
-        case 'MultipartUploadError':
-            return new MultipartUploadError();
-        case 'InvalidDatatype':
-            return new InvalidDataProvided();
-        case 'MissingUUID':
-            return new MissingUUID();
-        default:
-            return new UnknownS3Error();
-    }
+  switch (error.name) {
+    case 'InvalidBucket':
+      return new BucketConnectionFailure();
+    case 'InvalidBucketName':
+      return new InvalidBucketName();
+    case 'InvalidObject':
+      return new ObjectNotFound();
+    case 'ObjectConflict':
+      return new ObjectCollision();
+    case 'MultipartUploadError':
+      return new MultipartUploadError();
+    case 'InvalidDatatype':
+      return new InvalidDataProvided();
+    case 'MissingUUID':
+      return new MissingUUID();
+    default:
+      return new UnknownS3Error();
+  }
 }
 
 /**
@@ -159,38 +160,38 @@ function handleS3Error(error: S3Error) {
  * @returns An error object
  */
 function handleError($error: Error): ErrorJSON[] {
-    // An error occurred
-    let error = $error;
+  // An error occurred
+  let error = $error;
 
-    console.log({error});
+  console.log({ error });
 
-    // Check if it was a zod error
-    if (error instanceof ZodError) {
-        // We got a zod error
-        return handleZodError(error);
-    }
+  // Check if it was a zod error
+  if (error instanceof ZodError) {
+    // We got a zod error
+    return handleZodError(error);
+  }
 
-    // Check if it was a prisma error
-    if (error instanceof Prisma.PrismaClientKnownRequestError) {
-        // We got a prisma error
-        error = handlePrismaError(error);
-    }
+  // Check if it was a prisma error
+  if (error instanceof Prisma.PrismaClientKnownRequestError) {
+    // We got a prisma error
+    error = handlePrismaError(error);
+  }
 
-    // Check if it was a custom error
-    if (error instanceof S3Error) {
-        // Return the error message
-        error = handleS3Error(error);
-    }
+  // Check if it was a custom error
+  if (error instanceof S3Error) {
+    // Return the error message
+    error = handleS3Error(error);
+  }
 
-    // Check if it was a custom error
-    if (error instanceof BaseError) {
-        // Return the error message
-        return [error.toJSON()];
-    }
+  // Check if it was a custom error
+  if (error instanceof BaseError) {
+    // Return the error message
+    return [error.toJSON()];
+  }
 
-    // An unknown error was received
-    console.error(error);
-    return [new UnknownError().toJSON()];
+  // An unknown error was received
+  console.error(error);
+  return [new UnknownError().toJSON()];
 }
 
 /**
@@ -199,51 +200,51 @@ function handleError($error: Error): ErrorJSON[] {
  * @returns The status code
  */
 function getStatusCode(errors: ErrorJSON[]) {
-    // Check if the status code of all errors in the array are the same
-    const {status} = errors[0];
+  // Check if the status code of all errors in the array are the same
+  const { status } = errors[0];
 
-    // Iterate through the errors
-    for (let i = 1; i < errors.length; i++) {
-        // Check if the status code is the same
-        if (errors[i].status !== status) {
-            // The status codes are not the same
-            return 400;
-        }
+  // Iterate through the errors
+  for (let i = 1; i < errors.length; i++) {
+    // Check if the status code is the same
+    if (errors[i].status !== status) {
+      // The status codes are not the same
+      return 400;
     }
+  }
 
-    // The status codes are the same
-    return status;
+  // The status codes are the same
+  return status;
 }
 
 export default (options?: APIHandlerOptions) =>
-    // Return the next-connect handler
-    nextConnect<APIRequestType, NextApiResponse>({
-        // -- API error handling --//
-        onError(error: Error | Error[], req, res) {
-            // An error was thrown, initialise the response object
-            const response = {errors: <ErrorJSON[]>[]};
+  // Return the next-connect handler
+  nextConnect<APIRequestType, NextApiResponse>({
+    // -- API error handling --//
+    onError(error: Error | Error[], req, res) {
+      // An error was thrown, initialise the response object
+      const response = { errors: <ErrorJSON[]>[] };
 
-            // Check if it was an array of errors
-            if (Array.isArray(error)) {
-                // Handle the errors
-                error.forEach((err) => {
-                    response.errors.push(...handleError(err));
-                });
-            } else {
-                // Handle the error
-                response.errors.push(...handleError(error));
-            }
+      // Check if it was an array of errors
+      if (Array.isArray(error)) {
+        // Handle the errors
+        error.forEach((err) => {
+          response.errors.push(...handleError(err));
+        });
+      } else {
+        // Handle the error
+        response.errors.push(...handleError(error));
+      }
 
-            // Determine the status code to be returned
-            const status = getStatusCode(response.errors);
+      // Determine the status code to be returned
+      const status = getStatusCode(response.errors);
 
-            // Return the response
-            res.status(status).json(response);
-        },
-        onNoMatch(req, res) {
-            res.status(405).json({error: `Method ${req.method} not allowed`});
-        },
-        // -- Middlewares --//
-    })
-        .use(jwtMiddleware)
-        .use(apiGuardMiddleware(options));
+      // Return the response
+      res.status(status).json(response);
+    },
+    onNoMatch(req, res) {
+      res.status(405).json({ error: `Method ${req.method} not allowed` });
+    },
+    // -- Middlewares --//
+  })
+    .use(jwtMiddleware)
+    .use(apiGuardMiddleware(options));
