77f1842d8520ab04f08acdf79f921b48f3f769be
Cat <45095604+catcd1w3r5@users.noreply.github.com>
2023-06-06 09:33:02 +0800

    refactor: refactor file in preparation for insert image

--- apps/marketplace/src/pages/api/v1/listings/[id]/images/index.ts
+++ apps/marketplace/src/pages/api/v1/listings/[id]/images/index.ts
@@ -6,6 +6,8 @@ import { z } from 'zod';
 import { fileToS3Object, getFilesFromRequest } from '@/utils/imageUtils';
 import s3Connection from '@/utils/s3Connection';
 import { ForbiddenError, NotFoundError } from '@inc/errors';
+import { File } from 'formidable';
+
 
 const ListingBucketName = process.env.LISTING_BUCKET_NAME as string;
 
@@ -14,7 +16,7 @@ const ParamSchema = z.object({
 });
 
 const isAdmin = (user: { permissions: number } & APIRequestType) => user.permissions === 1;
-const userCompany = async (user: { id: string }, owner: string) => {
+const validateUser = async (user: { id: string }, owner: string) => {
   const userId = user.id;
   const companyPromise = PrismaClient.companies.findFirst({
     where: {
@@ -34,18 +36,11 @@ const userCompany = async (user: { id: string }, owner: string) => {
       },
     },
   });
-
   const [company, otherCompany] = await Promise.all([companyPromise, otherCompanyPromise]);
-
-
   return (company && otherCompany && company.id === otherCompany.id);
 };
 
-
-const PUT = async (req: NextApiRequest & APIRequestType, res: NextApiResponse) => {
-  // Validate query params
-  const { id } = ParamSchema.parse(req.query);
-  // find listing
+const validateListing = async (id: number) => {
   const listing = await PrismaClient.listing.findFirst({
     where: {
       id,
@@ -54,30 +49,38 @@ const PUT = async (req: NextApiRequest & APIRequestType, res: NextApiResponse) =
       listingImages: true,
     },
   });
+  if (!listing) throw new NotFoundError(`Listing with id '${id}`);
+  return listing;
+};
 
-  if (!listing) {
-    throw new NotFoundError(`Listing with id '${id}`);
-  }
-
-
-  // Check if the user is the owner of the listing or an admin
-  const user = req.token?.user;
-  if (!isAdmin(user) && !(await userCompany(user, listing.owner))) {
-    throw new ForbiddenError();
-  }
-
-  const files = await getFilesFromRequest(req, { multiples: true });
+const append = async (listing: { listingImages: { image: string }[] }, files: File[]) => {
   const bucket = await s3Connection.getBucket(ListingBucketName);
   const previousImages = listing.listingImages;
-
   const objects = await Promise.all(
-    files.map((file,i) => {
+    files.map((file, i) => {
       const sortOrder = i * 10000;
-      const object = fileToS3Object(file,{'sort-order': sortOrder.toString()});
+      const object = fileToS3Object(file, { 'sort-order': sortOrder.toString() });
       return bucket.createObject(object);
     }),
   );
   const images = objects.map((object) => ({ image: object.Id }));
+  return [...previousImages, ...images];
+};
+
+const PUT = async (req: NextApiRequest & APIRequestType, res: NextApiResponse) => {
+  // Validate query params
+  const { id } = ParamSchema.parse(req.query);
+  // find listing
+  const listing = await validateListing(id);
+
+  // Check if the user is the owner of the listing or an admin
+  const user = req.token?.user;
+  if (!isAdmin(user) && !(await validateUser(user, listing.owner))) throw new ForbiddenError();
+
+
+  const files = await getFilesFromRequest(req, { multiples: true });
+
+  const updatedListingImages = append(listing, files);
 
   await PrismaClient.listing.update({
     where: {
@@ -85,7 +88,7 @@ const PUT = async (req: NextApiRequest & APIRequestType, res: NextApiResponse) =
     },
     data: {
       listingImages: {
-        create: [...previousImages, ...images],
+        create: await updatedListingImages,
       },
     },
     include: {
