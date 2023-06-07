7d38278997a41d241048865893fc54e8447459b7
Mokke-31 <107909574+Mokke-31@users.noreply.github.com>
2023-06-07 15:06:20 +0800

    feat: fixed middleware for retrieving parms and users
    fixed middleware for retrieving parms and users

--- apps/marketplace/src/middlewares/fetchParamNames.tsx
+++ apps/marketplace/src/middlewares/fetchParamNames.tsx
@@ -2,9 +2,9 @@ import apiClient from '@/utils/api/client/apiClient';
 import parameters from '@/utils/api/client/zod/parameters';
 
 const fetchParams = async () => {
-  const response = await apiClient.get(`v1/parameters/types`);
+  const response = await apiClient.get(`v1/parameters`);
   // parse data through zod to ensure data is correct
-  const paramData = parameters.getTypes.parse(response.data.data);
+  const paramData = parameters.getAll.parse(response.data.data);
   return paramData;
 };
 
--- apps/marketplace/src/middlewares/fetchUser.tsx
+++ apps/marketplace/src/middlewares/fetchUser.tsx
@@ -1,11 +1,11 @@
 import apiClient from '@/utils/api/client/apiClient';
 import users from '@/utils/api/client/zod/users';
 
-const fetchUser = async (uuid: string) => {
-  const response = await apiClient.get(`/v1/users/${uuid}`);
+const fetchUsers = async () => {
+  const response = await apiClient.get(`/v1/users/`);
   // parse data through zod to ensure data is correct
-  const user = users.getById.parse(response.data.data);
+  const user = users.getAll.parse(response.data.data);
   return user;
 };
 
-export default fetchUser;
+export default fetchUsers;
