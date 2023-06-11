f9b0ea34cdd31cf657b717f23a7e599139e8b73a
Mokke-31 <107909574+Mokke-31@users.noreply.github.com>
2023-06-11 17:55:28 +0800

    feat: fixed pr fr fr
    seperate import

--- apps/marketplace/src/components/marketplace/category/CarouselImgsPlaceholder.tsx
+++ apps/marketplace/src/components/marketplace/category/CarouselImgsPlaceholder.tsx
@@ -1,4 +1,5 @@
-import { Box, Typography } from '@mui/material';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 
 const CatImgsPlaceholder = () => (
   <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', py: '2em', height: 250 }}>
