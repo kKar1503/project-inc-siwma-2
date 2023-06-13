e7c73ea7cb12442919b7a6c0f3c27972c4571317
Kaung Khant Zaw <mmmike2002@gmail.com>
2023-06-13 14:34:38 +0800

    feat: OnEditModal

---
+++ apps/marketplace/src/components/modal/OnEditModal.tsx
@@ -0,0 +1,37 @@
+import { Modal } from '@inc/ui';
+import { useState, useEffect } from 'react';
+
+export type ReportModalProps = {
+  open: boolean;
+  setOpen: (val: boolean) => void;
+};
+
+const OnEditModal = ({ open, setOpen }: ReportModalProps) => {
+  const [rightButtonState, setRightButtonState] = useState(false);
+
+  useEffect(() => {
+    if (rightButtonState === true) {
+      setOpen(false);
+    }
+    setRightButtonState(false);
+  });
+
+  return (
+    <Modal
+      open={open}
+      setOpen={setOpen}
+      buttonColor="#0288D1"
+      icon="success"
+      title="Successfully edited listing!"
+      content="Your listing is now updated on the marketplace."
+      leftButtonText={null}
+      rightButtonText="Close"
+      leftButtonState={false}
+      rightButtonState={rightButtonState}
+      setLeftButtonState={setRightButtonState}
+      setRightButtonState={setRightButtonState}
+    />
+  );
+};
+
+export default OnEditModal;
