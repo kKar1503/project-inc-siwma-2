452fbaaa7a0d14599b5136af0298be2ff1e8b1d6
Kaung Khant Zaw <mmmike2002@gmail.com>
2023-06-13 14:32:04 +0800

    feat: filling data for listingtype form

--- apps/marketplace/src/components/marketplace/createListing/useListingType.tsx
+++ apps/marketplace/src/components/marketplace/createListing/useListingType.tsx
@@ -1,22 +1,26 @@
 import React, { useState } from 'react';
-import ListingTypeForm, { ListingTypeProps } from '@/components/marketplace/createListing/ListingTypeForm';
-
-
+import ListingTypeForm, {
+  ListingTypeProps,
+} from '@/components/marketplace/createListing/ListingTypeForm';
 
 const useListingType = () => {
   const [listingType, setListingType] = useState<ListingTypeProps>('BUY');
+  const [disabled, setDisabled] = useState<boolean>(false);
 
-  const listingTypeForm = <ListingTypeForm setListingType={setListingType} />;
+  const listingTypeForm = (
+    <ListingTypeForm setListingType={setListingType} data={listingType} disabled={disabled} />
+  );
 
   const listingTypeData = {
     type: listingType,
-  }
+  };
 
   return {
     listingTypeForm,
     listingTypeData,
+    setListingType,
+    setDisabled,
   };
-
 };
 
 export default useListingType;
