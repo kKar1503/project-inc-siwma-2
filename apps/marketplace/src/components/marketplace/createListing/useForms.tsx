0cd9ead002c1f4ffb5c41af955a771df32266fe0
Kaung Khant Zaw <mmmike2002@gmail.com>
2023-06-13 14:39:09 +0800

    feat: edit-listing page

--- apps/marketplace/src/components/marketplace/createListing/useForms.tsx
+++ apps/marketplace/src/components/marketplace/createListing/useForms.tsx
@@ -1,22 +1,44 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { PostListingsRequestBody } from '@/utils/api/server/zod';
 import useListing from '@/components/marketplace/createListing/useListing';
 import useCategory from '@/components/marketplace/createListing/useCategory';
 import useListingType from '@/components/marketplace/createListing/useListingType';
 import useImages from '@/components/marketplace/createListing/useImages';
 
-
 const useForms = () => {
+  const { listingTypeForm, listingTypeData, setListingType, setDisabled } = useListingType();
 
-  const { listingTypeForm, listingTypeData } = useListingType();
-  const { categoryForm, categoryValidation, resetCategoryErrors, categoryData, parameters } = useCategory();
-  const { imagesForm, imageData } = useImages();
-  const { parameterForm, parameterValidation, resetParameterErrors, parameterData } = parameters;
-  const { listingForm, listingValidation, resetListingErrors, listingData } = useListing();
+  const {
+    categoryForm,
+    categoryValidation,
+    resetCategoryErrors,
+    categoryData,
+    parameters,
+    setCategory,
+    categoriesData,
+  } = useCategory();
 
+  const { imagesForm, imageData, imagesOrder, deletedImages, setListingImages } = useImages();
 
-  const [formData, setFormData] = useState<{listingBody: PostListingsRequestBody ,images:Blob[]}>();
+  const { parameterForm, parameterValidation, resetParameterErrors, parameterData, setParameters } =
+    parameters;
 
+  const {
+    listingForm,
+    listingValidation,
+    resetListingErrors,
+    listingData,
+    setTitle,
+    setPrice,
+    setNegotiable,
+    setUnitPrice,
+    setDescription,
+  } = useListing();
+
+  const [formData, setFormData] = useState<{
+    listingBody: PostListingsRequestBody;
+    images: Blob[];
+  }>();
 
   const validateForm = () => {
     resetListingErrors();
@@ -48,19 +70,60 @@ const useForms = () => {
     return true;
   };
 
-  const forms = <>
-    {listingTypeForm}
-    {categoryForm}
-    {imagesForm}
-    {parameterForm}
-    {listingForm}
-  </>;
+  const forms = (
+    <>
+      {listingTypeForm}
+      {categoryForm}
+      {imagesForm}
+      {parameterForm}
+      {listingForm}
+    </>
+  );
 
+  // filling the form with listing data
+  useEffect(() => {
+    if (formData) {
+      const listing = formData.listingBody;
+
+      setListingType(listing.type);
+
+      categoriesData?.forEach((category) => {
+        if (category.id === listing.categoryId) {
+          setCategory(category);
+        }
+      });
+
+      if (listing.parameters) {
+        setParameters(listing.parameters);
+      }
+      setTitle(listing.name);
+      setPrice(listing.price);
+      setNegotiable(listing.negotiable || false);
+      setUnitPrice(listing.unitPrice || false);
+      setDescription(listing.description);
+    }
+  }, [
+    categoriesData,
+    formData,
+    setCategory,
+    setDescription,
+    setListingType,
+    setNegotiable,
+    setParameters,
+    setPrice,
+    setTitle,
+    setUnitPrice,
+  ]);
 
   return {
     forms,
     formData,
+    imagesOrder,
+    deletedImages,
+    setFormData,
     submitForm,
+    setDisabled,
+    setListingImages,
   };
 };
 
---
+++ apps/marketplace/src/pages/edit-listing/[id].tsx
@@ -0,0 +1,171 @@
+import React, { useEffect } from 'react';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Divider from '@mui/material/Divider';
+import Grid from '@mui/material/Grid';
+import Button from '@mui/material/Button';
+
+// components
+import { Listing } from '@/utils/api/client/zod/listings';
+import { PostListingsRequestBody } from '@/utils/api/server/zod/listings';
+import { useQuery } from 'react-query';
+import { useRouter } from 'next/router';
+import { useSession } from 'next-auth/react';
+
+// hooks
+import useModals from '@/components/marketplace/createListing/useModals';
+import useForms from '@/components/marketplace/createListing/useForms';
+
+// middleware
+import fetchListing from '@/middlewares/fetchListing';
+import { Order, PreviewImageProps } from '@/components/marketplace/createListing/ImageUploadForm';
+import editListing from '@/middlewares/editListing';
+
+const useGetListingQuery = (id: string) => {
+  const { data } = useQuery(['listing', id], async () => fetchListing(id), {
+    enabled: id !== undefined && id.trim() !== '',
+  });
+
+  return data;
+};
+
+const useEditListingQuery = (
+  id: string,
+  listing: { listingBody: PostListingsRequestBody; images: Blob[] } | undefined,
+  previousData: Listing | undefined,
+  imagesOrder: Order,
+  deletedImages: string[]
+) => {
+  const { data } = useQuery(
+    ['postListing', listing],
+    () => editListing(id, listing?.listingBody, listing?.images, imagesOrder, deletedImages),
+    {
+      enabled:
+        id !== undefined &&
+        id.trim() !== '' &&
+        listing !== undefined &&
+        previousData !== undefined &&
+        listing.listingBody !== undefined &&
+        listing.listingBody !== previousData &&
+        listing.images !== undefined,
+    }
+  );
+
+  if (data === undefined) return false;
+
+  return data;
+};
+
+const CreateListingPage = () => {
+  const router = useRouter();
+  const { id } = router.query;
+  const { data } = useSession();
+
+  // modals
+  const { editModal, cancelModal } = useModals();
+
+  // form data
+  const {
+    forms,
+    formData,
+    setFormData,
+    submitForm,
+    setDisabled,
+    setListingImages,
+    imagesOrder,
+    deletedImages,
+  } = useForms();
+
+  // Hooks
+  const listingData = useGetListingQuery(id as string);
+  const editListingData = useEditListingQuery(
+    id as string,
+    formData,
+    listingData,
+    imagesOrder.order,
+    deletedImages
+  );
+
+  // check if the user editing is the owner or an admin
+  useEffect(() => {
+    if (!data || !listingData) return;
+    if (data?.user.permissions !== 1 && data?.user.id !== listingData?.owner.id) router.back();
+  }, [listingData, data, router]);
+
+  // setListingData
+  useEffect(() => {
+    if (listingData) {
+      setFormData({ listingBody: listingData, images: [] });
+      setDisabled(true);
+      if (listingData.images) {
+        const previewImages: PreviewImageProps[] = [];
+        listingData.images.forEach((image) => {
+          previewImages.push({
+            id: image.id,
+            preview: `https://${process.env.NEXT_PUBLIC_AWS_BUCKET}.s3.${process.env.NEXT_PUBLIC_AWS_REGION}.amazonaws.com/${image}`,
+          });
+        });
+        setListingImages(previewImages);
+      }
+    }
+  }, [listingData, setDisabled, setFormData, setListingImages]);
+
+  // Handle Submit/Cancel
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    editModal.open(true);
+    submitForm();
+  };
+
+  const handleCancel = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    cancelModal.open(true);
+  };
+
+  return (
+    <Container maxWidth="lg" sx={{ boxShadow: 4, padding: 2, marginTop: 2, marginBottom: 2 }}>
+      <form onSubmit={handleSubmit}>
+        <Grid container spacing={2}>
+          <Grid item xs={12} md={12} sx={{ width: '100%' }}>
+            <Typography variant="h6" sx={{ fontWeight: 'bold' }}>
+              Edit Listing
+            </Typography>
+            <Typography variant="body1">
+              Edit a buy or a sell listing to be shared on your profile.
+            </Typography>
+          </Grid>
+          <Grid item xs={12} md={12} sx={{ width: '100%' }}>
+            <Divider />
+          </Grid>
+
+          {forms}
+
+          <Grid item xs={6} md={6} sx={{ width: '100%' }}>
+            <Button
+              variant="contained"
+              type="submit"
+              size="large"
+              onClick={handleCancel}
+              sx={({ palette }) => ({
+                backgroundColor: palette.error.main,
+              })}
+              fullWidth
+            >
+              Cancel
+            </Button>
+            {cancelModal.element}
+          </Grid>
+          <Grid item xs={6} md={6} sx={{ width: '100%' }}>
+            <Button variant="contained" type="submit" size="large" fullWidth>
+              Edit LISTING
+            </Button>
+            {editModal.element}
+          </Grid>
+        </Grid>
+      </form>
+    </Container>
+  );
+};
+
+export default CreateListingPage;
