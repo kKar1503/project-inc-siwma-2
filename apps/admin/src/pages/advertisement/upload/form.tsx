b9a10a6101bb999f445654b25ded29849f49d518
Adeeb <adityaadeeb04@gmail.com>
2023-06-14 18:01:36 +0800

    adding more functionality and styling

--- apps/admin/src/pages/advertisement/upload/form.tsx
+++ apps/admin/src/pages/advertisement/upload/form.tsx
@@ -1,86 +1,158 @@
-import React from 'react';
-import { Box, TextField, Button } from '@mui/material';
+import { useState, useMemo } from 'react';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Grid from '@mui/material/Grid';
+import useResponsiveness from '@inc/ui/lib/hook/useResponsiveness';
+import { useTheme } from '@mui/material/styles';
 
-const form = () => {
-    return (
-        <Box
-            component="form"
-            sx={{
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
+const Form = () => {
+    const theme = useTheme();
+    const { spacing } = theme;
+    const [isSm, isMd, isLg] = useResponsiveness(['sm', 'md', 'lg']);
+
+
+    const gridCols = useMemo(() => {
+        if (isSm) {
+            return {
+                py: spacing(3),
+                px: '20px',
+                height: '100%;',
                 width: '100%',
-                '& .MuiTextField-root': { m: 1, width: '25ch' },
-            }}
-        >
-            <Box sx={{ display: 'flex', width: '100%' }}>
-                <TextField
-                    label="Company to tag"
-                    variant="outlined"
-                    sx={{
-                        '& .MuiOutlinedInput-root': {
-                            '& fieldset': { borderColor: 'black' },
-                            '&.Mui-focused fieldset': { borderColor: 'black' },
-                        },
-                        '& .MuiInputBase-input': { color: 'black' },
-                        backgroundColor: 'lightgray',
-                        mr: 1,
-                        width: '50%',
-                    }}
-                />
-                <TextField
-                    label="Company link"
-                    variant="outlined"
-                    sx={{
-                        '& .MuiOutlinedInput-root': {
-                            '& fieldset': { borderColor: 'black' },
-                            '&.Mui-focused fieldset': { borderColor: 'black' },
-                        },
-                        '& .MuiInputBase-input': { color: 'black' },
-                        backgroundColor: 'lightgray',
-                        width: '50%',
-                    }}
-                />
-            </Box>
-            <Box sx={{ width: '100%' }}>
+                justifyContent: 'center',
+            };
+        }
+        if (isMd) {
+            return {
+                py: spacing(3),
+                px: '40px',
+                height: '100%;',
+                width: '100%',
+                justifyContent: 'center',
+            };
+        }
+        if (isLg) {
+            return {
+                py: spacing(3),
+                px: '60px',
+                height: '100%;',
+                width: '100%',
+                display: 'flex',
+                justifyContent: 'center',
+            };
+        }
+        return {
+            py: spacing(3),
+            px: '20px',
+            height: '100%;',
+            width: '100%',
+        };
+    }, [isSm, isMd, isLg]);
+
+    return (
+        <Grid sx={gridCols}>
+            <Box
+                component="form"
+                sx={() => ({
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                    width: '100%',
+                })}
+            >
+                <Box
+                    sx={() => ({
+                        display: 'flex',
+                        alignItems: 'center',
+                        width: '100%',
+                    })}
+                >
+                    <TextField
+                        label="Company to tag"
+                        variant="outlined"
+                        sx={({ spacing, palette }) => ({
+                            mr: spacing(2),
+                            bgcolor: palette.grey[400],
+                            '& .MuiOutlinedInput-root': {
+                                '& fieldset': { borderColor: palette.grey[800] },
+                                '&.Mui-focused fieldset': { borderColor: palette.grey[800] },
+                            },
+                            '& .MuiInputBase-input': { color: palette.grey[800] },
+                            width: '100%',
+                        })}
+                    />
+
+                    <TextField
+                        label="Company link"
+                        variant="outlined"
+                        sx={({ palette }) => ({
+                            bgcolor: palette.grey[400],
+                            width: '100%',
+                            '& .MuiOutlinedInput-root': {
+                                '& fieldset': { borderColor: palette.grey[800] },
+                                '&.Mui-focused fieldset': { borderColor: palette.grey[800] },
+                            },
+                            '& .MuiInputBase-input': { color: palette.grey[800] },
+                        })}
+                    />
+                </Box>
+
                 <TextField
                     label="Advertisement title"
                     variant="outlined"
                     fullWidth
-                    sx={{
+                    sx={({ spacing, palette }) => ({
+                        mt: spacing(2),
+                        bgcolor: palette.grey[400],
+                        width: '100%',
                         '& .MuiOutlinedInput-root': {
-                            '& fieldset': { borderColor: 'black' },
-                            '&.Mui-focused fieldset': { borderColor: 'black' },
+                            '& fieldset': { borderColor: palette.grey[800] },
+                            '&.Mui-focused fieldset': { borderColor: palette.grey[800] },
                         },
-                        '& .MuiInputBase-input': { color: 'black' },
-                        backgroundColor: 'lightgray',
-                    }}
+                        '& .MuiInputBase-input': { color: palette.grey[800] },
+                    })}
                 />
+
+                <TextField
+                    label="Advertisement description"
+                    variant="outlined"
+                    multiline
+                    rows={4}
+                    fullWidth
+                    sx={({ spacing, palette }) => ({
+                        mt: spacing(2),
+                        bgcolor: palette.grey[400],
+                        width: '100%',
+                        '& .MuiOutlinedInput-root': {
+                            '& fieldset': { borderColor: palette.grey[800] },
+                            '&.Mui-focused fieldset': { borderColor: palette.grey[800] },
+                        },
+                        '& .MuiInputBase-input': { color: palette.grey[800] },
+                    })}
+                />
+                <Box
+                    sx={({ spacing }) => ({
+                        width: '100%',
+                        mt: spacing(2),
+                        display: 'flex',
+                        justifyContent: 'flex-end',
+                    })}
+                >
+                    <Button
+                        type="submit"
+                        variant="contained"
+                        sx={({ spacing }) => ({
+                            width: '10%',
+                            mt: spacing(1),
+                            mb: spacing(1),
+                        })}
+                    >
+                        Submit
+                    </Button>
+                </Box>
             </Box>
-            <TextField
-                label="Advertisement description"
-                variant="outlined"
-                multiline
-                rows={4}
-                fullWidth
-                sx={{
-                    '& .MuiOutlinedInput-root': {
-                        '& fieldset': { borderColor: 'black' },
-                        '&.Mui-focused fieldset': { borderColor: 'black' },
-                    },
-                    '& .MuiInputBase-input': { color: 'black' },
-                    backgroundColor: 'lightgray',
-                }}
-            />
-            <Button
-                type="submit"
-                variant="contained"
-                sx={{ backgroundColor: 'blue', color: 'white', mt: 2 }}
-            >
-                Submit
-            </Button>
-        </Box>
+        </Grid>
     );
 };
 
-export default form;
+export default Form
--- apps/admin/src/pages/advertisement/upload/index.tsx
+++ apps/admin/src/pages/advertisement/upload/index.tsx
@@ -1,16 +1,24 @@
 import React from 'react';
-import { Box, Card, CardContent } from '@mui/material';
+import { Box, Card, CardContent, Typography } from '@mui/material';
 import Form from './form';
 import Upload from './upload';
 
 const AdvertisementUpload = () => {
     return (
-        <Card>
-            <CardContent>
-                <Upload />
-                <Form />
-            </CardContent>
-        </Card>
+        <Box>
+            <Card>
+                <Typography variant="h4" sx={{ mb: 2 }}>
+                    Advertisement Upload
+                </Typography>
+                <Typography variant="subtitle1" sx={{ mb: 2 }}>
+                    Import a JPG or PNG file to upload an advertisement
+                </Typography>
+                <CardContent>
+                    <Upload />
+                    <Form />
+                </CardContent>
+            </Card>
+        </Box>
     );
 };
 
