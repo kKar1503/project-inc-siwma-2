36aca11b845174ef3afebba2192d9758d7b42df5
Mokke-31 <107909574+Mokke-31@users.noreply.github.com>
2023-06-15 11:33:40 +0800

    feat: edit to middleware
    edited createchat and postreview middlewares

--- apps/marketplace/src/middlewares/createChat.tsx
+++ apps/marketplace/src/middlewares/createChat.tsx
@@ -1,10 +1,12 @@
 import apiClient from '@/utils/api/client/apiClient';
 import chat from '@/utils/api/client/zod/chat';
-// import fetchListing from './fetchListing';
-// import { useQuery } from 'react-query';
 
-const createRoom = async () => {
-  const response = await apiClient.get(`v1/chat/`);
+const createRoom = async (sellerId: string, buyerId: string, listingId: string) => {
+  const response = await apiClient.post(`v1/chat/`, {
+    sellerId,
+    buyerId,
+    listingId
+  });
   const newRoom = chat.room.create.parse(response.data.data);
 
   return newRoom;
--- apps/marketplace/src/middlewares/postReview.tsx
+++ apps/marketplace/src/middlewares/postReview.tsx
@@ -1,13 +1,16 @@
 import apiClient from '@/utils/api/client/apiClient';
 import listing from '@/utils/api/client/zod/listings';
-// import fetchListing from './fetchListing';
-// import { useQuery } from 'react-query';
 
-const postReview = async (uuid: string) => {
-  const response = await apiClient.get(`v1/listings/${uuid}/review`);
-  const newReview = listing.createReview.parse(response.data.data);
-
-  return newReview;
+const postReview = async (uuid: string, review: string, rating: number) => {
+  if (uuid) {
+    const response = await apiClient.post(`v1/listings/${uuid}/review`, {
+      review,
+      rating,
+    });
+    const newReview = listing.createReview.parse(response.data.data);
+    return newReview;
+  }
+  return null;
 };
 
 export default postReview;
