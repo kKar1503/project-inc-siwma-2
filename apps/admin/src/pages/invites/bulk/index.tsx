a88ef9558474093361e3d399a403b69b617f668c
Karandeep Singh <46585033+ConfuseIous@users.noreply.github.com>
2023-06-16 11:56:57 +0800

    added checks

--- apps/admin/src/pages/invites/bulk/index.tsx
+++ apps/admin/src/pages/invites/bulk/index.tsx
@@ -1,21 +1,47 @@
+/* eslint-disable no-alert */
 import { useState } from 'react';
-import FileUpload, { AcceptedFileTypes } from '@/components/FileUpload/FileUploadBase';
+import FileUpload, {
+  FileUploadProps,
+  AcceptedFileTypes,
+} from '@/components/FileUpload/FileUploadBase';
 import { Box } from '@mui/material';
 
 const BulkInvitesPage = () => {
   const [file, setFile] = useState<File | null>(null);
 
+  const handleFileChange: FileUploadProps['changeHandler'] = (event) => {
+    if (!event.target.files) return;
+
+    if (event.target.files.length === 1) {
+      setFile(event.target.files[0]);
+    } else if (event.target.files.length > 1) {
+      setFile(null);
+      alert('Please Select Only One File');
+
+      return;
+    } else {
+      setFile(null);
+      alert('Please Select a File');
+
+      return;
+    }
+
+    if (event.target.files[0].size > 64000000) {
+      setFile(null);
+      alert('Please Select a File Smaller Than 64 MB');
+    }
+
+    
+  };
+
   return (
     <Box>
-      <p>Hello World</p>
       <FileUpload
         title="Bulk Add Companies & Invite Users"
         description="Import an .xlsx file below to bulk add company profiles and bulk invite users"
         selectedFile={file}
         changeHandler={(event) => {
-          if (event.target.files) {
-            setFile(event.target.files[0]);
-          }
+          handleFileChange(event);
         }}
         accept={[AcceptedFileTypes.XLSX]}
         maxWidth="200px"
