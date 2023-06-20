73cb890dbc0a1a15a3ba6260a408e9aa06e1f221
Yam Kar Lok <isonice4572@gmail.com>
2023-06-20 23:09:47 +0800

    added eventLogFormatter

--- apps/chat/src/utils/logger.ts
+++ apps/chat/src/utils/logger.ts
@@ -1,5 +1,10 @@
 import logger from 'pino';
 import dayjs from 'dayjs';
+import { Event } from '@inc/types';
+import { Socket } from 'socket.io';
+
+export const eventLogFormatter = (eventName: Event, socket: Socket) =>
+  `[${socket.id}:${eventName}]`;
 
 export default logger({
   transport: {
