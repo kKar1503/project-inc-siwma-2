d55e1f2c45d71bf579d8f766dcd9b3c4386f9485
Xavier Tay <45095604+catcd1w3r5@users.noreply.github.com>
2023-06-20 17:27:19 +0800

    fix: wrong prisma function used

--- apps/marketplace/src/pages/api/v1/companies/[id]/images.ts
+++ apps/marketplace/src/pages/api/v1/companies/[id]/images.ts
@@ -1,4 +1,4 @@
-import { apiHandler, formatAPIResponse } from '@/utils/api';
+import { apiHandler, formatAPIResponse, parseToNumber } from '@/utils/api';
 import PrismaClient from '@inc/db';
 import { ParamError } from '@inc/errors';
 import { fileToS3Object, getFilesFromRequest } from '@/utils/imageUtils';
@@ -6,7 +6,11 @@ import bucket from '@/utils/s3Bucket';
 
 export default apiHandler({ allowAdminsOnly: true })
   .put(async (req, res) => {
-
+    const { id } = req.query;
+    if (!id) {
+      throw new ParamError('id');
+    }
+    const companyid = parseToNumber(id as string, 'id');
     const files = await getFilesFromRequest(req);
     if (files.length === 0) {
       throw new ParamError('company logo');
@@ -14,10 +18,13 @@ export default apiHandler({ allowAdminsOnly: true })
 
     const s3Object = await bucket.createObject(fileToS3Object(files[0]));
 
-    const response = await PrismaClient.companies.create({
+    const response = await PrismaClient.companies.update({
       data: {
         logo: s3Object.Id,
       },
+      where: {
+        id: companyid,
+      }
     });
 
     res.status(201).json(formatAPIResponse({ companyId: response.id.toString() }));
