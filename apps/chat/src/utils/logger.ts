aeabdeb19f78778e7d75d324414ac7778bb554f1
Yam Kar Lok <isonice4572@gmail.com>
2023-06-21 01:34:19 +0800

    refined logs for logger.ts

--- apps/chat/src/utils/logger.ts
+++ apps/chat/src/utils/logger.ts
@@ -3,15 +3,26 @@ import dayjs from 'dayjs';
 import { Event } from '@inc/types';
 import { Socket } from 'socket.io';
 
-export const eventLogFormatter = (eventName: Event, socket: Socket) =>
-  `[${socket.id}:${eventName}]`;
+export type LogLevels = 'trace' | 'debug' | 'info' | 'event' | 'warn' | 'error' | 'fatal';
 
-export default logger({
+const pinoLogger = logger({
   transport: {
     target: 'pino-pretty',
     options: {
       colorize: true,
     },
   },
+  customLevels: {
+    event: 35,
+  },
+  level: process.env.LOG_LEVEL || 'debug',
   timestamp: () => `,"time":"${dayjs().format()}"`,
 });
+
+export const eventLogHelper = (eventName: Event, socket: Socket) => {
+  pinoLogger.info(`[${socket.id}:${eventName}]`);
+  return (level: LogLevels, logMessage: string) =>
+    pinoLogger[level](`[${socket.id}:${eventName}] ${logMessage}`);
+};
+
+export default pinoLogger;
