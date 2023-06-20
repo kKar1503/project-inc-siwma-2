ac4c34c9424658014d8e2d66cef3a5e190fd55f8
Yam Kar Lok <isonice4572@gmail.com>
2023-06-21 01:42:21 +0800

    adding logs to server.ts

--- apps/chat/src/server.ts
+++ apps/chat/src/server.ts
@@ -1,23 +1,30 @@
+import { config } from 'dotenv';
 import express from 'express';
 import { createServer } from 'http';
 import { Server } from 'socket.io';
-import { config } from 'dotenv';
-import logger from './utils/logger';
-import typedSocket from './utils/typedSocket';
-import Papa, { spawnPapa } from './store';
-
-import socket from './socket';
 
 config();
 
-const host = process.env.HOST ?? 'localhost';
-const port = process.env.PORT ? Number(process.env.PORT) : 4000;
-const corsOrigin = process.env.CORS ?? 'http://localhost:3000';
+import logger from './utils/logger';
+import { spawnPapa } from './store';
+import socket from './socket';
 
+logger.info('Validating Env Variables...');
+const host = process.env.HOST ?? 'localhost';
+const port = process.env.PORT !== undefined ? Number(process.env.PORT) : 4000;
+const corsOrigin = process.env.CORS.split(',') ?? 'http://localhost:3000';
+
+logger.debug(`HOST: ${host}`);
+logger.debug(`PORT: ${port}`);
+logger.debug(`CORS: ${corsOrigin}`);
+
+logger.info('Initializing Express Application...');
 const app = express();
 
+logger.info('Initializing HTTP Server with Express App...');
 const httpServer = createServer(app);
 
+logger.info('Initializing Socket.io Server...');
 const io = new Server(httpServer, {
   cors: {
     origin: corsOrigin,
@@ -26,17 +33,16 @@ const io = new Server(httpServer, {
 });
 
 app.get('/', (_, res) => {
-  res.send(`Server is up!`);
+  logger.info('GET: /');
+  res.send('Server is up!');
 });
 
-setInterval(() => {
-  typedSocket(io)('serverPing', `Pinged: ${new Date().toISOString()}`);
-}, 5000);
-
 httpServer.listen(port, host, () => {
-  logger.info('Server has started.');
+  logger.info(`Server is now listening at ${host}:${port}...`);
 
+  logger.info('Initializing Store...');
   spawnPapa();
 
+  logger.info('Initializing Socket.io connection...');
   socket(io);
 });
