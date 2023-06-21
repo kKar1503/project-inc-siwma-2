972901589b9d7529f3edf63a94f129d04a637164
Yam Kar Lok <isonice4572@gmail.com>
2023-06-21 13:32:57 +0800

    added a helper class to manage set of tuple

---
+++ apps/chat/src/store/OccupantsSet.ts
@@ -0,0 +1,71 @@
+export type Occupants = [User1: string, User2: string];
+
+export default class OccupantsSet {
+  #occupants: Occupants[];
+
+  constructor(occupants?: Occupants) {
+    this.#occupants = [];
+
+    if (occupants) {
+      this.#occupants.push(occupants);
+    }
+  }
+
+  add(occupants: Occupants) {
+    for (const occupant of this.#occupants) {
+      if (occupant[0] === occupants[0] && occupant[1] === occupants[1]) {
+        return false;
+      }
+
+      if (occupant[0] === occupants[1] && occupant[1] === occupants[0]) {
+        return false;
+      }
+    }
+
+    this.#occupants.push(occupants);
+  }
+
+  delete(occupants: Occupants) {
+    for (let i = 0; i < this.#occupants.length; i++) {
+      const occupant = this.#occupants[i];
+
+      if (occupant[0] === occupants[0] && occupant[1] === occupants[1]) {
+        this.#occupants.splice(i, 1);
+        return true;
+      }
+
+      if (occupant[0] === occupants[1] && occupant[1] === occupants[0]) {
+        this.#occupants.splice(i, 1);
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  has(occupants: Occupants) {
+    for (const occupant of this.#occupants) {
+      if (occupant[0] === occupants[0] && occupant[1] === occupants[1]) {
+        return true;
+      }
+
+      if (occupant[0] === occupants[1] && occupant[1] === occupants[0]) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  get(index: number) {
+    return this.#occupants[index];
+  }
+
+  get size() {
+    return this.#occupants.length;
+  }
+
+  [Symbol.iterator]() {
+    return this.#occupants[Symbol.iterator]();
+  }
+}
