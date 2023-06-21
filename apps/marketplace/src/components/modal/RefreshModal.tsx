88ed09152f2eb748a9cbebb572e59921f587afe0
Kai Jun <85498628+Xu3huapiaopiao@users.noreply.github.com>
2023-06-21 18:45:12 +0900

    Refresh modal
    Allows the user to refresh

---
+++ apps/marketplace/src/components/modal/RefreshModal.tsx
@@ -0,0 +1,34 @@
+import { Modal } from '@inc/ui';
+import { useState } from 'react';
+import { useRouter } from 'next/router';
+
+export type RefreshModalProps = {
+  open: boolean;
+  setOpen: (val: boolean) => void;
+};
+
+const OnRefreshModal = ({ open, setOpen }: RefreshModalProps) => {
+  const [buttonState, setButtonState] = useState(false);
+  const router = useRouter();
+
+  if (buttonState === true) {
+    router.reload();
+  }
+
+  return (
+    <Modal
+      open={open}
+      setOpen={setOpen}
+      buttonColor="#0288D1"
+      icon="warning"
+      title="Disconnected from Chat Server"
+      content="Chat server is being disconnected. Refresh the page or click 'Reload'."
+      rightButtonText="Reload"
+      rightButtonState={buttonState}
+      setLeftButtonState={setButtonState}
+      setRightButtonState={setButtonState}
+    />
+  );
+};
+
+export default OnRefreshModal;
