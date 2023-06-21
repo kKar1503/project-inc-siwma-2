425473e0ddcd8881f8e1237bdb9f6977b36dbbd8
Xavier Tay <45095604+catcd1w3r5@users.noreply.github.com>
2023-06-21 13:57:39 +0800

    fix: merge conflicts

--- apps/marketplace/src/utils/s3Bucket.ts
+++ apps/marketplace/src/utils/s3Bucket.ts
@@ -1,11 +1,21 @@
-import { Regions, S3Bucket, S3libInternal } from '@inc/s3-simplified';
+import { Regions, S3Bucket, S3LibInternal } from '@inc/s3-simplified';
+import hashFn from '@/utils/timeBasedHash';
 
-const s3Connection = new S3libInternal({
+const s3Connection = new S3LibInternal({
     accessKey:{
         id: process.env.AWS_ACCESS_KEY_ID as string,
         secret: process.env.AWS_SECRET_ACCESS_KEY as string,
     },
     region: process.env.AWS_REGION as Regions,
+
+    objectCreation: {
+        appendFileTypeToKey: false,
+        hash: {
+            function: hashFn,
+            requireBuffer: false,
+            requireMetadata: false,
+        },
+    },
 })
 const bucket = new S3Bucket(s3Connection, process.env.AWS_BUCKET as string);
 
