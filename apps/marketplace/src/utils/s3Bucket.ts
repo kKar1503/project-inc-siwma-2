16fba8d36bb9db753d952c002736a22d8860a3c9
Xavier Tay <45095604+catcd1w3r5@users.noreply.github.com>
2023-06-21 14:12:28 +0800

    fix: updated code to be compatible with updates s3

--- apps/marketplace/src/utils/s3Bucket.ts
+++ apps/marketplace/src/utils/s3Bucket.ts
@@ -1,22 +1,35 @@
 import { Regions, S3Bucket, S3LibInternal } from '@inc/s3-simplified';
 import hashFn from '@/utils/timeBasedHash';
+import { S3BucketInternal } from '@inc/s3-simplified/dist/classes/buckets/s3BucketInternal';
 
-const s3Connection = new S3LibInternal({
-    accessKey:{
-        id: process.env.AWS_ACCESS_KEY_ID as string,
-        secret: process.env.AWS_SECRET_ACCESS_KEY as string,
-    },
-    region: process.env.AWS_REGION as Regions,
+class CustomS3 extends S3LibInternal {
 
-    objectCreation: {
-        appendFileTypeToKey: false,
-        hash: {
-            function: hashFn,
-            requireBuffer: false,
-            requireMetadata: false,
-        },
-    },
-})
-const bucket = new S3Bucket(s3Connection, process.env.AWS_BUCKET as string);
+    constructor() {
+        super({
+            accessKey: {
+                id: process.env.AWS_ACCESS_KEY_ID as string,
+                secret: process.env.AWS_SECRET_ACCESS_KEY as string,
+            },
+            region: process.env.AWS_REGION as Regions,
+
+            objectCreation: {
+                appendFileTypeToKey: false,
+                hash: {
+                    function: hashFn,
+                    requireBuffer: false,
+                    requireMetadata: false,
+                },
+            },
+        });
+    }
+
+    public getBucketSync(bucketName: string): S3Bucket {
+        const internal = new S3BucketInternal(this.s3, this.config.region, bucketName);
+        return new S3Bucket(internal, this.config);
+    }
+}
+
+const s3Connection = new CustomS3();
+const bucket = s3Connection.getBucketSync(process.env.AWS_BUCKET as string);
 
 export default bucket;
