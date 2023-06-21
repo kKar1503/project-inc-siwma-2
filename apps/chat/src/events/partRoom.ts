62a196f8029bd29e94b823e6cb93a898bb9b863d
Yam Kar Lok <isonice4572@gmail.com>
2023-06-21 22:03:56 +0800

    added implementation for partRoom

--- apps/chat/src/events/partRoom.ts
+++ apps/chat/src/events/partRoom.ts
@@ -1,6 +1,8 @@
 import { EventFile } from '@inc/types';
-import logger, { eventLogHelper } from '../utils/logger';
+import { eventLogHelper } from '@/utils/logger';
 import { EVENTS } from '@inc/events';
+import RoomOccupantsStore from '@/store/RoomOccupantsStore';
+import SocketUserStore from '@/store/SocketUserStore';
 
 const eventName = EVENTS.CLIENT.ROOM.PART;
 
@@ -10,12 +12,70 @@ const startType: EventFile = (io, socket) => ({
   callback: (roomId) => {
     const eventLog = eventLogHelper(eventName, socket);
 
-    if (socket.rooms.has(roomId)) {
-      socket.leave(roomId);
-      eventLog('info', `Left ${roomId}.`);
-    } else {
+    if (!socket.rooms.has(roomId)) {
       eventLog('warn', `Not in ${roomId}.`);
+      return;
     }
+
+    // Checking if room is in cache. If not, then fetch it from db.
+    eventLog('trace', `Attempting to check if room (${roomId}) is in cache...`);
+    const [roomIdFromCache, [occupant1, occupant2]] =
+      RoomOccupantsStore.searchRoomOccupantsByRoomId(roomId);
+
+    if (roomIdFromCache !== '') {
+      eventLog('warn', `Room (${roomId}) cannot be found in cache.`);
+
+      eventLog('trace', `Fetching current socket userId from cache...`);
+      const [, userId] = SocketUserStore.searchSocketUser('socketId', socket.id);
+      eventLog('debug', `Current socket userId: ${userId}`);
+
+      const otherOccupantId = occupant1 === userId ? occupant2 : occupant1;
+      eventLog('debug', `Other occupant userId: ${otherOccupantId}`);
+
+      eventLog('trace', `Fetching other occupant socketId from cache...`);
+      const [otherOccupantSocketId, _] = SocketUserStore.searchSocketUser(
+        'userId',
+        otherOccupantId
+      );
+
+      const removeRoom = () => {
+        eventLog('trace', `Attempting to remove room (${roomId}) from cache...`);
+        const isRoomRemoved = RoomOccupantsStore.removeRoomOccupantsByRoomId(roomId);
+
+        if (isRoomRemoved) {
+          eventLog('debug', `Room (${roomId}) removed from cache.`);
+        } else {
+          eventLog('warn', `Room (${roomId}) not removed from cache.`);
+        }
+      };
+
+      if (otherOccupantSocketId === '') {
+        eventLog('debug', `Other occupant socketId cannot be found in cache.`);
+        removeRoom();
+      } else {
+        eventLog('debug', `Other occupant socketId found in cache: ${otherOccupantSocketId}`);
+        eventLog(
+          'trace',
+          `Checking if socket (${otherOccupantSocketId}) is connected to room (${roomId})...`
+        );
+        const roomOccupants = io.sockets.adapter.rooms.get(roomId);
+        eventLog('debug', `Room occupants: ${[...roomOccupants]}`);
+
+        if (!roomOccupants.has(otherOccupantSocketId)) {
+          eventLog(
+            'debug',
+            `Socket (${otherOccupantSocketId}) is not connected to room (${roomId}).`
+          );
+          removeRoom();
+        } else {
+          eventLog('debug', `Socket (${otherOccupantSocketId}) is connected to room (${roomId}).`);
+          eventLog('debug', `Room (${roomId}) not removed from cache.`);
+        }
+      }
+    }
+
+    socket.leave(roomId);
+    eventLog('info', `Left ${roomId}.`);
   },
 });
 
