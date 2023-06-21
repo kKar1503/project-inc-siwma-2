c1887b4af9a81a8d7e2d3a7c425984a0dd5b23ad
Yam Kar Lok <isonice4572@gmail.com>
2023-06-21 19:06:20 +0800

    added event listeners re-export

--- apps/chat/src/events/index.ts
+++ apps/chat/src/events/index.ts
@@ -1,6 +1,16 @@
-export { default as roomMessage } from './roomMessage';
-export { default as deleteMessage } from './deleteMessage';
+// export { default as roomMessage } from './roomMessage';
+// export { default as deleteMessage } from './deleteMessage';
+// export { default as newMsgEvent } from './newMessage';
+// export { default as readEvent } from './read';
+
+// ** Room Events
+export { default as joinRoom } from './joinRoom';
+// export { default as partRoom } from './partRoom';
+// export { default as createRoom } from './createRoom';
+// export { default as deleteRoom } from './deleteRoom';
+
+// ** Message Events
+
+// ** Typing Events
 export { default as startType } from './startType';
 export { default as stopType } from './stopType';
-export { default as newMsgEvent } from './newMessage';
-export { default as readEvent } from './read';
