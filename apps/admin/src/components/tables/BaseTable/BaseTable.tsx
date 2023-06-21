eaca9f78c8501b61e8ae9b3399019f1da1da97ed
Xavier Teo <xavierteo26@gmail.com>
2023-06-22 03:05:07 +0800

    refactor: BaseTable props
    - accept data and headers as props

--- apps/admin/src/components/tables/BaseTable/BaseTable.tsx
+++ apps/admin/src/components/tables/BaseTable/BaseTable.tsx
@@ -8,7 +8,7 @@ import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import Checkbox from '@mui/material/Checkbox';
 import { useState } from 'react';
-import BaseTableHead from './BaseTableHead';
+import BaseTableHead, { Header } from './BaseTableHead';
 import BaseTableToolbar from './BaseTableToolbar';
 
 export interface BaseTableData {
@@ -16,73 +16,22 @@ export interface BaseTableData {
   [key: string]: string | number | boolean;
 }
 
-function createData(
-  name: string,
-  calories: number,
-  fat: number,
-  carbs: number,
-  protein: number,
-  enabled?: boolean
-): BaseTableData {
-  return {
-    id: name,
-    name,
-    calories,
-    fat,
-    carbs,
-    protein,
-    enabled: enabled ?? false,
-  };
-}
+type BaseTableProps = {
+  headers: Header[];
+  rows: BaseTableData[];
+};
 
-const headCells = [
-  {
-    key: 'name',
-    label: 'Dessert (100g serving)',
-  },
-  {
-    key: 'calories',
-    label: 'Calories',
-  },
-  {
-    key: 'fat',
-    label: 'Fat (g)',
-  },
-  {
-    key: 'carbs',
-    label: 'Carbs (g)',
-  },
-  {
-    key: 'protein',
-    label: 'Protein (g)',
-  },
-  {
-    key: 'enabled',
-    label: 'Enabled',
-  },
-];
-
-const rows = [
-  createData('Cupcake', 305, 3.7, 67, 4.3, true),
-  createData('Donut', 452, 25.0, 51, 4.9),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-  createData('Honeycomb', 408, 3.2, 87, 6.5),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Jelly Bean', 375, 0.0, 94, 0.0),
-  createData('KitKat', 518, 26.0, 65, 7.0),
-  createData('Lollipop', 392, 0.2, 98, 0.0),
-  createData('Marshmallow', 318, 0, 81, 2.0),
-  createData('Nougat', 360, 19.0, 9, 37.0),
-  createData('Oreo', 437, 18.0, 63, 4.0),
-];
-
-const BaseTable = () => {
+const BaseTable = (props: BaseTableProps) => {
   const [selected, setSelected] = useState<readonly BaseTableData[]>([]);
   const [page, setPage] = useState(0);
   const [rowsPerPage, setRowsPerPage] = useState(5);
 
+  // Destructure props
+  const { rows, headers } = props;
+
+  /**
+   * Event handlers
+   */
   const handleSelectAllClick = (event: React.ChangeEvent<HTMLInputElement>) => {
     if (event.target.checked) {
       const newSelected = rows.map((n) => n);
@@ -143,7 +92,7 @@ const BaseTable = () => {
               numSelected={selected.length}
               onSelectAllClick={handleSelectAllClick}
               rowCount={rows.length}
-              columns={headCells}
+              columns={headers}
             />
             <TableBody>
               {rows.map((row, index) => {
--- apps/admin/src/pages/table-test.tsx
+++ apps/admin/src/pages/table-test.tsx
@@ -1,5 +1,67 @@
-import BaseTable from '@/components/tables/BaseTable/BaseTable';
+import BaseTable, { BaseTableData } from '@/components/tables/BaseTable/BaseTable';
 import Head from 'next/head';
+
+function createData(
+  name: string,
+  calories: number,
+  fat: number,
+  carbs: number,
+  protein: number,
+  enabled?: boolean
+): BaseTableData {
+  return {
+    id: name,
+    name,
+    calories,
+    fat,
+    carbs,
+    protein,
+    enabled: enabled ?? false,
+  };
+}
+
+const headCells = [
+  {
+    key: 'name',
+    label: 'Dessert (100g serving)',
+  },
+  {
+    key: 'calories',
+    label: 'Calories',
+  },
+  {
+    key: 'fat',
+    label: 'Fat (g)',
+  },
+  {
+    key: 'carbs',
+    label: 'Carbs (g)',
+  },
+  {
+    key: 'protein',
+    label: 'Protein (g)',
+  },
+  {
+    key: 'enabled',
+    label: 'Enabled',
+  },
+];
+
+const rows = [
+  createData('Cupcake', 305, 3.7, 67, 4.3, true),
+  createData('Donut', 452, 25.0, 51, 4.9),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+  createData('Honeycomb', 408, 3.2, 87, 6.5),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Jelly Bean', 375, 0.0, 94, 0.0),
+  createData('KitKat', 518, 26.0, 65, 7.0),
+  createData('Lollipop', 392, 0.2, 98, 0.0),
+  createData('Marshmallow', 318, 0, 81, 2.0),
+  createData('Nougat', 360, 19.0, 9, 37.0),
+  createData('Oreo', 437, 18.0, 63, 4.0),
+];
 
 const TableTest = () => (
   <>
@@ -10,7 +72,7 @@ const TableTest = () => (
       <link rel="icon" href="/favicon.ico" />
     </Head>
     <main>
-      <BaseTable />
+      <BaseTable rows={rows} headers={headCells} />
     </main>
   </>
 );
