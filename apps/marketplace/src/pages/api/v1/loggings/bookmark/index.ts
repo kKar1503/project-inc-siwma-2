6f7868d3ef1db1c3a1a3bb8fb6235b0d7145e946
Hein Htet Aung <hhaung5142@gmail.com>
2023-06-21 13:05:17 +0800

    feat: :sparkles: test logging endpoint

---
+++ apps/marketplace/src/pages/api/v1/loggings/bookmark/index.ts
@@ -0,0 +1,26 @@
+import { apiHandler, formatAPIResponse } from '@/utils/api';
+import { NextApiRequest, NextApiResponse } from 'next';
+import PrismaClient from '@inc/db';
+
+const POST = async (req: NextApiRequest, res: NextApiResponse) => {
+  // Validate payload
+  const payload = req.body;
+
+  // Create log message
+  const logId = (
+    await PrismaClient.logs.create({
+      select: {
+        id: true,
+      },
+      data: {
+        logLevel: payload.logLevel,
+        logMessage: payload.logMessage,
+      },
+    })
+  ).id;
+
+  // Return log id
+  res.status(201).json(formatAPIResponse({ logId }));
+};
+
+export default apiHandler().post(POST);
