f3f2bf05264ff64ab3fe41245844872056abfad9
yiningsp <yyiningsp@gmail.com>
2023-06-23 00:05:04 +0800

    perf: adding userid to the logging when user successfully login

--- apps/marketplace/src/pages/api/v1/users/[id]/successfulLogin.ts
+++ apps/marketplace/src/pages/api/v1/users/[id]/successfulLogin.ts
@@ -1,6 +1,8 @@
 import { apiHandler, formatAPIResponse } from '@/utils/api';
 import { apiGuardMiddleware } from '@/utils/api/server/middlewares/apiGuardMiddleware';
+import { userSchema } from '@/utils/api/server/zod';
 import PrismaClient from '@inc/db';
+import { ParamError } from '@inc/errors';
 import { NextApiRequest, NextApiResponse } from 'next';
 
 export default apiHandler({ allowNonAuthenticated: true }).post(
@@ -8,10 +10,15 @@ export default apiHandler({ allowNonAuthenticated: true }).post(
     allowAdminsOnly: true,
   }),
   async (req: NextApiRequest, res: NextApiResponse) => {
+    const { id } = userSchema.userId.parse(req.query);
+    if (!id) {
+      throw new ParamError('id');
+    }
+    
     const creatingSuccessfulLog = await PrismaClient.logs.create({
       data: {
         logLevel: 'info',
-        logMessage: `Sucessful ${req.body.role} Login - user: ${req.body.user}`,
+        logMessage: `Sucessful ${req.body.role} Login - user: ${req.body.username}, userId: ${id}`,
       },
     });
     return res.status(201).json(formatAPIResponse({ logId: creatingSuccessfulLog.id }));
