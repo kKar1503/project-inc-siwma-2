5e6fa467e2ad42068ec576f9ebc1bc0a98073a64
Yam Kar Lok <isonice4572@gmail.com>
2023-06-22 14:08:10 +0800

    added log for prisma client init

--- apps/chat/src/db/index.ts
+++ apps/chat/src/db/index.ts
@@ -1,5 +1,7 @@
 import { PrismaClient } from '@prisma/client';
+import { logger } from '@/utils/logger';
 
+logger.info('Initializing PrismaClient...');
 let prisma = new PrismaClient();
 
 export default prisma;
