39c4fc538002a12cd6f8d54bd2dab91d30cf51a7
Yam Kar Lok <isonice4572@gmail.com>
2023-06-22 12:18:42 +0800

    added implementation for createRoom event listener

--- apps/chat/src/events/createRoom.ts
+++ apps/chat/src/events/createRoom.ts
@@ -1,18 +1,88 @@
-import { EventFile } from '@inc/types';
-import { eventLogHelper } from '../utils/logger';
+import { EventFile, TypedSocketEmitter } from '@inc/types';
+import { eventLogHelper } from '@/utils/logger';
 import { EVENTS } from '@inc/events';
-import crypto from 'node:crypto';
+import prisma from '@/db';
+import SocketUserStore from '@/store/SocketUserStore';
 
 const eventName = EVENTS.CLIENT.ROOM.CREATE;
 
-const startType: EventFile = (_, socket) => ({
+const startType: EventFile = (io, socket) => ({
   eventName: eventName,
   type: 'on',
-  callback: (userId) => {
+  callback: ({ sellerId, listingId }, ack) => {
     const eventLog = eventLogHelper(eventName, socket);
 
-    // TODO: Create the room and get the ID back from db
-    const roomId = crypto.randomUUID();
+    eventLog('trace', `Attempting to retrieve userId for socket (${socket.id}) from cache...`);
+    const [, userId] = SocketUserStore.searchSocketUser('socketId', socket.id);
+
+    eventLog('debug', `Room data (sellerId): ${sellerId}`);
+    eventLog('debug', `Room data (buyerId): ${userId}`);
+    eventLog('debug', `Room data (listingId): ${listingId}`);
+
+    eventLog('trace', `Checking if room already exists in database...`);
+    prisma.rooms
+      .findFirst({
+        where: {
+          AND: [
+            {
+              seller: sellerId,
+            },
+            {
+              buyer: userId,
+            },
+            {
+              listing: listingId,
+            },
+          ],
+        },
+      })
+      .then((room) => {
+        if (room !== null) {
+          eventLog('debug', `Room already exists in database. RoomId: ${room.id}`);
+
+          eventLog('trace', `Acknowledging room already exists...`);
+          ack({ success: false, err: { message: 'Room already exists.' } });
+
+          eventLog('error', `Disconnecting socket... Reason: Room already exists in database.`);
+          socket.disconnect(true);
+          return;
+        } else {
+          eventLog('debug', `Room does not exist in database.`);
+
+          eventLog('trace', `Creating room in database...`);
+          prisma.rooms
+            .create({
+              data: {
+                listing: listingId,
+                seller: sellerId,
+                buyer: userId,
+              },
+            })
+            .then(({ id }) => {
+              eventLog('debug', `Created room in database.`);
+
+              eventLog('trace', `Acknowledging room created...`);
+              ack({ success: true, data: { id } });
+
+              eventLog('trace', `Attempting to retrieve seller socketId from cache...`);
+              const [sellerSocketId] = SocketUserStore.searchSocketUser('userId', sellerId);
+
+              eventLog('trace', `Emitting ${EVENTS.SERVER.ROOM.CREATED} to ${sellerSocketId}...`);
+              (io.to(sellerSocketId).emit as TypedSocketEmitter)(EVENTS.SERVER.ROOM.CREATED, {
+                id,
+                user: userId,
+              });
+            })
+            .catch(() => {
+              eventLog('error', `Failed to create room in database.`);
+              ack({ success: false });
+            });
+        }
+      })
+      .catch(() => {
+        eventLog('error', `Failed to check if room already exists in database.`);
+        ack({ success: false });
+      });
   },
 });
 
