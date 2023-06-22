97c878b1fad8cd23aecb7674ad94994bd79aac7f
Yam Kar Lok <isonice4572@gmail.com>
2023-06-23 00:24:44 +0800

    added index file to re-export event listeners

---
+++ apps/marketplace/src/chat/index.ts
@@ -0,0 +1,12 @@
+// ** Types Imports **
+import type { EventFile } from '@inc/types';
+
+// ** Event Listeners Imports **
+// import roomMessage from './roomMessage';
+
+// ** Event Listeners Compilation **
+const listeners: Record<string, EventFile> = {
+  // roomMessage
+};
+
+export default listeners;
