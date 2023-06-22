3547da4136d749ab2dcf127012f337e1a8601aec
Matthew Ng <matthew.ngjb@gmail.com>
2023-06-22 20:52:51 +0800

    feat: :sparkles: - fetchProfilesListings API now accepts searchQuery and sortBy

--- apps/marketplace/src/middlewares/fetchProfilesListings.tsx
+++ apps/marketplace/src/middlewares/fetchProfilesListings.tsx
@@ -1,22 +1,11 @@
 import listing from '@/utils/api/client/zod/listings';
 import apiClient from '@/utils/api/client/apiClient';
 
-export type SortingOptions =
-  | 'recent_newest'
-  | 'recent_oldest'
-  | 'price_desc'
-  | 'price_asc'
-
-export type FilterOptions = {
-  sortBy?: SortingOptions;
-};
-
-const fetchListing = async (uuid: string, matching?: string, filter?: FilterOptions) => {
+const fetchListing = async (uuid: string, matching?: string, filter?: string) => {
   let query = `v1/users/${uuid}/listings?matching=${matching || ''}`;
-  if (filter?.sortBy) query += `&sortBy=${filter.sortBy}`;
+  if (filter) query += `&sortBy=${filter}`;
   // parse data through zod to ensure data is correct
   const response = await apiClient.get(query);
-  console.log(query)
 
   const listingData = listing.getAll.parse(response.data.data);
   return listingData;
