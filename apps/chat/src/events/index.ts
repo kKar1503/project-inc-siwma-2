ee11f71d4820dce7da56a4f87addd4c3348b8e8d
Yam Kar Lok <isonice4572@gmail.com>
2023-06-22 12:19:08 +0800

    added reexport for createRoom

--- apps/chat/src/events/index.ts
+++ apps/chat/src/events/index.ts
@@ -1,13 +1,13 @@
 // ** Room Events
 export { default as joinRoom } from './joinRoom';
 export { default as partRoom } from './partRoom';
-// export { default as createRoom } from './createRoom';
+export { default as createRoom } from './createRoom';
 // export { default as deleteRoom } from './deleteRoom';
 
 // ** Message Events
 export { default as sendMessage } from './sendMessage';
-export { default as readMessage } from './readMessage';
 export { default as deleteMessage } from './deleteMessage';
+export { default as readMessage } from './readMessage';
 
 // ** Typing Events
 export { default as startType } from './startType';
