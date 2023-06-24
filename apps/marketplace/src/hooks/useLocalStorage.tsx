b952531ea3335acf61c8650a0f774b93b9f5d502
Yam Kar Lok <isonice4572@gmail.com>
2023-06-24 22:21:12 +0800

    added custom implementation to useLocalStorge for refreshing value based on keyChange

--- apps/marketplace/src/hooks/useLocalStorage.tsx
+++ apps/marketplace/src/hooks/useLocalStorage.tsx
@@ -4,6 +4,7 @@ import { Dispatch, SetStateAction, useCallback, useEffect, useState } from 'reac
 
 import useEventListener from '@/hooks/useEventListener';
 import useEventCallback from '@/hooks/useEventCallback';
+import useIsomorphicLayoutEffect from './useIsomorphicLayoutEffect';
 
 declare global {
   interface WindowEventMap {
@@ -34,6 +35,14 @@ export function useLocalStorage<T>(key: string, initialValue: T): [T, SetValue<T
   // State to store our value
   // Pass initial state function to useState so logic is only executed once
   const [storedValue, setStoredValue] = useState<T>(readValue);
+
+  // ** Custom implementation for refreshing the storedValue when key changes **
+  // We will need to debounce this ideally, since this might be "running" too often
+  useIsomorphicLayoutEffect(() => {
+    // Refreshes the storedValue when key changes
+    setStoredValue(readValue());
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [key]);
 
   // Return a wrapped version of useState's setter function that ...
   // ... persists the new value to localStorage.
