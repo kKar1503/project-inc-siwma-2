67a768d0a5730ff5f9486fbe801747994353879b
Yam Kar Lok <isonice4572@gmail.com>
2023-06-24 21:21:43 +0800

    test file changes

--- apps/marketplace/src/pages/test-chat.tsx
+++ apps/marketplace/src/pages/test-chat.tsx
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client';
 import useChat from '@/hooks/useChat';
 import type { Messages } from '@inc/types';
-import { sendMessage, joinRoom, partRoom } from '@/chat/emitters';
+import { sendMessage, joinRoom, partRoom, syncMessage } from '@/chat/emitters';
 import { useEffect, useRef, useState } from 'react';
 
 const socket = io('http://localhost:4000');
@@ -9,13 +9,54 @@ const socket = io('http://localhost:4000');
 const TestChatPage = () => {
   const inputRef = useRef<HTMLInputElement>(null);
   const [messages, setMessages] = useState<Messages[]>([]);
+  const [progress, setProgress] = useState(0);
 
   const { isConnected, loading } = useChat(socket, {
     userId: 'c9f22ccc-0e8e-42bd-9388-7f18a5520c26',
     currentRoom: '',
+    chatMessagesProgressCallback: (progress) => setProgress(progress),
+    messageSyncCallback: (messageSync) => {
+      switch (messageSync.status) {
+        case 'success': {
+          console.log('Sync success');
+          break;
+        }
+        case 'error': {
+          console.log(`Sync error: ${messageSync.err}`);
+          break;
+        }
+        case 'in_progress': {
+          console.log('Sync in progress');
+          console.log(messageSync.progress);
+          console.log(messageSync.message);
+          setMessages((curr) => [...curr, messageSync.message]);
+        }
+      }
+    },
   });
 
-  useEffect(() => {}, []);
+  // useEffect(() => {
+  //   if (isConnected && messages.length === 0) {
+  //     syncMessage(socket, 0, (ack) => {
+  //       if (ack.success) {
+  //         console.log(ack.data);
+  //       } else {
+  //         console.log(ack.err);
+  //       }
+  //     });
+  //   }
+  // }, [isConnected]);
+
+  const sync = () => {
+    setMessages([]);
+    syncMessage(socket, 0, (ack) => {
+      if (ack.success) {
+        console.log(ack.data);
+      } else {
+        console.log(ack.err);
+      }
+    });
+  };
 
   const join = () => {
     joinRoom(socket, '4633cfa1-5a90-4ac4-9bae-5ce3ef14dbf5', (ack) => console.log(ack));
@@ -40,6 +81,7 @@ const TestChatPage = () => {
     <>
       <h1 id="title">Test Chat Page</h1>
       <p id="loading">Loading: {loading}</p>
+      <p id="progress">Progress: {progress}</p>
       <span>Message</span>
       <input id="message" ref={inputRef} />
       <button id="emit-btn" onClick={emit}>
@@ -53,8 +95,12 @@ const TestChatPage = () => {
       <button id="part-btn" onClick={part}>
         Part
       </button>
+      <br />
+      <button id="sync-btn" onClick={sync}>
+        Sync
+      </button>
       {messages.map((message) => {
-        if (message.contentType === 'text') return <p>{message.content}</p>;
+        return <p>{message.content}</p>;
       })}
     </>
   );
