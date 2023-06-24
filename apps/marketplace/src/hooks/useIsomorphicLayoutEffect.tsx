25ab23e71ebb3b1c48d9c232488b39543a767d99
Yam Kar Lok <isonice4572@gmail.com>
2023-06-24 21:39:11 +0800

    changed to default export

--- apps/marketplace/src/hooks/useIsomorphicLayoutEffect.tsx
+++ apps/marketplace/src/hooks/useIsomorphicLayoutEffect.tsx
@@ -1,5 +1,6 @@
 // Reference & Credit: https://usehooks-ts.com/react-hook/use-isomorphic-layout-effect
 import { useEffect, useLayoutEffect } from 'react';
 
-export const useIsomorphicLayoutEffect =
-  typeof window !== 'undefined' ? useLayoutEffect : useEffect;
+const useIsomorphicLayoutEffect = typeof window !== 'undefined' ? useLayoutEffect : useEffect;
+
+export default useIsomorphicLayoutEffect;
