c2850a6b2430a0f964c2162002f00bf8837a625e
Matthew Ng <matthew.ngjb@gmail.com>
2023-06-24 21:34:06 +0800

    chore: :fire: - Remove comments

--- apps/marketplace/src/middlewares/fetchProfilesReview.tsx
+++ apps/marketplace/src/middlewares/fetchProfilesReview.tsx
@@ -5,9 +5,6 @@ const fetchReview = async (uuid: string, sortBy?: string) => {
   let query = `v1/users/${uuid}/reviews`;
   if (sortBy) query += `?sortBy=${sortBy}`;
   const response = await apiClient.get(query);
-  console.log(query);
-  console.log(uuid);
-  console.log(sortBy);
   // parse data through zod to ensure data is correct
   // console.log(response);
   const reviewData = listing.getReviews.parse(response.data.data);
