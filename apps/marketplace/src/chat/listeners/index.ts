f90bc44201fa1a771690a5022717c48a6891e529
Yam Kar Lok <isonice4572@gmail.com>
2023-06-24 20:26:09 +0800

    added syncMessage event listener

--- apps/marketplace/src/chat/listeners/index.ts
+++ apps/marketplace/src/chat/listeners/index.ts
@@ -4,10 +4,12 @@ import type { ClientEventFile } from '@inc/types';
 
 // ** Event Listeners Imports **
 import roomMessage from './roomMessage';
+import syncMessage from './syncMessage';
 
 // ** Event Listeners Compilation **
 const listeners: Record<string, ClientEventFile<UseChatParams>> = {
   roomMessage,
+  syncMessage,
 };
 
 export default listeners;
---
+++ apps/marketplace/src/chat/listeners/syncMessage.ts
@@ -0,0 +1,28 @@
+// ** Types Imports **
+import type { ClientEventFile } from '@inc/types';
+import type { UseChatParams } from '@/types/useChatTypes';
+
+// ** Events Imports **
+import { EVENTS } from '@inc/events';
+
+// ** Event Name **
+const eventName = EVENTS.SERVER.MESSAGE.SYNC;
+
+// ** Event File **
+const event: ClientEventFile<UseChatParams> = (_, hookParams) => ({
+  eventName: eventName,
+  type: 'on', // 'on' | 'once'
+  callback: (messageSync) => {
+    const { chatMessagesProgressCallback, messageSyncCallback } = hookParams;
+
+    if (messageSync.status === 'in_progress' && chatMessagesProgressCallback !== undefined) {
+      chatMessagesProgressCallback(messageSync.progress);
+    }
+
+    if (messageSyncCallback !== undefined) {
+      messageSyncCallback(messageSync);
+    }
+  },
+});
+
+export default event;
