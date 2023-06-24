fc278e0710085ee8ffeb931dfe2385fb51c381ef
Hein Htet Aung <hhaung5142@gmail.com>
2023-06-24 19:32:52 +0800

    feat: :sparkles: comment fix: removal of variant for typography

--- apps/marketplace/src/components/marketplace/carousel/ListingImgsPlaceholder.tsx
+++ apps/marketplace/src/components/marketplace/carousel/ListingImgsPlaceholder.tsx
@@ -2,7 +2,16 @@ import { Box, Typography } from '@mui/material';
 
 const ListingImgsPlaceholder = () => (
   <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', py: '2em' }}>
-    <Typography variant='h1' sx={{fontSize: '32px', color: 'red', textTransform: 'uppercase'}}>No Images available</Typography>
+    <Typography
+      sx={({ typography, palette }) => ({
+        color: palette.error.main,
+        fontWeight: typography.fontWeightMedium,
+        fontSize: '32px',
+        textTransform: 'uppercase',
+      })}
+    >
+      No Images available
+    </Typography>
   </Box>
 );
 
