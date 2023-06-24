64a5a850a4fe3133b86f134196f72c1a01fe12a6
Yam Kar Lok <isonice4572@gmail.com>
2023-06-24 18:49:18 +0800

    added callbacks for syncing messages

--- apps/marketplace/src/types/useChatTypes.ts
+++ apps/marketplace/src/types/useChatTypes.ts
@@ -1,10 +1,11 @@
 // ** Types Imports **
-import type { Dispatch, SetStateAction } from 'react';
-import type { ServerRoomMessage } from '@inc/types';
+import type { MessageSync, ServerRoomMessage } from '@inc/types';
 
 // ** Hooks Params **
 export interface UseChatParams {
   userId: string;
   currentRoom: string;
+  chatMessagesProgressCallback: (progress: number) => void;
   messageCallback?: (message: ServerRoomMessage) => void;
+  messageSyncCallback?: (message: MessageSync) => void;
 }
