17dbdd8e373a5af7262d57dedc2de0a2bc95c0f5
Matthew Ng <matthew.ngjb@gmail.com>
2023-06-26 03:54:51 +0800

    feat: :sparkles: - ListingsTab Filter Buy/Sell Listings working

--- apps/marketplace/src/middlewares/fetchProfilesListings.tsx
+++ apps/marketplace/src/middlewares/fetchProfilesListings.tsx
@@ -1,12 +1,17 @@
 import listing from '@/utils/api/client/zod/listings';
 import apiClient from '@/utils/api/client/apiClient';
 
-const fetchProfilesListings = async (uuid: string, matching?: string, filter?: string) => {
+const fetchProfilesListings = async (
+  uuid: string,
+  matching?: string,
+  sortBy?: string,
+  filter?: string
+) => {
   let query = `v1/users/${uuid}/listings?matching=${matching || ''}`;
-  if (filter) query += `&sortBy=${filter}`;
+  if (sortBy) query += `&sortBy=${sortBy}`;
+  if (filter === 'BUY' || filter === 'SELL') query += `&type=${filter}`;
   // parse data through zod to ensure data is correct
   const response = await apiClient.get(query);
-  console.log(response);
 
   const listingData = listing.getAll.parse(response.data.data);
   return listingData;
