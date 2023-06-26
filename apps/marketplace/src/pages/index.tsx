108e8c33964c75a456b0a1a98d0d47aad392b410
Hein Htet Aung <hhaung5142@gmail.com>
2023-06-26 20:34:32 +0800

    feat: :sparkles: styling of end of infinite scroll

--- apps/marketplace/src/pages/index.tsx
+++ apps/marketplace/src/pages/index.tsx
@@ -202,7 +202,15 @@ const Marketplace = () => {
             loadingComponent={<CircularProgress />}
             parent={Grid}
             endMessage={
-              <Typography variant="h6" textAlign="center" sx={{ marginTop: '2em' }}>
+              <Typography
+                variant="h6"
+                textAlign="center"
+                sx={({ spacing }) => ({
+                  my: spacing(5),
+                  textAlign: 'center',
+                  textTransform: 'uppercase',
+                })}
+              >
                 No more listings available
               </Typography>
             }
