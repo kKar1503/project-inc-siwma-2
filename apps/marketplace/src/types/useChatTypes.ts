9c5ad0cc88629ed859b9bd939647e78124a7ceb9
Yam Kar Lok <isonice4572@gmail.com>
2023-06-26 23:01:14 +0800

    added types for all events' callbacks

--- apps/marketplace/src/types/useChatTypes.ts
+++ apps/marketplace/src/types/useChatTypes.ts
@@ -1,11 +1,19 @@
 // ** Types Imports **
-import type { MessageSync, Messages } from '@inc/types';
+import type { MessageSync, Messages, Room } from '@inc/types';
 
 // ** Hooks Params **
 export interface UseChatParams {
   userId: string;
   currentRoom: string;
-  chatMessagesProgressCallback?: (progress: number) => void;
+  // ** Room Events Callbacks **
+  roomCreateCallback?: (room: Room) => void;
+  roomDeleteCallback?: (roomId: string) => void;
+  // ** Message Events Callbacks **
   messageCallback?: (message: Messages) => void;
+  messageDeleteCallback?: (messageId: string) => void;
+  messageReadCallback?: (messageIds: string[]) => void;
   messageSyncCallback?: (message: MessageSync) => void;
+  chatMessagesProgressCallback?: (progress: number) => void;
+  // ** Typing Events Callbacks **
+  typingCallback?: (userId: string, typing: 'start' | 'stop') => void;
 }
