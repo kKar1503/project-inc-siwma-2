bf01e5ac21f15ebd177c47210758fbb3b8bbfd22
Hein Htet Aung <hhaung5142@gmail.com>
2023-06-27 04:02:33 +0800

    feat: :sparkles: responsive display of image

--- apps/marketplace/src/components/marketplace/createListing/CrossSectionImageTooltip.tsx
+++ apps/marketplace/src/components/marketplace/createListing/CrossSectionImageTooltip.tsx
@@ -2,15 +2,29 @@ import React from 'react';
 import Tooltip from '@mui/material/Tooltip';
 import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
 import S3Image from '@/components/S3Image';
+import { useResponsiveness } from '@inc/ui';
 
 export type CrossSectionImageType = {
   data: string;
 };
 
-const CrossSectionImageTooltip = ({ data }: CrossSectionImageType) => (
-  <Tooltip title={<S3Image src={data} alt="cross section image" height="100" width="100" />}>
-    <InfoOutlinedIcon sx={{ ml: 1 }} />
-  </Tooltip>
-);
+const CrossSectionImageTooltip = ({ data }: CrossSectionImageType) => {
+  const [isSm, isMd, isLg] = useResponsiveness(['sm', 'md', 'lg']);
+
+  return (
+    <Tooltip
+      title={
+        <S3Image
+          src={data}
+          alt="cross section image"
+          height={((isLg || isMd) && '200') || (isSm && '150') || '100'}
+          width={((isLg || isMd) && '200') || (isSm && '150') || '100'}
+        />
+      }
+    >
+      <InfoOutlinedIcon sx={{ ml: 1 }} />
+    </Tooltip>
+  );
+};
 
 export default CrossSectionImageTooltip;
