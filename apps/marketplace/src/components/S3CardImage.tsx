af3a299092163f452783106b41d38d6440575097
Xavier Tay <45095604+catcd1w3r5@users.noreply.github.com>
2023-06-27 01:41:50 +0800

    fix: fixed eslint error
    oops im so sorry

--- apps/marketplace/src/components/S3CardImage.tsx
+++ apps/marketplace/src/components/S3CardImage.tsx
@@ -5,13 +5,11 @@ import S3Image from '@/components/S3Image';
 
 export type S3CardMediaProps = CardMediaProps & { image: string };
 
-const S3CardImage = ({ image, title, ...others }: S3CardMediaProps) => {
-
-  return (
+const S3CardImage = ({ image, title, ...others }: S3CardMediaProps) =>
+  (
     <CardMedia component='div'  {...others}>
       <S3Image src={image} alt={title || ''} fill />
     </CardMedia>
   );
-};
 
 export default S3CardImage;
