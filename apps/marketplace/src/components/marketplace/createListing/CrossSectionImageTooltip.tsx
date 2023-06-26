032dc85aa59262ea5d6bc21d5f1f7387980215cd
Hein Htet Aung <hhaung5142@gmail.com>
2023-06-27 04:39:03 +0800

    feat: :sparkles: Changed image import

--- apps/marketplace/src/components/marketplace/createListing/CrossSectionImageTooltip.tsx
+++ apps/marketplace/src/components/marketplace/createListing/CrossSectionImageTooltip.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
+import Image from 'next/image';
 import Tooltip from '@mui/material/Tooltip';
 import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
-import S3Image from '@/components/S3Image';
 import { useResponsiveness } from '@inc/ui';
 
 export type CrossSectionImageType = {
@@ -14,8 +14,8 @@ const CrossSectionImageTooltip = ({ data }: CrossSectionImageType) => {
   return (
     <Tooltip
       title={
-        <S3Image
-          src={data}
+        <Image
+          src={data === '' ? data : '/images/placeholder.png'}
           alt="cross section image"
           height={((isLg || isMd) && '200') || (isSm && '150') || '100'}
           width={((isLg || isMd) && '200') || (isSm && '150') || '100'}
