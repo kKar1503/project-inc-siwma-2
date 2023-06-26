eb15051b26ff1a2479b042c2bae200ed37ac5737
Yam Kar Lok <isonice4572@gmail.com>
2023-06-26 22:36:55 +0800

    added syncMessage2 listener

--- apps/marketplace/src/chat/listeners/index.ts
+++ apps/marketplace/src/chat/listeners/index.ts
@@ -5,11 +5,13 @@ import type { ClientEventFile } from '@inc/types';
 // ** Event Listeners Imports **
 import roomMessage from './roomMessage';
 import syncMessage from './syncMessage';
+import syncMessage2 from './syncMessage2';
 
 // ** Event Listeners Compilation **
 const listeners: Record<string, ClientEventFile<UseChatParams>> = {
   roomMessage,
   syncMessage,
+  syncMessage2,
 };
 
 export default listeners;
