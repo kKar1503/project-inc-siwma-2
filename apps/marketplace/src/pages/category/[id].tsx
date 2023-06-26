dbadec6e2b31928a1fcf86fb616f679b9db6947f
huishilhs <huishilhs@gmail.com>
2023-06-27 01:02:36 +0800

    fix: :bug: fix filter last id pointer

--- apps/marketplace/src/pages/category/[id].tsx
+++ apps/marketplace/src/pages/category/[id].tsx
@@ -4,14 +4,13 @@ import Grid from '@mui/material/Grid';
 import { useQuery } from 'react-query';
 import { useState, useRef, useEffect } from 'react';
 import { useRouter } from 'next/router';
-import { Box, CircularProgress, Container } from '@mui/material';
+import { CircularProgress, Container } from '@mui/material';
 import Typography from '@mui/material/Typography';
 import categoryListings from '@/middlewares/categoryListings';
 import fetchCatById from '@/middlewares/fetchCatById';
-import searchListings, { FilterOptions } from '@/middlewares/searchListings';
+import { FilterOptions } from '@/middlewares/searchListings';
 import { Listing } from '@/utils/api/client/zod/listings';
-import fetchListings from '@/middlewares/fetchListingsWithCatId';
-import { InfiniteScroll, useResponsiveness } from '@inc/ui';
+import { InfiniteScroll } from '@inc/ui';
 
 const useFetchCatById = (id: string | string[] | undefined) => {
   const data = useQuery(['CatIdListing', id], async () => fetchCatById(id), {
@@ -24,7 +23,6 @@ const useFetchCatById = (id: string | string[] | undefined) => {
 const IndividualCategoryPg = () => {
   const router = useRouter();
   const { id } = router.query;
-  const [isSm, isMd, isLg, isXl] = useResponsiveness(['sm', 'md', 'lg', 'xl']);
   const [filterOptions, setFilterOptions] = useState<FilterOptions>({});
   const [listings, setListings] = useState<Array<Listing>>([]);
   const [lastListingId, setLastListingId] = useState<number>(9);
@@ -37,10 +35,7 @@ const IndividualCategoryPg = () => {
     {
       enabled: id !== undefined,
       onSuccess: (data) => {
-        const lastItem = data[data.length - 1];
-        if (lastItem) {
-          setLastListingId(parseInt(lastItem.id, 10));
-        }
+        setLastListingId(lastListingId + data.length);
 
         if (data.length === 0) {
           setMaxItems(true);
@@ -56,11 +51,10 @@ const IndividualCategoryPg = () => {
   );
 
   useEffect(() => {
-   setLastListingId(0)
-   setListings([])
+    setLastListingId(0);
+    setListings([]);
   }, [filterOptions]);
 
-  // const { data: listingData, isLoading } = useCatListings(id, filterOptions);
   const { data: catData } = useFetchCatById(id);
 
   const Header: HeaderProps = {
@@ -81,50 +75,33 @@ const IndividualCategoryPg = () => {
         subHeader={false}
         isLoading={isLoading}
       >
-        {isLoading && (
-          <Box
-            sx={{
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-              height: '100vh',
-            }}
-          >
-            <CircularProgress />
-          </Box>
-        )}
-
-        {/* {listings && listings.length > 0 && ( */}
-          <InfiniteScroll
-            onLoadMore={refetch}
-            loading={isLoading}
-            reachedMaxItems={maxItems}
-            loadingComponent={<CircularProgress />}
-            parent={Grid}
-            endMessage={
-              <Typography variant="h6" textAlign="center" sx={{ marginTop: '2em' }}>
-                No more listings available
-              </Typography>
-            }
-            parentProps={{
-              container: true,
-              spacing: 2,
-            }}
-            child={Grid}
-            childProps={{
-              item: true,
-              xl: 3,
-              lg: 2.5,
-              md: 3.5,
-              sm: 5,
-              xs: 12,
-            }}
-          >
-            {listings.map((item: Listing) => (
-              <ProductListingItem data={item} showBookmark />
-            ))}
-          </InfiniteScroll>
-        {/* )} */}
+        <InfiniteScroll
+          onLoadMore={refetch}
+          loading={isLoading}
+          reachedMaxItems={maxItems}
+          loadingComponent={<CircularProgress />}
+          parent={Grid}
+          endMessage={
+            <Typography variant="h6" textAlign="center" sx={{ marginTop: '2em' }}>
+              No more listings available
+            </Typography>
+          }
+          parentProps={{
+            container: true,
+            spacing: 2,
+          }}
+          child={Grid}
+          childProps={{
+            item: true,
+            xl: 3,
+            md: 4,
+            xs: 6,
+          }}
+        >
+          {listings.map((item: Listing) => (
+            <ProductListingItem data={item} showBookmark />
+          ))}
+        </InfiniteScroll>
       </DisplayResults>
     </Container>
   );
