d5cdf1c8e74472da9115436d612244c110e6cf04
Yam Kar Lok <isonice4572@gmail.com>
2023-06-26 23:33:45 +0800

    added deletedMessage listener

--- apps/marketplace/src/chat/listeners/index.ts
+++ apps/marketplace/src/chat/listeners/index.ts
@@ -3,19 +3,21 @@ import type { UseChatParams } from '@/types/useChatTypes';
 import type { ClientEventFile } from '@inc/types';
 
 // ** Event Listeners Imports **
-import roomMessage from './roomMessage';
-import syncMessage from './syncMessage';
-import syncMessage2 from './syncMessage2';
 import createdRoom from './createdRoom';
 import deletedRoom from './deletedRoom';
+import roomMessage from './roomMessage';
+import deletedMessage from './deletedMessage';
+import syncMessage from './syncMessage';
+import syncMessage2 from './syncMessage2';
 
 // ** Event Listeners Compilation **
 const listeners: Record<string, ClientEventFile<UseChatParams>> = {
-  roomMessage,
-  syncMessage,
-  syncMessage2,
   createdRoom,
   deletedRoom,
+  roomMessage,
+  deletedMessage,
+  syncMessage,
+  syncMessage2,
 };
 
 export default listeners;
