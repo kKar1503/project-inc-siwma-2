11c06079dbd98ab14dd64ff50cd964f254ea7e21
Karandeep Singh <46585033+ConfuseIous@users.noreply.github.com>
2023-06-28 01:38:56 +0800

    Revert AdvertisementsPlaceholder

--- apps/marketplace/src/components/marketplace/carousel/AdvertisementsPlaceholder.tsx
+++ apps/marketplace/src/components/marketplace/carousel/AdvertisementsPlaceholder.tsx
@@ -1,10 +1,10 @@
 import { Box, Typography } from '@mui/material';
 
 const AdvertisementsPlaceholder = () => (
-  <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', py: '2em' }}>
+  <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', py: '2em' }}>
     <Typography
       variant="h1"
-      sx={{ fontSize: '32px', color: '#C0C2C9', textTransform: 'uppercase', paddingTop: '1' }}
+      sx={{ fontSize: '32px', color: '#C0C2C9', textTransform: 'uppercase' }}
     >
       No Advertisements available
     </Typography>
