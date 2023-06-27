a64164fc0b40f2edc453ed12aeac95d24efba248
Matthew Ng <93643719+RuinedRuins@users.noreply.github.com>
2023-06-28 04:59:12 +0800

    - Removed comments

--- apps/marketplace/src/middlewares/deleteListing.tsx
+++ apps/marketplace/src/middlewares/deleteListing.tsx
@@ -3,7 +3,6 @@ import apiClient from '@/utils/api/client/apiClient';
 
 const deleteListing = async (listingId: string) => {
   const response = await apiClient.delete(`/v1/listings/${listingId}`);
-  console.log(response);
   const responseData = listings.delete.parse(response.data);
   return responseData;
 };
