b89129aeb1c6879594d6ca18ec0c166bc48c8a33
Yam Kar Lok <isonice4572@gmail.com>
2023-06-27 14:36:41 +0800

    added unread logic and change currentRoom to ref

--- apps/marketplace/src/types/useChatTypes.ts
+++ apps/marketplace/src/types/useChatTypes.ts
@@ -1,16 +1,18 @@
 // ** Types Imports **
 import type { MessageSync, Messages, Room, RoomSync } from '@inc/types';
+import { MutableRefObject } from 'react';
 
 // ** Hooks Params **
 export interface UseChatParams {
   userId: string;
-  currentRoom: string;
+  currentRoom: MutableRefObject<string>;
   // ** Room Events Callbacks **
   roomCreateCallback?: (room: Room) => void;
   roomDeleteCallback?: (roomId: string) => void;
   roomSyncCallback?: (room: RoomSync) => void;
   // ** Message Events Callbacks **
   messageCallback?: (message: Messages) => void;
+  messageUnreadCallback?: (message: Messages) => void;
   messageDeleteCallback?: (messageId: number) => void;
   messageReadCallback?: (messageIds: number[]) => void;
   messageSyncCallback?: (message: MessageSync) => void;
