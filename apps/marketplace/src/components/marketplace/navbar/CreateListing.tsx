d53c0fa261a0d71812cd92763732c11d388a779a
Yam Kar Lok <isonice4572@gmail.com>
2023-06-27 14:35:19 +0800

    fixed the missing code

--- apps/marketplace/src/components/marketplace/navbar/CreateListing.tsx
+++ apps/marketplace/src/components/marketplace/navbar/CreateListing.tsx
@@ -1,48 +1,13 @@
-import React, { useMemo } from 'react';
+import React from 'react';
 import Link from '@mui/material/Link';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import { useResponsiveness } from '@inc/ui';
 import { useTranslation } from 'react-i18next';
 import { useTheme } from '@mui/material/styles';
 
 const AddListing = () => {
   const { t } = useTranslation();
-  const [isSm, isMd, isLg] = useResponsiveness(['sm', 'md', 'lg']);
   const { palette } = useTheme();
-
-  const addListingStyles = useMemo(() => {
-    if (isSm) {
-      return {
-        textBtn: {
-          fontSize: '0.5rem',
-          color: palette.common.white,
-        },
-      };
-    }
-    if (isMd) {
-      return {
-        textBtn: {
-          fontSize: '0.6rem',
-          color: palette.common.white,
-        },
-      };
-    }
-    if (isLg) {
-      return {
-        textBtn: {
-          fontSize: '0.8rem',
-          color: palette.common.white,
-        },
-      };
-    }
-    return {
-      textBtn: {
-        fontSize: '0.8rem',
-        color: palette.common.white,
-      },
-    };
-  }, [isSm, isMd, isLg]);
 
   return (
     <Link href="/create-listing" underline="none">
@@ -53,7 +18,7 @@ const AddListing = () => {
           textTransform: 'capitalize',
         }}
       >
-        <Typography noWrap sx={addListingStyles?.textBtn}>
+        <Typography noWrap sx={{ fontSize: '1em', color: palette.common.white }}>
           {t('ADD LISTINGS')}
         </Typography>
       </Button>
