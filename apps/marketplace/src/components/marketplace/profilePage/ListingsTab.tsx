02711f019e9f84f975f8e52db788ce2b8a7bdb20
yiningsp <yyiningsp@gmail.com>
2023-07-05 01:56:44 +0800

    refactor: removing unused listing function
    removing unused listing function under marketplace home page and profile lisitng page

--- apps/marketplace/src/components/marketplace/profilePage/ListingsTab.tsx
+++ apps/marketplace/src/components/marketplace/profilePage/ListingsTab.tsx
@@ -2,10 +2,8 @@ import React, { useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
-import ProductListingItem from '@/components/marketplace/listing/ProductListingItem';
 import { SelectComponent, useResponsiveness, SearchBar } from '@inc/ui';
 import { useTheme } from '@mui/material/styles';
-import { Listing } from '@/utils/api/client/zod';
 
 // pass filter values to select component
 const filterValues = ['All Listings', 'Buy Listings', 'Sell Listings'];
@@ -16,60 +14,25 @@ const sortValuesCn = ['近期', '价格（从高到低）', '价格（从低到�
 const sortEndpointValues = ['recent_newest', 'price_desc', 'price_asc'];
 
 export type ListingsTabProps = {
-  allListings: Listing[] | null | undefined;
   handleSearch: (query: string) => void;
   filterListings: (newData: (typeof filterValues)[number]) => void;
   sortByListings: (newData: (typeof sortValues)[number]) => void;
-  setDel: React.Dispatch<React.SetStateAction<string>>;
+  // setDel: React.Dispatch<React.SetStateAction<string>>;
 };
 
 const ListingsTab = ({
-  allListings,
   handleSearch,
   filterListings,
   sortByListings,
-  setDel,
+  // setDel,
 }: ListingsTabProps) => {
-  const [isSm, isMd, isLg] = useResponsiveness(['sm', 'md', 'lg']);
+  const [isSm] = useResponsiveness(['sm']);
   const { spacing } = useTheme();
 
   const { i18n, t } = useTranslation();
 
   const tFilterValues = i18n.language === 'en' ? filterValues : filterValuesCn;
   const tsortValues = i18n.language === 'en' ? sortValues : sortValuesCn;
-
-  const stylesListing = useMemo(() => {
-    if (isSm) {
-      return {
-        display: 'grid',
-        gridTemplateColumns: 'repeat(2, 1fr)',
-        columnGap: spacing(1),
-        rowGap: spacing(3),
-      };
-    }
-    if (isMd) {
-      return {
-        display: 'grid',
-        gridTemplateColumns: 'repeat(3, 1fr)',
-        columnGap: spacing(1),
-        rowGap: spacing(3),
-      };
-    }
-    if (isLg) {
-      return {
-        display: 'grid',
-        gridTemplateColumns: 'repeat(4, 1fr)',
-        columnGap: spacing(1),
-        rowGap: spacing(3),
-      };
-    }
-    return {
-      display: 'grid',
-      gridTemplateColumns: 'repeat(2, 1fr)',
-      columnGap: spacing(1),
-      rowGap: spacing(3),
-    };
-  }, [isSm, isMd, isLg]);
 
   const styleFilter = useMemo(
     () => ({
@@ -122,12 +85,6 @@ const ListingsTab = ({
         </Box>
       </Box>
       {/* lower portion showing marketplace cards */}
-
-      <Box sx={stylesListing}>
-        {allListings?.map((listing) => (
-          <ProductListingItem data={listing} key={listing.id} setDel={setDel} />
-        ))}
-      </Box>
     </Box>
   );
 };
