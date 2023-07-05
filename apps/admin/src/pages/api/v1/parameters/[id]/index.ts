c8d95a9f48336ce763d1e6189e697537d2f5a5f1
Matthew Ng <matthew.ngjb@gmail.com>
2023-07-05 20:52:11 +0800

    refactor: :recycle: - Moved APIs and AuthGuard over from marketplace to admin

---
+++ apps/admin/src/pages/api/v1/parameters/[id]/index.ts
@@ -0,0 +1,115 @@
+import { apiHandler } from '@/utils/api';
+import PrismaClient from '@inc/db';
+import { NotFoundError } from '@inc/errors';
+import { paramSchema } from '@/utils/api/server/zod';
+import { formatParamResponse, validateOptions } from '..';
+
+// -- Functions --//
+function parseParamid($id: string) {
+  // Parse and validate param id provided
+  const id = parseInt($id, 10);
+
+  // Check if the parameter id is valid
+  if (Number.isNaN(id)) {
+    throw new NotFoundError(`Parameter with id '${id}'`);
+  }
+
+  return id;
+}
+
+/**
+ * Checks if a parameter exists
+ * @param id The parameter id
+ * @returns The parameter if it exists
+ */
+async function checkParamExists($id: string | number) {
+  // Parse and validate param id provided
+  const id = typeof $id === 'number' ? $id : parseParamid($id);
+
+  // Check if the parameter exists
+  const parameter = await PrismaClient.parameter.findUnique({
+    where: {
+      id,
+    },
+  });
+
+  // Check if the parameter exists
+  if (!parameter) {
+    throw new NotFoundError(`Parameter with id '${id}'`);
+  }
+
+  return parameter;
+}
+
+export default apiHandler({
+  allowAdminsOnly: true,
+})
+  .put(async (req, res) => {
+    // Update an existing parameter
+    // Parse and validate param id provided
+    const id = parseParamid(req.query.id as string);
+
+    // Check if the parameter exists
+    await checkParamExists(id);
+
+    // Parse and validate the request body
+    const data = paramSchema.post.body.partial().parse(req.body);
+
+    // Validate parameter options
+    if (data.type != null) {
+      validateOptions({ type: data.type, options: data.options });
+    }
+
+    // Update the parameter in the database
+    const updatedParams = await PrismaClient.parameter.update({
+      where: {
+        id,
+      },
+      data: {
+        ...data,
+        options: data.type !== 'MANY_CHOICES' && data.type !== 'TWO_CHOICES' ? [] : data.options,
+      },
+    });
+
+    // Return the result
+    res.status(200).json(formatParamResponse(updatedParams));
+  })
+  .patch(async (req, res) => {
+    // Toggles the active state of a parameter
+    // Parse and validate param id provided
+    const id = parseParamid(req.query.id as string);
+
+    // Check if the parameter exists
+    const parameter = await checkParamExists(id);
+
+    // Update the active state of the parameter
+    await PrismaClient.parameter.update({
+      where: {
+        id,
+      },
+      data: {
+        active: !parameter.active,
+      },
+    });
+
+    // Return 204 no content
+    res.status(204).end();
+  })
+  .delete(async (req, res) => {
+    // Deletes a parameter
+    // Parse and validate param id provided
+    const id = parseParamid(req.query.id as string);
+
+    // Check if the parameter exists
+    await checkParamExists(id);
+
+    // Delete the parameter from the database
+    await PrismaClient.parameter.delete({
+      where: {
+        id,
+      },
+    });
+
+    // Return 204 no content
+    res.status(204).end();
+  });
