1179af2df3c073f41e4b20b7a25047209d7c4fc7
Xavier Tay <45095604+catcd1w3r5@users.noreply.github.com>
2023-07-09 16:32:13 +0800

    refactor: changed folder structure and conventions to match marketplace

--- apps/admin/src/components/advertisementsDashboard/clicksPie/index.tsx
+++ apps/admin/src/components/advertisementsDashboard/clicksPie/index.tsx
@@ -1,3 +1,47 @@
-import clicksPie from './clicksPie';
+import { VictoryPie, VictoryTheme, VictoryTooltip } from 'victory';
+import ModuleBase from '@/components/advertisementsDashboard/moduleBase';
 
-export default clicksPie;
+export interface ClicksPieProps {
+  data: Array<{
+    company: string;
+    clicks: number;
+  }>;
+}
+
+const Index = ({ data }: ClicksPieProps) => {
+  const formattedData = data.map((item) => ({
+    x: item.company,
+    y: item.clicks,
+    label: `${item.company}\n${item.clicks}`,
+  }));
+  return (
+    <ModuleBase>
+      <VictoryPie
+        labelComponent={<VictoryTooltip />}
+        theme={VictoryTheme.material}
+        innerRadius={() => 100}
+        events={[{
+          target: 'data',
+          eventHandlers: {
+            onMouseOver: () => [
+              {
+                target: 'labels',
+                mutation: () => ({ active: true }),
+              },
+            ],
+            onMouseOut: () => [
+              {
+                target: 'labels',
+                mutation: () => ({ active: false }),
+              },
+            ],
+          },
+        }]}
+        data={formattedData}
+      />
+    </ModuleBase>
+  );
+};
+
+
+export default Index;
