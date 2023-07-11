0167d15dd57f9752872a7d2f219549d20c9e3ba2
Xavier Tay <45095604+catcd1w3r5@users.noreply.github.com>
2023-07-11 13:14:32 +0800

    refactor: made underlying graph component reusable

--- apps/admin/src/components/advertisementsDashboard/activeGraph/index.tsx
+++ apps/admin/src/components/advertisementsDashboard/activeGraph/index.tsx
@@ -1,102 +1,41 @@
-import { VictoryAxis, VictoryBar, VictoryChart, VictoryTheme, VictoryTooltip } from 'victory';
 import ModuleBase from '@/components/advertisementsDashboard/moduleBase';
 import Title from '@/components/advertisementsDashboard/analyticsOverlay/title';
+import DataGraph from '@/components/graphs/dataGraph';
 
 const data = [
-  { month: 1, adSpaces: 5, label: '5' },
-  { month: 2, adSpaces: 1, label: '1'  },
-  { month: 3, adSpaces: 2 , label: '2' },
-  { month: 4, adSpaces: 4 , label: '4' },
-  { month: 5, adSpaces: 4 , label: '4' },
-  { month: 6, adSpaces: 2 , label: '2' },
-  { month: 7, adSpaces: 4 , label: '4' },
-  { month: 8, adSpaces: 2 , label: '2' },
-  { month: 9, adSpaces: 3 , label: '3' },
-  { month: 10, adSpaces: 4, label: '4'  },
-  { month: 11, adSpaces: 0 , label: '0' },
-  { month: 12, adSpaces: 0 , label: '0' },
+  { id: 1, value: 5, label: '5' },
+  { id: 2, value: 1, label: '1'  },
+  { id: 3, value: 2 , label: '2' },
+  { id: 4, value: 4 , label: '4' },
+  { id: 5, value: 4 , label: '4' },
+  { id: 6, value: 2 , label: '2' },
+  { id: 7, value: 4 , label: '4' },
+  { id: 8, value: 2 , label: '2' },
+  { id: 9, value: 3 , label: '3' },
+  { id: 10, value: 4, label: '4'  },
+  { id: 11, value: 0 , label: '0' },
+  { id: 12, value: 0 , label: '0' },
 ];
 
-const ExampleGraph = () => (
+const format = [
+  {id:1, display: 'Jan'},
+  {id:2, display: 'Feb'},
+  {id:3, display: 'Mar'},
+  {id:4, display: 'Apr'},
+  {id:5, display: 'May'},
+  {id:6, display: 'Jun'},
+  {id:7, display: 'Jul'},
+  {id:8, display: 'Aug'},
+  {id:9, display: 'Sep'},
+  {id:10, display: 'Oct'},
+  {id:11, display: 'Nov'},
+  {id:12, display: 'Dec'}
+]
+const ActiveGraph = () => (
   <ModuleBase>
     <Title title='Number of active ad-spaces per month' subtitle='Year 2022' />
-    <VictoryChart
-      // adding the material theme provided with Victory
-      theme={VictoryTheme.material}
-      domainPadding={20}
-    >
-      <VictoryAxis
-        theme={VictoryTheme.material}
-        tickValues={[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]}
-        tickFormat={['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']}
-        style={{
-          ticks:
-            {
-              stroke: 'none',
-            },
-          tickLabels:
-            {
-              fontSize: 10,
-              padding: 5,
-            },
-        }}
-      />
-      <VictoryAxis
-        theme={VictoryTheme.material}
-        dependentAxis
-        tickFormat={x => x.toFixed(1)}
-        style={{
-          ticks:
-            {
-              stroke: 'none',
-            },
-          tickLabels:
-            {
-              fontSize: 10,
-              padding: 5,
-            },
-        }}
-      />
-      <VictoryBar
-        barRatio={0.8}
-        data={data}
-        x='month'
-        y='adSpaces'
-        labelComponent={<VictoryTooltip />}
-        events={[{
-          target: 'data',
-          eventHandlers: {
-            onMouseOver: () => [
-              {
-                target: 'labels',
-                mutation: () => ({ active: true }),
-              },
-              {
-                target: 'data',
-                mutation: () => ({ style: { fill: '#ea3b59' } }),
-              },
-            ],
-            onMouseOut: () => [
-              {
-                target: 'data',
-                mutation: () => ({ style: undefined }),
-              },
-              {
-                target: 'labels',
-                mutation: () => ({ active: false }),
-              },
-            ],
-          },
-        }]}
-        style={{
-          data: {
-            fill: '#d7d7d7',
-          },
-        }}
-        cornerRadius={{ top: 7, bottom: 7 }}
-      />
-    </VictoryChart>
+    <DataGraph data={data} format={format} style={{fillColor:'#d7d7d7' ,hoverColor:'#ea3b59' }}/>
   </ModuleBase>
   );
 
-export default ExampleGraph;
+export default ActiveGraph;
---
+++ apps/admin/src/components/graphs/dataGraph/index.tsx
@@ -0,0 +1,106 @@
+import { VictoryAxis, VictoryBar, VictoryChart, VictoryTheme, VictoryTooltip } from 'victory';
+
+export interface DataGraphElement {
+  id: number;
+  value: number;
+  label: string;
+}
+
+export interface DataGraphFormatElement {
+  id: number;
+  display: string;
+}
+
+export interface DataGraphProps {
+  data: DataGraphElement[];
+  format: Array<DataGraphFormatElement>;
+  style: {
+    fillColor: string;
+    hoverColor: string;
+  };
+}
+
+const DataGraph = ({ data, format, style }: DataGraphProps) => {
+  const tickValues = format.map((item) => item.id);
+  const tickFormat = format.map((item) => item.display);
+  return (
+    <VictoryChart
+      // adding the material theme provided with Victory
+      theme={VictoryTheme.material}
+      domainPadding={20}
+    >
+      <VictoryAxis
+        theme={VictoryTheme.material}
+        tickValues={tickValues}
+        tickFormat={tickFormat}
+        style={{
+          ticks:
+            {
+              stroke: 'none',
+            },
+          tickLabels:
+            {
+              fontSize: 10,
+              padding: 5,
+            },
+        }}
+      />
+      <VictoryAxis
+        theme={VictoryTheme.material}
+        dependentAxis
+        tickFormat={x => x.toFixed(1)}
+        style={{
+          ticks:
+            {
+              stroke: 'none',
+            },
+          tickLabels:
+            {
+              fontSize: 10,
+              padding: 5,
+            },
+        }}
+      />
+      <VictoryBar
+        barRatio={0.8}
+        data={data}
+        x='id'
+        y='value'
+        labelComponent={<VictoryTooltip />}
+        events={[{
+          target: 'data',
+          eventHandlers: {
+            onMouseOver: () => [
+              {
+                target: 'labels',
+                mutation: () => ({ active: true }),
+              },
+              {
+                target: 'data',
+                mutation: () => ({ style: { fill: style.hoverColor} }),
+              },
+            ],
+            onMouseOut: () => [
+              {
+                target: 'data',
+                mutation: () => ({ style: undefined }),
+              },
+              {
+                target: 'labels',
+                mutation: () => ({ active: false }),
+              },
+            ],
+          },
+        }]}
+        style={{
+          data: {
+            fill: style.fillColor,
+          },
+        }}
+        cornerRadius={{ top: 7, bottom: 7 }}
+      />
+    </VictoryChart>
+  );
+};
+
+export default DataGraph;
