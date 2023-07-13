2bb07b2b8ffd72866c59ff271340dc62ec0df6fb
yiningsp <yyiningsp@gmail.com>
2023-07-14 02:05:40 +0800

    feat: coding of category table with integration

--- apps/admin/src/pages/view-category.tsx
+++ apps/admin/src/pages/view-category.tsx
@@ -1,53 +1,75 @@
 import BaseTable, { BaseTableData } from '@/components/tables/BaseTable/BaseTable';
 import { Header } from '@/components/tables/BaseTable/BaseTableHead';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import { useQuery } from 'react-query';
 import fetchCategories from '@/middlewares/fetchCategories';
 import { CategoryResponseBody } from '@/utils/api/client/zod';
-import { useEffect, useState } from 'react';
-import { useQuery } from 'react-query';
 
 export type CategoryProps = {
   data: CategoryResponseBody[];
 };
 
 export type CategoryTableProps = {
-  data: CategoryProps[];
+  data: CategoryResponseBody[];
 };
+
+function createData(id: string, name: string, active: boolean): BaseTableData {
+  return {
+    id,
+    name,
+    active,
+  };
+}
+
+const headCells: Header[] = [
+  {
+    key: 'name',
+    label: 'Category Name',
+  },
+  {
+    key: 'name',
+    label: 'Category Name (Chinese)',
+  },
+  {
+    key: 'active',
+    label: 'Status',
+    replace: {
+      true: 'Active',
+      false: 'Inactive',
+    },
+  },
+];
 
 const useCategoryPageQuery = () => {
   const { data } = useQuery('cat', async () => fetchCategories());
   return data;
 };
 
-const headCells: Header[] = [
-  {
-    key: 'name',
-    label: 'Category',
-  },
-  {
-    key: 'name chinese',
-    label: 'Category (Chinese)',
-  },
-  {
-    key: 'status',
-    label: 'Status',
-  },
-];
-
-const CategoryTable = ({ data }: CategoryTableProps) => {
+const CategoryTable = () => {
   const [page, setPage] = useState(0);
   const [rowsPerPage, setRowsPerPage] = useState(5);
   const [rows, setRows] = useState<BaseTableData[]>([]);
-  const categoryQuery = useCategoryPageQuery();
-  console.log(categoryQuery);
+  const category = useCategoryPageQuery();
+
+  const router = useRouter();
 
   const sortRows = (): void => {
-    // const rowsData: BaseTableData[] = data.map((item: CategoryProps) => ({
-    //   id: item.data[0].id,
-    //   category: item.data[0].name,
-    //   chinese: item.data[0].name,
-    //   status: item.data[0].active ? 'Active' : 'Inactive',
-    // }));
-    // setRows(rowsData);
+    const rowsData: BaseTableData[] = [];
+
+    category?.forEach((item) => {
+      rowsData.push(createData(item.id, item.name, item.active));
+    });
+
+    setRows(rowsData);
+  };
+
+  const handleEdit = (row: BaseTableData) => {
+    const { id } = row;
+    const editUrl = `/edit-category/${id}`;
+    router.push(editUrl);
   };
 
   const handleChangePage = (event: unknown, newPage: number) => {
@@ -55,29 +77,41 @@ const CategoryTable = ({ data }: CategoryTableProps) => {
   };
 
   const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setRowsPerPage(parseInt(event.target.value, 10));
+    const newRowsPerPage = parseInt(event.target.value, 10);
+    setRowsPerPage(newRowsPerPage);
     setPage(0);
   };
 
   useEffect(() => {
     sortRows();
-  }, [data]);
+  }, [category]);
+
+  const startIndex = page * rowsPerPage;
+  const endIndex = startIndex + rowsPerPage;
+
+  const pageRows = rows.slice(startIndex, endIndex);
 
   return (
-    <BaseTable
-      heading="Categories"
-      rows={rows}
-      headers={headCells}
-      onPageChange={handleChangePage}
-      onRowsPerPageChange={handleChangeRowsPerPage}
-      onDelete={() => console.log('delete')}
-      onEdit={() => console.log('edit')}
-      onToggle={() => console.log('toggle')}
-      page={page}
-      rowsPerPage={rowsPerPage}
-      rowsPerPageOptions={[5, 10, 25]}
-      totalCount={rows.length}
-    />
+    <Container>
+      <Box sx={{ display: 'flex', justifyContent: 'center', mt: 3 }}>
+        <Box sx={{ width: '80%' }}>
+          <BaseTable
+            heading="Categories"
+            rows={pageRows}
+            headers={headCells}
+            onPageChange={handleChangePage}
+            onRowsPerPageChange={handleChangeRowsPerPage}
+            onDelete={() => console.log('delete')}
+            onEdit={handleEdit}
+            onToggle={() => console.log('toggle')}
+            page={page}
+            rowsPerPage={rowsPerPage}
+            rowsPerPageOptions={[5, 10, 25]}
+            totalCount={rows.length}
+          />
+        </Box>
+      </Box>
+    </Container>
   );
 };
 
