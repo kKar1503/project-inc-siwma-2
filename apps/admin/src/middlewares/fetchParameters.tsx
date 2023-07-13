4c9767d1b9c03d06f4af9a45dbb55e10792bbe09
Charmaine Choo <charmainechoo288@gmail.com>
2023-07-13 19:59:46 +0800

    feat: created admin parameters table in parameters page

---
+++ apps/admin/src/middlewares/fetchParameters.tsx
@@ -0,0 +1,15 @@
+import apiClient from '@/utils/api/client/apiClient';
+import parameters, { Parameter } from '@/utils/api/client/zod/parameters';
+
+const fetchParameters = async () => {
+  const response = await apiClient.get(`/v1/parameters`);
+
+  const parsedParams = parameters.getAll.parse(response.data.data);
+  const hashMap: Record<string, Parameter> = {};
+  parsedParams.forEach((param) => {
+    hashMap[param.id] = param;
+  });
+  return hashMap;
+};
+
+export default fetchParameters;
---
+++ apps/admin/src/pages/parameters.tsx
@@ -0,0 +1,124 @@
+import BaseTable, { BaseTableData } from '@/components/tables/BaseTable/BaseTable';
+import { Header } from '@/components/tables/BaseTable/BaseTableHead';
+import Container from '@mui/material/Container';
+import useResponsiveness from '@inc/ui/lib/hook/useResponsiveness';
+import { useTheme } from '@mui/material/styles';
+import fetchParameters from '@/middlewares/fetchParameters';
+import { ParameterResponseBody } from '@/utils/api/client/zod';
+import { useState, useMemo} from 'react';
+import { useQuery } from 'react-query';
+import Head from 'next/head';
+
+export type ParameterProps = {
+  data: ParameterResponseBody[];
+};
+
+export type ParameterTableProps = {
+  data: ParameterProps[];
+};
+
+const useParameterQuery = () => {
+  const { data } = useQuery('parameter', async () => fetchParameters());
+  return data;
+};
+
+const headCells: Header[] = [
+  {
+    key: 'parameter name',
+    label: 'Parameter Name',
+  },
+  {
+    key: 'display name',
+    label: 'Display Name',
+  },
+  {
+    key: 'parameter type',
+    label: 'Parameter Type',
+  },
+  {
+    key: 'data type',
+    label: 'Data Type',
+  },
+  {
+    key: 'status',
+    label: 'Status',
+  },
+  {
+    key: 'enabled',
+    label: 'Enabled',
+    replace: {
+      a: 'Yes',
+      false: 'No',
+    },
+  },
+];
+
+const ParameterTable = ({ data }: ParameterTableProps) => {
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+  const [rows, setRows] = useState<BaseTableData[]>([]);
+  const parameterQuery = useParameterQuery();
+  console.log(useParameterQuery);
+  const theme = useTheme();
+  const { spacing } = theme;
+  const [isSm, isMd, isLg] = useResponsiveness(['sm', 'md', 'lg']);
+  const tableStyle = useMemo(() => {
+    if (isSm) {
+      return {
+        py: spacing(3),
+        px: '20px',
+      };
+    }
+    if (isMd) {
+      return {
+        py: spacing(3),
+        px: '40px',
+      };
+    }
+    if (isLg) {
+      return {
+        py: spacing(3),
+        px: '60px',
+      };
+    }
+    return {
+      py: spacing(3),
+      px: '20px',
+    };
+  }, [isSm, isMd, isLg]);
+
+  const handleChangePage = (event: unknown, newPage: number) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  return (
+    <>
+      <Head>
+        <title>Parameters</title>
+      </Head>
+      <Container sx={tableStyle}>
+        <BaseTable
+          heading="Parameters"
+          rows={rows}
+          headers={headCells}
+          onPageChange={handleChangePage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+          onDelete={() => console.log('delete')}
+          onEdit={() => console.log('edit')}
+          onToggle={() => console.log('toggle')}
+          page={page}
+          rowsPerPage={rowsPerPage}
+          rowsPerPageOptions={[5, 10, 25]}
+          totalCount={rows.length}
+        />
+      </Container>
+    </>
+  );
+};
+
+export default ParameterTable;
