9f0fa3a74e1e79f2937a1ae00e4fbfa0512ae689
Mokke-31 <107909574+Mokke-31@users.noreply.github.com>
2023-07-16 15:12:15 +0800

    feat: zod for listing item for client folder
    created new zod for listing item for client folder

---
+++ apps/admin/src/utils/api/client/zod/listingItems.ts
@@ -0,0 +1,49 @@
+import { z } from 'zod';
+
+// -- Define properties -- //
+const id = z.string();
+const name = z.string();
+const chineseName = z.string();
+const description = z.string();
+const categoryId = z.string();
+const unit = z.string();
+const chineseUnit = z.string();
+const createdAt = z.string();
+
+// -- Define listing schema -- //
+const listingItem = z.object({
+  id,
+  name,
+  chineseName,
+  description,
+  categoryId,
+  unit,
+  chineseUnit,
+  createdAt,
+});
+
+// POST /listingItems
+const createListingItem = z.object({ listingItemId: id });
+
+// GET /listingItems
+const getListingItems = z.array(listingItem);
+
+// GET /listingItems/:id
+const getListingItem = listingItem;
+
+// PUT /listingItems/:id
+const updateListingItem = listingItem;
+
+// DELETE /listingItems/:id
+const deleteListingItem = z.object({});
+
+export type ListingItemResponseBody = z.infer<typeof listingItem>;
+export type ListingItem = z.infer<typeof listingItem>;
+
+export default {
+  create: createListingItem,
+  getAll: getListingItems,
+  getById: getListingItem,
+  update: updateListingItem,
+  delete: deleteListingItem,
+};
