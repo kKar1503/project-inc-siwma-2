be020be81725207f3d2fe3c8459a68162141a960
Hein Htet Aung <hhaung5142@gmail.com>
2023-07-18 03:00:59 +0800

    feat: :sparkles: removed unused code

--- apps/admin/src/pages/company-management/index.tsx
+++ apps/admin/src/pages/company-management/index.tsx
@@ -1,56 +1,24 @@
-import React, { useEffect, useState } from 'react';
-import { useQuery } from 'react-query';
-import { Company } from '@/utils/api/client/zod/companies';
-import fetchCompanies from '@/middlewares/company-management/fetchCompanies';
+import React from 'react';
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
 import CompanyTable from './companyTable';
 import RegisterCompanyCard from './registerCompanyCard';
 import BulkRegisterCompanyCard from './bulkRegisterCompanyCard';
 
-const useGetCompaniesQuery = () => {
-  const { data } = useQuery('companies', async () => fetchCompanies(), {
-    refetchOnWindowFocus: false,
-    refetchOnReconnect: false,
-  });
-
-  return data;
-};
-
-const CompanyManagement = () => {
-  const companies = useGetCompaniesQuery();
-  const [companiesData, setcompaniesData] = useState<Company[]>();
-
-  // const handleCompaniesChange = async (companies: Company[]) => {
-  //   setcompaniesData(companies);
-  // };
-
-  // useEffect(() => {
-  //   if (companies) {
-  //     handleCompaniesChange(companies);
-  //   }
-  // }, [companies]);
-
-  // const updateCompanyData = async () => {
-  //   const updatedCompanies = await fetchCompanies();
-  //   handleCompaniesChange(updatedCompanies);
-  // };
-
-  return (
-    <Container maxWidth="lg" sx={{ my: 2 }}>
-      <Grid container spacing={2}>
-        <Grid item xs={12} lg={6}>
-          <RegisterCompanyCard />
-        </Grid>
-        <Grid item xs={12} lg={6}>
-          <BulkRegisterCompanyCard />
-        </Grid>
-        <Grid item xs={12}>
-          <CompanyTable />
-        </Grid>
+const CompanyManagement = () => (
+  <Container maxWidth="lg" sx={{ my: 2 }}>
+    <Grid container spacing={2}>
+      <Grid item xs={12} lg={6}>
+        <RegisterCompanyCard />
       </Grid>
-    </Container>
-  );
-};
+      <Grid item xs={12} lg={6}>
+        <BulkRegisterCompanyCard />
+      </Grid>
+      <Grid item xs={12}>
+        <CompanyTable />
+      </Grid>
+    </Grid>
+  </Container>
+);
 
 export default CompanyManagement;
