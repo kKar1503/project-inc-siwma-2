5c9d5f3f1b066ae6de0ed20435e3ef191104d302
Adeeb <adityaadeeb04@gmail.com>
2023-07-18 16:59:44 +0800

    adding backend integration and updating fake data

--- apps/marketplace/src/pages/compare-listings/CompareDifferences.tsx
+++ apps/marketplace/src/pages/compare-listings/CompareDifferences.tsx
@@ -1,15 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
+import { useLocation } from 'react-router-dom';
 import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import fetchListing from '@/middlewares/fetchListing';
+import { Listing } from '@/utils/api/client/zod/listings';
 
 interface TableRowData {
     id: string;
-    data: string;
+    data: string[];
 }
 
 interface TableData {
@@ -17,7 +20,28 @@ interface TableData {
     rows: TableRowData[];
 }
 
-const CompareDifferences: React.FC = () => {
+interface CompareDifferencesProps {
+    productIds: string[];
+}
+
+
+
+
+const CompareDifferences: React.FC<CompareDifferencesProps> = ({ productIds }) => {
+    productIds = ['1', '2', '3'];
+    const [listings, setListings] = useState<Listing[]>([]);
+    console.log('Product IDs:', productIds);
+    useEffect(() => {
+        const fetchListings = async () => {
+            const listings = await Promise.all(productIds.map((id) => fetchListing(id)));
+            setListings(listings);
+        };
+
+        fetchListings();
+    }, [productIds]);
+
+    console.log('Listings:', listings);
+
     const tableData: TableData = {
         sideHeaders: [
             'Dimensions',
@@ -32,16 +56,16 @@ const CompareDifferences: React.FC = () => {
             'Company',
         ],
         rows: [
-            { id: 'row1', data: 'Length: 355, Width: 255, Height: 44, Density: 150' },
-            { id: 'row2', data: '$ 999 / kg' },
-            { id: 'row3', data: '1000kg' },
-            { id: 'row4', data: 'Good' },
-            { id: 'row5', data: 'Round Bars' },
-            { id: 'row6', data: 'No' },
-            { id: 'row7', data: 'S233' },
-            { id: 'row8', data: 'S233' },
-            { id: 'row9', data: 'image' },
-            { id: 'row10', data: 'Hi Metals Pte Ltd' },
+            { id: 'row1', data: ['Length: 355, Width: 255, Height: 44, Density: 150', 'Length: 400, Width: 300, Height: 50, Density: 200', 'Length: 450, Width: 350, Height: 55, Density: 250'] },
+            { id: 'row2', data: ['$ 999 / kg', '$ 1099 / kg', '$ 1199 / kg'] },
+            { id: 'row3', data: ['1000kg', '2000kg', '3000kg'] },
+            { id: 'row4', data: ['Good', 'Excellent', 'Fair'] },
+            { id: 'row5', data: ['Round Bars', 'Square Bars', 'Hexagonal Bars'] },
+            { id: 'row6', data: ['No', 'Yes', 'No'] },
+            { id: 'row7', data: ['S233', 'S244', 'S255'] },
+            { id: 'row8', data: ['S233', 'S244', 'S255'] },
+            { id: 'row9', data: ['image1', 'image2', 'image3'] },
+            { id: 'row10', data: ['Hi Metals Pte Ltd', 'MetalWorks Inc.', 'SteelCorp Ltd.'] },
         ],
     };
 
@@ -51,14 +75,18 @@ const CompareDifferences: React.FC = () => {
                 <TableHead>
                     <TableRow>
                         <TableCell>Key Specs</TableCell>
-                        <TableCell>Steel Metal Bar</TableCell>
+                        <TableCell>Steel Metal Bar 1</TableCell>
+                        <TableCell>Steel Metal Bar 2</TableCell>
+                        <TableCell>Steel Metal Bar 3</TableCell>
                     </TableRow>
                 </TableHead>
                 <TableBody>
                     {tableData.sideHeaders.map((header, index) => (
                         <TableRow key={header}>
                             <TableCell>{header}</TableCell>
-                            <TableCell>{tableData.rows[index]?.data}</TableCell>
+                            {tableData.rows[index]?.data.map((cellData) => (
+                                <TableCell key={cellData}>{cellData}</TableCell>
+                            ))}
                         </TableRow>
                     ))}
                 </TableBody>
