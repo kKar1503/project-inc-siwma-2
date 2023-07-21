3f8f49b7d5d05356357158cdb793f8e4cc17c9df
Xavier Tay <45095604+catcd1w3r5@users.noreply.github.com>
2023-07-22 03:02:01 +0800

    fix: deleteAdvertisement.ts returns correct type

--- apps/admin/src/middlewares/advertisements/deleteAdvertisement.ts
+++ apps/admin/src/middlewares/advertisements/deleteAdvertisement.ts
@@ -1,7 +1,7 @@
 import apiClient from '@/utils/api/client/apiClient';
 import advertisements from '@/utils/api/client/zod/advertisements';
 
-const deleteAdvertisement = async (id: string) : Promise<unknown> => {
+const deleteAdvertisement = async (id: string): Promise<Record<string, never>> => {
   const response = await apiClient.delete(`/v1/advertisements/${id}`);
   return advertisements.delete.parse(response.data.data[0]);
 };
