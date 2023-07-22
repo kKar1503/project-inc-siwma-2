2e713a13c31c708b51c27fd4e42f5181ae3ed4f3
Xavier Tay <45095604+catcd1w3r5@users.noreply.github.com>
2023-07-22 17:43:47 +0800

    feat: added error handling

--- apps/marketplace/src/components/S3Avatar.tsx
+++ apps/marketplace/src/components/S3Avatar.tsx
@@ -5,40 +5,46 @@ import fetchS3Image from '@/middlewares/fetchS3Image';
 import Avatar, { AvatarProps } from '@mui/material/Avatar';
 
 const useImageQuery = (imgKey: string) => useQuery(['image', imgKey], () => fetchS3Image(imgKey), {
-  enabled: !!imgKey,
+  enabled: !!imgKey && imgKey !== '',
 });
 
-export type S3AvatarProps = AvatarProps & { src: string };
+export type S3AvatarProps = AvatarProps & { src: string, placeholderImg?: string };
 
 const onClick = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
   e.preventDefault();
   e.stopPropagation();
 };
 
-const S3Avatar = ({ src, alt, children, ...others }: S3AvatarProps) => {
-  const { data } = useImageQuery(src);
-  const [image, setImage] = useState<{ url: string; name?: string } | undefined>();
+const S3Avatar = ({ src, alt, placeholderImg, children, ...others }: S3AvatarProps) => {
+  const imgData = useImageQuery(src);
+  const [image, setImage] = useState<{ url: string | undefined; name: string | undefined }>({
+    url: undefined,
+    name: undefined,
+  });
+
 
   useEffect(() => {
-    if (!data) return;
-    const { url, name } = data;
-    setImage({ url, name: name || alt });
-    // eslint-disable-next-line consistent-return
-    return () => URL.revokeObjectURL(url);
-  }, [alt, data]);
+    if (!imgData.isSuccess) return;
+    if (!imgData.data) return;
+    const { url, name } = imgData.data;
+    setImage({ url, name });
 
-  return image ? (
+    // eslint-disable-next-line consistent-return
+    if (url !== undefined) return () => URL.revokeObjectURL(url);
+  }, [imgData.isFetched]);
+
+  return (
     <Link
-      href={image.url}
-      download={image.name}
+      href={image.url || placeholderImg || ''}
+      download={image.name || alt}
       onClick={onClick}
       style={{ cursor: 'default', textDecoration: 'none' }}
     >
-      <Avatar src={image.url} {...others}>
+      <Avatar src={image.url || placeholderImg || ''} {...others}>
         {children}
       </Avatar>
     </Link>
-  ) : null;
+  );
 };
 
 export default S3Avatar;
--- apps/marketplace/src/components/S3BoxImage.tsx
+++ apps/marketplace/src/components/S3BoxImage.tsx
@@ -5,7 +5,7 @@ import fetchS3Image from '@/middlewares/fetchS3Image';
 import Box, { BoxProps } from '@mui/material/Box';
 
 const useImageQuery = (imgKey: string) => useQuery(['image', imgKey], () => fetchS3Image(imgKey), {
-  enabled: !!imgKey,
+  enabled: !!imgKey && imgKey !== '',
 });
 
 export type S3BoxImageProps = BoxProps & { src: string, placeholderImg?: string };
@@ -16,34 +16,39 @@ const onClick = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
 };
 
 const S3BoxImage = ({ src, placeholderImg, children, ...others }: S3BoxImageProps) => {
-  const { data } = useImageQuery(src);
-  const [image, setImage] = useState<{ url: string; name: string | undefined | null } | undefined>();
+  const imgData = useImageQuery(src);
+  const [image, setImage] = useState<{ url: string | undefined; name: string | undefined }>({
+    url: undefined,
+    name: undefined,
+  });
 
   useEffect(() => {
-    if (!data) return;
-    const { url, name } = data;
-    if (url === '' && placeholderImg) {
-      setImage({ url: placeholderImg, name });
-      return;
-    }
+    if (!imgData.isSuccess) return;
+    if (!imgData.data) return;
+    const { url, name } = imgData.data;
     setImage({ url, name });
+
     // eslint-disable-next-line consistent-return
-    return () => URL.revokeObjectURL(url);
-  }, [data]);
+    if (url !== undefined) return () => URL.revokeObjectURL(url);
+  }, [imgData.isFetched]);
 
 
-  return image ? (
-    <Link href={image.url} download={image.name} onClick={onClick}
-          style={{ cursor: 'default', textDecoration: 'none' }}>
+  return (
+    <Link
+      href={image.url || placeholderImg || ''}
+      download={image.name}
+      onClick={onClick}
+      style={{ cursor: 'default', textDecoration: 'none' }}
+    >
       <Box
         component='img'
-        src={image.url}
+        src={image.url || placeholderImg || ''}
         {...others}
       >
         {children}
       </Box>
     </Link>
-  ) : null;
+  );
 };
 
 export default S3BoxImage;
