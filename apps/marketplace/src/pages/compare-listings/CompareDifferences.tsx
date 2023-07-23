dae7ef60069d371aff12cc65ada91c19c62e7d5d
Adeeb <adityaadeeb04@gmail.com>
2023-07-23 22:30:39 +0800

    adding category images

--- apps/marketplace/src/pages/compare-listings/CompareDifferences.tsx
+++ apps/marketplace/src/pages/compare-listings/CompareDifferences.tsx
@@ -10,11 +10,13 @@ import fetchListing from '@/middlewares/fetchListing';
 import fetchCatById from '@/middlewares/fetchCatById';
 import { Listing } from '@/utils/api/client/zod/listings';
 import { Category } from '@/utils/api/client/zod/categories';
+import S3BoxImage from '@/components/S3BoxImage';
 
 interface TableRowData {
   id: string;
-  data: string[];
+  data: (string | JSX.Element)[];
 }
+
 
 interface TableData {
   sideHeaders: string[];
@@ -52,7 +54,6 @@ const CompareDifferences = ({ productIds }: CompareDifferencesProps) => {
       'Condition',
       'Category',
       'Negotiable',
-      'Metal Grade',
       'Cross Section Image',
       'Type',
       'Company',
@@ -73,10 +74,25 @@ const CompareDifferences = ({ productIds }: CompareDifferencesProps) => {
       { id: 'row2', data: listings.map((listing) => `$ ${listing.price} / ${listing.unit}`) },
       { id: 'row3', data: listings.map((listing) => `${listing.quantity}`) },
       { id: 'row4', data: listings.map((listing) => listing.description) },
-      { id: 'row5', data: listings.map((listing) => listing.categoryId) },
+      { id: 'row5', data: category.map((cat) => cat.name) },
       { id: 'row6', data: listings.map((listing) => (listing.negotiable ? 'Yes' : 'No')) },
-      { id: 'row7', data: [] },
-      { id: 'row8', data: [] },
+      {
+        id: 'row8',
+        data: category.map((cat) => (
+          <S3BoxImage
+            sx={{
+              height: { xs: 50, sm: 75, md: 100, lg:   125 },
+              display: 'block',
+              width: { xs: 0, sm: 150, md: 200, lg: 250 },
+              overflow: 'hidden',
+              opacity: '30%',
+              marginLeft: 'auto',
+              marginRight: 'auto',
+            }}
+            src={cat.image}
+          />
+        )),
+      },
       { id: 'row9', data: listings.map((listing) => listing.type) },
       { id: 'row10', data: listings.map((listing) => listing.owner.company.name) },
     ],
@@ -93,16 +109,21 @@ const CompareDifferences = ({ productIds }: CompareDifferencesProps) => {
             ))}
           </TableRow>
         </TableHead>
-        <TableBody>
-          {tableData.sideHeaders.map((header, index) => (
-            <TableRow key={header}>
-              <TableCell>{header}</TableCell>
-              {tableData.rows[index]?.data.map((cellData) => (
-                <TableCell key={cellData}>{cellData}</TableCell>
-              ))}
-            </TableRow>
-          ))}
-        </TableBody>
+       <TableBody>
+  {tableData.sideHeaders.map((header, index) => (
+    <TableRow key={header}>
+      <TableCell>{header}</TableCell>
+      {tableData.rows[index]?.data.map((cellData, cellIndex) =>
+        header === 'Cross Section Image' ? (
+          <TableCell key={`cell-${index}-${cellIndex}`}>{cellData}</TableCell>
+        ) : (
+          <TableCell key={`cell-${index}-${cellIndex}`}>{cellData}</TableCell>
+        )
+      )}
+    </TableRow>
+  ))}
+</TableBody>
+
       </Table>
     </TableContainer>
   );
