eeff3b864ba465dc4b1af54a6a2c4433746f3dbc
Yam Kar Lok <isonice4572@gmail.com>
2023-07-25 05:38:02 +0800

    Swap the usage of ListingTable component

--- apps/marketplace/src/components/marketplace/listing/ListingTable.tsx
+++ apps/marketplace/src/components/marketplace/listing/ListingTable.tsx
@@ -1,17 +1,35 @@
 // ** React Imports
-import React from 'react';
+import React, { useEffect } from 'react';
 
 // ** Mui Imports
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+
+// ** Hooks Imports
+import { useTranslation } from 'react-i18next';
+
+// ** Store Imports
+import useProductStore from '@/stores/products';
+import useParamStore from '@/stores/parameters';
+import {
+  useTableMode,
+  useTablePagination,
+  useTableSelection,
+  useTableStates,
+} from '@/stores/table';
 
 // ** Type Imports
-import type { Listing } from '@/utils/api/client/zod';
+import type { Listing } from '@/utils/api/server/zod/listingTable';
+
+// ** Custom Components Imports
+import TableHeader from './table/TableHeader';
+import TablePagination from './table/TablePagination';
+import ListingBadge from './table/ListingBadge';
 
 // ** ListingTable Props Types
 export type ListingTableProps = {
@@ -33,22 +51,80 @@ const ListingTable = (props: ListingTableProps) => {
   const { listings } = props;
 
   // ** States
+
   // ** Hooks
+  const { t } = useTranslation();
+  const [products, productInitializeTime, productActions] = useProductStore((state) => {
+    const { products, initialized, ...actions } = state;
+    return [state.products, state.initialized, actions];
+  });
+  const [params, paramInitializeTime, paramActions] = useParamStore((state) => {
+    const { params, initialized, ...actions } = state;
+    return [state.params, state.initialized, actions];
+  });
+  const [mode, modeActions] = useTableMode();
+  const [tableStates, stateActions] = useTableStates();
+  const [pagination, setPagination] = useTablePagination();
+  const [selected, selectionActions] = useTableSelection();
+
   // ** Effects
+  useEffect(() => {
+    console.log('products');
+  }, [products]);
+
+  useEffect(() => {
+    console.log('product initialized');
+  }, [productInitializeTime]);
+
+  useEffect(() => {
+    console.log('product actions');
+  }, [productActions]);
+
+  useEffect(() => {
+    console.log('params');
+  }, [params]);
+
+  useEffect(() => {
+    console.log('params initialized');
+  }, [paramInitializeTime]);
+
+  useEffect(() => {
+    console.log('params actions');
+  }, [paramActions]);
+
+  useEffect(() => {
+    console.log(listings);
+  }, [listings]);
 
   return (
     <>
       <h1>Hello World</h1>
       <TableContainer component={Paper}>
         <Table sx={{ minWidth: 800 }}>
-          <TableHead>
-            <TableRow>
-              <TableCell>Listing Name</TableCell>
-              <TableCell>Listing Name</TableCell>
-            </TableRow>
-          </TableHead>
+          <TableHeader />
+          <TableBody>
+            {listings.map((row) => (
+              <TableRow key={row.id} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                <TableCell component="th" scope="row">
+                  {row.id}
+                </TableCell>
+                <TableCell align="left">{row.price}</TableCell>
+                <TableCell align="left">{row.quantity}</TableCell>
+                <TableCell align="left">
+                  <Box sx={{ display: 'flex', gap: '8px' }}>
+                    <ListingBadge type={row.type === 'BUY' ? 'buy' : 'sell'} />{' '}
+                    {row.negotiable ? <ListingBadge type="negotiable" /> : null}
+                  </Box>
+                </TableCell>
+                <TableCell align="left">{row.createdAt}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
         </Table>
       </TableContainer>
+      <TablePagination />
     </>
   );
 };
+
+export default ListingTable;
