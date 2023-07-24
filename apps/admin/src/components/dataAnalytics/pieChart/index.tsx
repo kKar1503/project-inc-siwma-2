3a325cdedd315c712cec6e32913801842c8c018f
Xavier Tay <45095604+catcd1w3r5@users.noreply.github.com>
2023-07-25 07:21:54 +0800

    style: formatted pie chart

--- apps/admin/src/components/dataAnalytics/pieChart/index.tsx
+++ apps/admin/src/components/dataAnalytics/pieChart/index.tsx
@@ -1,7 +1,6 @@
 import { VictoryContainer, VictoryPie, VictoryTheme, VictoryTooltip } from 'victory';
 import ModuleBase from '@/components/advertisementsDashboard/moduleBase';
 import Title from '@/components/graphs/overlay/title';
-import Grid from '@mui/material/Grid';
 
 export interface ActiveCategoriesProps {
   data: Array<{
@@ -20,36 +19,33 @@ const PieChart = ({ data,title }: ActiveCategoriesProps) => {
   );
   return (
     <ModuleBase noFlex width='85%'>
-      <Grid container spacing={2} sx={{ height: '100%' }}>
-        <Grid item xs={1} />
-        <Grid item xs={11}>
-          <Title title={title} primarySize={20} x={-35} />
-          <VictoryPie
-            containerComponent={<VictoryContainer />}
-            // origin={}
-            labelComponent={<VictoryTooltip />}
-            theme={VictoryTheme.material}
-            events={[{
-              target: 'data',
-              eventHandlers: {
-                onMouseOver: () => [
-                  {
-                    target: 'labels',
-                    mutation: () => ({ active: true }),
-                  },
-                ],
-                onMouseOut: () => [
-                  {
-                    target: 'labels',
-                    mutation: () => ({ active: false }),
-                  },
-                ],
+      {/* idk why but the other graphs have a small spacing at the top but this doesn't so im adding this for consistency */}
+      <br/>
+      <Title title={`${title} (${formattedData.length})`} primarySize={20} x={-35} />
+      <VictoryPie
+        containerComponent={<VictoryContainer />}
+        // origin={}
+        labelComponent={<VictoryTooltip />}
+        theme={VictoryTheme.material}
+        events={[{
+          target: 'data',
+          eventHandlers: {
+            onMouseOver: () => [
+              {
+                target: 'labels',
+                mutation: () => ({ active: true }),
+              },
+            ],
+            onMouseOut: () => [
+              {
+                target: 'labels',
+                mutation: () => ({ active: false }),
+              },
+            ],
               },
             }]}
             data={formattedData}
           />
-        </Grid>
-      </Grid>
     </ModuleBase>
   );
 };
