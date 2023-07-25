e017c488d48c276e9d5baf5e245b0551cdc7952c
Yam Kar Lok <isonice4572@gmail.com>
2023-07-26 05:45:40 +0800

    Added useShareListings hooks for getting those gucchi shares

--- apps/marketplace/src/services/listings/useShareListings.tsx
+++ apps/marketplace/src/services/listings/useShareListings.tsx
@@ -1,1 +1,24 @@
+// ** API Client
+import apiClient from '@/utils/api/client/apiClient';
 
+// ** React Query
+import { useQuery } from 'react-query';
+
+/**
+ * Creates a share link for selected listings
+ */
+const useListings = (ownerId: string, listings: string[]) =>
+  useQuery({
+    queryFn: async () =>
+      apiClient
+        .post(`/v1/share`, {
+          ownerId,
+          listings,
+        })
+        // parse data through zod to ensure data is correct
+        .then((res) => res.data.data[0].hash as string),
+    queryKey: ['shareListing', ownerId, listings],
+    enabled: false,
+  });
+
+export default useListings;
