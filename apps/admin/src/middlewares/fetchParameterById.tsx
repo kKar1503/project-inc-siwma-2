d5e95ba135293b53ee0acc65715359ccdbba231d
huishilhs <huishilhs@gmail.com>
2023-07-25 22:00:55 +0800

    fix: :bug: fix param get by id endpoint

--- apps/admin/src/middlewares/fetchParameterById.tsx
+++ apps/admin/src/middlewares/fetchParameterById.tsx
@@ -2,7 +2,7 @@ import apiClient from '@/utils/api/client/apiClient';
 import parameters from '@/utils/api/client/zod/parameters';
 
 const fetchParameterById = async (id: string | string[] | undefined) => {
-  const response = await apiClient.get(`/v1/parameters/${id}`);
+  const response = await apiClient.get(`/v1/parameters?id=${id}`);
 
   const parameterId = parameters.getById.parse(response.data.data[0]);
 
