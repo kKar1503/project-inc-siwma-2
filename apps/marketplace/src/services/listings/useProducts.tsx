a07a3e5bf421905b305c3ce12b6d0cd8415a6c1a
Yam Kar Lok <isonice4572@gmail.com>
2023-07-26 00:01:20 +0800

    And here's the hook for the forgotten baby girl

---
+++ apps/marketplace/src/services/listings/useProducts.tsx
@@ -0,0 +1,28 @@
+// ** API Client
+import apiClient from '@/utils/api/client/apiClient';
+
+// ** React Query
+import { useQuery } from 'react-query';
+
+// ** Types Imports
+import { productsSchema } from '@/utils/api/server/zod/products';
+
+/**
+ * Fetch products data for zustand store
+ */
+const useProducts = (productIds: string[]) =>
+  useQuery({
+    queryFn: async () =>
+      apiClient
+        .get(`/v1/listings/table/products`, {
+          params: {
+            id: productIds.join(','),
+          },
+        })
+        // parse data through zod to ensure data is correct
+        .then((res) => productsSchema.parse(res.data.data)),
+    queryKey: ['products', productIds],
+    enabled: productIds.length !== 0,
+  });
+
+export default useProducts;
