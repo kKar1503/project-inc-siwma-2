581f25f10609227e270772ae0477b832f81f9427
Xavier Teo <xavierteo26@gmail.com>
2023-07-25 23:23:58 +0800

    fix: :bug: FormSearchDropdown default value brokey

--- apps/admin/src/components/forms/FormSearchDropdown.tsx
+++ apps/admin/src/components/forms/FormSearchDropdown.tsx
@@ -3,10 +3,15 @@ import { Controller, FieldValues, RegisterOptions, useFormContext } from 'react-
 import Skeleton from 'react-loading-skeleton';
 import 'react-loading-skeleton/dist/skeleton.css';
 
+type FormSearchDropdownOption = {
+  label: string;
+  value: string;
+};
+
 type FormSearchDropdownProps = {
   name: string;
   label: string;
-  options: React.ComponentProps<typeof Autocomplete>['options'];
+  options: FormSearchDropdownOption[];
   placeholder?: string;
   // We do not know what the shape of the object will be
   // eslint-disable-next-line react/forbid-prop-types
@@ -63,7 +68,7 @@ const FormSearchDropdown = ({
       <Controller
         name={name}
         control={control}
-        render={({ field: { ref, ...field }, fieldState: { error, invalid } }) => (
+        render={({ field: { ref, ...field }, formState: { defaultValues } }) => (
           <Autocomplete
             {...field}
             placeholder={placeholder}
@@ -76,10 +81,15 @@ const FormSearchDropdown = ({
               },
               ...sx,
             }}
+            defaultValue={defaultValues ? defaultValues[name] : undefined}
             renderInput={(params) => <TextField {...params} label="Dropdown Option" />}
+            getOptionLabel={(option) => option.label}
+            isOptionEqualToValue={(option, value) => option.value === value.value}
             {...hookInput(name, label, customValidation)}
-            onChange={(e, value) => field.onChange(value)}
-            onInputChange={(_, data) => field.onChange(data)}
+            onChange={(e, value) => {
+              field.onChange(value);
+              console.log({ value });
+            }}
           />
         )}
       />
