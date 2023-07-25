5537d132330884026727f255f7f5800ba759dc46
Ong Hin Journ <onghinjourn@gmail.com>
2023-07-25 22:42:10 +0800

    style: :rotating_light: updates eslint rules again

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -53,7 +53,7 @@ module.exports = {
       },
     ],
     'max-classes-per-file': 'off',
-    '@typescript-eslint/no-shadow': 'off',
+    '@typescript-eslint/no-shadow': 'warn',
   },
   overrides: [
     {
