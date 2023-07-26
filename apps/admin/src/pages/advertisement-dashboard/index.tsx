9a5b6f69e0f615a734ece0d4bf866811b6229565
Xavier Tay <45095604+catcd1w3r5@users.noreply.github.com>
2023-07-26 13:57:17 +0800

    fix: eslint

--- apps/admin/src/pages/advertisement-dashboard/index.tsx
+++ apps/admin/src/pages/advertisement-dashboard/index.tsx
@@ -42,14 +42,14 @@ const AdvertisementDashboard = () => {
         <Grid item xs={12} md={9}>
           <AdSpaceSection totalClicks={totalClicks} />
         </Grid>
-        {/*<Grid item container xs={12} md={3} spacing={2}>*/}
-        {/*  <Grid item xs={6} md={12}>*/}
-        {/*    <ClicksPie data={companyClicks} totalClicks={totalClicks} />*/}
-        {/*  </Grid>*/}
-        {/*  <Grid item xs={6} md={12}>*/}
-        {/*    <ActiveGraph data={activeData} />*/}
-        {/*  </Grid>*/}
-        {/*</Grid>*/}
+        {/* <Grid item container xs={12} md={3} spacing={2}> */}
+        {/*  <Grid item xs={6} md={12}> */}
+        {/*    <ClicksPie data={companyClicks} totalClicks={totalClicks} /> */}
+        {/*  </Grid> */}
+        {/*  <Grid item xs={6} md={12}> */}
+        {/*    <ActiveGraph data={activeData} /> */}
+        {/*  </Grid> */}
+        {/* </Grid> */}
       </Grid>
     </Box>
   );
