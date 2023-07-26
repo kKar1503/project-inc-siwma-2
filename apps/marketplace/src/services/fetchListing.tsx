f14c1327a4acce4384e2c16ac536b07c4c9609d1
Matthew Ng <matthew.ngjb@gmail.com>
2023-07-26 14:00:27 +0800

    fix: :rotating_light: - Fix lint errors

--- apps/marketplace/src/services/fetchListing.tsx
+++ apps/marketplace/src/services/fetchListing.tsx
@@ -2,11 +2,11 @@ import apiClient from '@/utils/api/client/apiClient';
 import listings from '@/utils/api/client/zod/listings';
 
 const fetchListing = async (listingID: string, includeParameters?: boolean) => {
-  let query = `/v1/listings/${listingID}`;
+  const query = `/v1/listings/${listingID}`;
 
   const response = await apiClient.get(query, {
     params: {
-      includeParameters: includeParameters,
+      includeParameters,
     },
   });
   const parsedListing = listings.getById.parse(response.data.data[0]);
