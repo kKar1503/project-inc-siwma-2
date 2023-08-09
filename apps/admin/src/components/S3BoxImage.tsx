8efea63a73baf74b745db5726ebe08fcbcea41e9
Xavier Tay <45095604+catcd1w3r5@users.noreply.github.com>
2023-08-10 07:04:28 +0800

    fix: applied corresponding changes to admin

--- apps/admin/src/components/S3BoxImage.tsx
+++ apps/admin/src/components/S3BoxImage.tsx
@@ -4,7 +4,9 @@ import { useQuery } from 'react-query';
 import fetchS3Image from '@/services/fetchS3Image';
 import Box, { BoxProps } from '@mui/material/Box';
 
-const useImageQuery = (imgKey: string) => useQuery(['image', imgKey], () => fetchS3Image(imgKey));
+const useImageQuery = (imgKey: string) => useQuery(['image', imgKey], () => fetchS3Image(imgKey), {
+  enabled: !!imgKey && imgKey !== '',
+});
 
 export type S3BoxImageProps = BoxProps & { src: string; placeholderImg?: string };
 
@@ -14,35 +16,39 @@ const onClick = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
 };
 
 const S3BoxImage = ({ src, placeholderImg, children, ...others }: S3BoxImageProps) => {
-  const { data } = useImageQuery(src);
-  const [image, setImage] = useState<
-    { url: string; name: string | undefined | null } | undefined
-  >();
+  const imgData = useImageQuery(src);
+  const [image, setImage] = useState<{ url: string | undefined; name: string | undefined }>({
+    url: undefined,
+    name: undefined,
+  });
 
   useEffect(() => {
-    if (!data) return;
-    const { url, name } = data;
-    if (url === '' && placeholderImg) {
-      setImage({ url: placeholderImg, name });
-      return;
-    }
+    if (!imgData.isSuccess) return;
+    if (!imgData.data) return;
+    const { url, name } = imgData.data;
     setImage({ url, name });
-    // eslint-disable-next-line consistent-return
-    return () => URL.revokeObjectURL(url);
-  }, [data]);
 
-  return image ? (
+    // eslint-disable-next-line consistent-return
+    if (url !== undefined) return () => URL.revokeObjectURL(url);
+  }, [imgData.data, imgData.isFetched, imgData.isSuccess]);
+
+
+  return (
     <Link
-      href={image.url}
+      href={image.url || placeholderImg || '/images/catPlaceholder.png'}
       download={image.name}
       onClick={onClick}
       style={{ cursor: 'default', textDecoration: 'none' }}
     >
-      <Box component="img" src={image.url} {...others}>
+      <Box
+        component='img'
+        src={image.url || placeholderImg || '/images/catPlaceholder.png'}
+        {...others}
+      >
         {children}
       </Box>
     </Link>
-  ) : null;
+  );
 };
 
 export default S3BoxImage;
