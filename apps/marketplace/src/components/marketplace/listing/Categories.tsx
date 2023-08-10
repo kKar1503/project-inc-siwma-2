f54f34eb938db129673b9d77a802b9b21ac7e6ec
Xavier Tay <45095604+catcd1w3r5@users.noreply.github.com>
2023-08-10 16:04:51 +0800

    refactor: refactored imports, removed unused vars

--- apps/marketplace/src/components/marketplace/listing/Categories.tsx
+++ apps/marketplace/src/components/marketplace/listing/Categories.tsx
@@ -1,5 +1,8 @@
 import { TCategory } from '@/types/category';
-import { Card, CardActionArea, CardContent, Typography } from '@mui/material';
+import Card from '@mui/material/Card';
+import CardActionArea from '@mui/material/CardActionArea';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
 import React from 'react';
 import S3CardImage from '@/components/S3CardImage';
 import { useResponsiveness } from '@inc/ui';
@@ -9,11 +12,7 @@ import { useTranslation } from 'react-i18next';
 const CategoryCard: React.FC<TCategory> = ({
                                              id,
                                              name,
-                                             description,
                                              image,
-  crossSectionImage,
-  active,
-  parameters,
 }) => {
   const [isSm] = useResponsiveness(['sm']);
   const { t } = useTranslation();
