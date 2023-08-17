f77f565153a506bb3a8b37f504dea704537f47cc
Javier <JAVIERFOKJF11.21@ichat.sp.edu.sg>
2023-08-17 14:39:16 +0800

    adding loading spinner

---
+++ apps/lighthouse/src/components/LoadingSpinner.jsx
@@ -0,0 +1,7 @@
+const LoadingSpinner = () => (
+    <div className="flex justify-center items-center h-full">
+      <div className="animate-spin rounded-full h-10 w-10 border-b-2 border-white"></div>
+    </div>
+  );
+  
+  export default LoadingSpinner;
